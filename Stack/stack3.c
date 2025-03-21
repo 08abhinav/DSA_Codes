7f655c2acf0e5ed8063f75c12fc3e7c80842b79b
08abhinav <abhinavnegi101@gmail.com>
2025-03-21 22:56:02 +0530

    functions defined

--- Stack/stack3.c
+++ Stack/stack3.c
@@ -2,23 +2,30 @@
 #define max 5
 struct stack{
     int top = -1;
+    int arr[max];
 }typedef stk;
 
+void push(stk *);
+void pop(stk *);
+void display(stk *);
+void peek(stk *);
+
 int main(){
-    int top=-1, arr[max], ch;
+    stk s;
     do{
         printf("1.Push\n2.Pop\n3.Display\n4.Peek\nEnter your choice: ");
         scanf("%d",&ch);
         switch(ch){
-            case 1: push(arr, top);
+            case 1: push(&s);
             break;
-            case 2: pop(arr, &top);
+            case 2: pop(&s);
             break;
-            case 3: display(arr, &top);
+            case 3: display(&s);
             break;
-            case 4: peek(arr, &top);
+            case 4: peek(&s);
             break;
             default:printf("Not a valid choice\n");
         }
     }while(ch<=4);
 }
+
