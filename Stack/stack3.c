84cadf7ea6fff95a104cfb49a2ae3539d50ba6ba
08abhinav <abhinavnegi101@gmail.com>
2025-03-21 23:05:01 +0530

    implemented push and display

--- Stack/stack3.c
+++ Stack/stack3.c
@@ -1,31 +1,56 @@
 #include<stdio.h>
 #define max 5
 struct stack{
-    int top = -1;
+    int top;
     int arr[max];
 }typedef stk;
 
 void push(stk *);
 void pop(stk *);
 void display(stk *);
-void peek(stk *);
+// void peek(stk *);
 
 int main(){
     stk s;
+    s.top = -1;
+    int ch;
     do{
         printf("1.Push\n2.Pop\n3.Display\n4.Peek\nEnter your choice: ");
         scanf("%d",&ch);
         switch(ch){
             case 1: push(&s);
             break;
-            case 2: pop(&s);
+            case 2: display(&s);
             break;
-            case 3: display(&s);
-            break;
-            case 4: peek(&s);
-            break;
+            // case 3: display(&s);
+            // break;
+            // case 4: peek(&s);
+            // break;
             default:printf("Not a valid choice\n");
         }
     }while(ch<=4);
 }
 
+void push(stk *s){
+    if(s->top==max-1){
+        printf("Stack overflow\n");
+    }else{
+        int x;
+        printf("Enter the value to push: ");
+        scanf("%d", &x);
+        s->top += 1;
+        s->arr[s->top] = x;
+    }
+}
+
+void display(stk *s){
+    if(s->top==-1){
+        printf("Stack is empty\n");
+    }else{
+        printf("Stack elements:\n");
+        for (int i = s->top; i >= 0; i--) {
+            printf("%d\n", s->arr[i]);
+        }
+        printf("\n");
+    }
+}
