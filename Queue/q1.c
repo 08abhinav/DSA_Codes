36d768cdba520590601bbcb56a6eb2c6b3a7b24a
08abhinav <abhinavnegi101@gmail.com>
2025-03-23 19:19:23 +0530

    updated queue's insert method

--- Queue/q1.c
+++ Queue/q1.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #define max 5
 
-int insert(int);
-int delete(int);
-void display(int, int);
+int insert(int[], int);
+int delete(int[], int);
+void display(int[], int, int);
 
 int main(){
     int ch, arr[max], f=-1, r=-1;
@@ -11,7 +11,7 @@ int main(){
         printf("1.Insert\n2.Delete\n3.Display\nEnter your choice: ");
         scanf("%d", &ch);
         switch(ch){
-            case 1: r = insert(r);
+            case 1: r = insert(arr, r);
             if(f==-1){
                 f=0;
             }
@@ -29,3 +29,18 @@ int main(){
         }
     }while(ch<=3);
 }
+
+int insert(int r){
+    if(r==max-1){
+        printf("Queue is \n");
+    }else{
+        int x;
+        printf("Enter the value to insert: ");
+        scanf("%d", &x);
+        r++;
+        arr[r] = x;
+    }
+    return r;
+}
+
+
