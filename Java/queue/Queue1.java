32f72d30c12be8a4e3a332fa10ce617f856bb1c5
08abhinav <abhinavnegi101@gmail.com>
2025-03-25 17:59:45 +0530

    queue's insertion done

--- Java/queue/Queue1.java
+++ Java/queue/Queue1.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 class Queue1{
+    Scanner sc = new Scanner(System.in);
     private int[] q;
     private int size;
     private int f=-1, r=-1;
     Queue1(int n){
         q = new int[n];
         this.size = n;
+    }
+
+    void insert(){
+        if(r==size-1){
+            System.out.print("Queue is full\n");
+        }else{
+            int x;
+            System.out.print("Enter the value to insert: ");
+            x = sc.nextInt();
+            if(f==-1){
+                f=0;
+            }else{
+                r += 1;
+                q[r] = x;
+            }
+        }
     }
 
     
