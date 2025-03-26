34a47a868ac6cf38ce008f9e74797d51a107db9e
08abhinav <abhinavnegi101@gmail.com>
2025-03-26 17:33:51 +0530

    updated circular queue's insert method

--- Java/queue/Queue2.java
+++ Java/queue/Queue2.java
@@ -9,17 +9,18 @@ class Queue2{
     }
 
     void insert(){
-        if((r+1)%size == size -1){
+        if((r+1)%size==size-1){
             System.out.print("Queue is full\n");
         }else{
             int x;
-            System.out.print("Enter the element to insert: ");
+            System.out.print("Enter the value to insert: ");
             x = sc.nextInt();
             if(f==-1){
                 f=0;
+            }else{
+                r = (r+1)%size;
+                q[r] = x;
             }
-            r = (r+1)%size;
-            q[r] = x;
         }
     }
 }
