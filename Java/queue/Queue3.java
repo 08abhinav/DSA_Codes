702124aba60c8e3af485355efb0b27b5bd6e625b
08abhinav <abhinavnegi101@gmail.com>
2025-03-26 18:04:43 +0530

    Implementing Double ended queue

---
+++ Java/queue/Queue3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Queue3{
+    Scanner sc = new Scanner(System.in);
+    private int size, f=-1, r=-1;
+    private int[] q;
+    Queue3(int n){
+        q = new int[n];
+        this.size = n;
+    }
+}
