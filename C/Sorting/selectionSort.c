75726c0e114786bd2de72d2816373f1ef9a6be45
08abhinav <abhinavnegi101@gmail.com>
2025-03-31 21:31:53 +0530

    updated print function

--- C/Sorting/selectionSort.c
+++ C/Sorting/selectionSort.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
-#define max 5;
+#define max 5
 void print(int[]);
 void selectionSort(int[]);
 
 int main(){
     int arr[max];
+    printf("Enter the elements\n");
+    for(int i=0;i<max;i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("You entered\n");
+    print(arr);
 }
+
+void print(int arr[max]){
+    for(int i=0;i<max;i++){
+        printf("%d\t", arr[i]);
+    }
+}
+
