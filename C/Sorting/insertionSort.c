0e000f88974d5745aa543ec1066154ae6bc5eb57
08abhinav <abhinavnegi101@gmail.com>
2025-04-01 10:40:35 +0530

    starting with insertion sort

---
+++ C/Sorting/insertionSort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void print(int[], int);
+void insertionSort(int[], int);
+
+int main(){
+    int arr[100], n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    arr[n];
+    printf("Enter the elements\n");
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Before sorting\n");
+    print(arr, n);
+    selectionSort(arr, n);
+    printf("\nAfter Sorting\n");
+    print(arr, n);
+}
