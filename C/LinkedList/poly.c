15aaf343576b30f329ebf50a35d09ab2a5157dde
08abhinav <abhinavnegi101@gmail.com>
2025-04-02 21:54:01 +0530

    starting with  polynomial evaluation using linked list

---
+++ C/LinkedList/poly.c
@@ -0,0 +1,16 @@
+/* Polynomial execution using linked list
+eg- given to us 2x^3+5x^2+7x+10
+so we have to solve this polynomial equation using linked list
+
+***IMPORTANT POINTS:
+-> if exponent is greater than the existing exponent it should
+be before the smallest exponent
+
+-> if exponent is smaller thant the existing exponent then it 
+should be after the greater exponent
+
+-> if exponent already exist and user insert the same exponent
+it should not inserted in list
+
+-> there should be three function 1.Insert 2.Display 3.Evaluate
+*/
