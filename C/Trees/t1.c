a37c67076c9187c22c763afad13d09904e522168
08abhinav <abhinavnegi101@gmail.com>
2025-04-05 15:10:28 +0530

    Updated the starting of tree

--- C/Trees/t1.c
+++ C/Trees/t1.c
@@ -1,51 +1,30 @@
-    #include<stdio.h>
-    #include<stdlib.h>
-    struct list{
-        int data;
-        struct list *prev
-        struct list *next;
-    } typedef node;
+#include<stdio.h>
+#include<stdlib.h>
 
-    void insert(node **, node **, node **);
-    void displayRoot(node **);
+struct list{
+    struct list *left;
+    int data;
+    struct list *right;
+}typedef node;
 
-    int main(){
-        node *l=NULL, *r=NULL, *root=NULL;
-        int ch;
-        do{
-            printf("1.Insert\n2.Display\nEnter your choice: ");
-            scanf("%d", &ch);
-            switch(ch){
-                case 1: insert(&l, &r, &root);
-                break;
+node* createNode(int);
+node* insert(node*, int);
+node* display(node*);
 
-                case 2: displayRoot(&root);
-                break;
-            }
-        }while(ch<=2);
-    }
+int main(){
+    node *root = NULL;
+    int ch, data;
+    do{
+        printf("1.Insert\n2.Display\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1: printf("Enter the value you want to insert: ");
+            scanf("%d", &data);
+            root = insert(root, data);
+            break;
 
-    void insert(node **l, node **r, node **root){
-        node *ptr = NULL;
-        ptr = (node *)malloc(sizeof(node));
-        if(!ptr){
-            printf("Memory not allocated\n");
-            return;
-        }else{
-            int x;
-            printf("Enter the value: ");
-            scanf("%d", &x);
-            ptr->data = x;
-            ptr->prev = NULL;
-            ptr->next = NULL;
-            if(*l==NULL && *r==NULL){
-                *l = *r = *root = ptr;
-            }else if(ptr->data > (*root)->data){
-                (*r)->next = ptr;
-                *r = ptr;
-            }else{
-                (*l)->next = ptr;
-                *l = ptr;
-            }
+            case 2: display(root);
+            break;
         }
-    }
+    }while(ch>0 && ch<=2);
+}
