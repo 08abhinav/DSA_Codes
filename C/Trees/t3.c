2a5c1d03d9867351d4db804c633792cf31c4abef
08abhinav <abhinavnegi101@gmail.com>
2025-04-15 16:48:25 +0530

    implemented insert method

--- C/Trees/t3.c
+++ C/Trees/t3.c
@@ -14,7 +14,7 @@ void search(tree *);
 void inorder(tree *);
 
 int main(){
-    struct *root=NULL;
+    tree *root=NULL;
     int ch;
     do{
         printf("1.Insert\n2.Search\n3.Inorder\nEnter your choice: ");
@@ -32,3 +32,45 @@ int main(){
     }while(ch<=3);
 }
 
+tree* insert(tree *root){
+    tree *ptr = NULL, *p=NULL, *c=NULL;
+    int x;
+    ptr = (tree *)malloc(sizeof(tree));
+    if(ptr){
+        printf("Enter the value to insert: ");
+        scanf("%d", &x);
+        ptr->data = x;
+        ptr->left = NULL;
+        ptr->right = NULL;
+
+        if(root == NULL){
+            root = ptr;
+            return root;
+        }
+        p = c = root;
+
+        while(c!=NULL){
+            p = c;
+            if(c->data > x){
+                c = p->left;
+            }
+            else if(c->data < x){
+                c = p->right;
+            }
+            else{
+                printf("Duplicate value not allowed\n");
+                free(ptr);
+                return root;
+            }
+        }
+        if(p->data > x){
+            p->left = ptr;
+        }else{
+            p->right = ptr;
+        }
+        return root;
+    }else{
+        printf("Memory not allocated\n");
+    }
+
+}
