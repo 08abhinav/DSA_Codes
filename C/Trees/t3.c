388adb7dfeea650670d10e5a0204fe03d11a0c7b
08abhinav <abhinavnegi101@gmail.com>
2025-04-15 16:36:52 +0530

    starting with searching an element in a BST

---
+++ C/Trees/t3.c
@@ -0,0 +1,30 @@
+// Finding a node in a tree
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct list{
+    struct list *left;
+    int data;
+    struct list *right;
+}tree;
+
+int main(){
+    struct *root=NULL;
+    int ch;
+    do{
+        printf("1.Insert\n2.Search\n3.Inorder\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1: root = insert(root);
+            break;
+
+            case 2: search(root):
+            break;
+
+            case 3: inorder(root);
+            break;
+        }
+    }while(ch<=3);
+}
+
