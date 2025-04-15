989c26780f4c75695b9acc9e0ccfa31fd14a272e
08abhinav <abhinavnegi101@gmail.com>
2025-04-15 17:26:28 +0530

    impelementing BST deletion

---
+++ C/Trees/t4.c
@@ -0,0 +1,34 @@
+// Deletion in BST
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct list{
+    struct list *left;
+    int data;
+    struct list *right;
+}tree;
+
+tree* insert(tree *);
+tree* delete(tree *);
+void inorder(tree *);
+
+int main(){
+    tree *root=NULL;
+    int ch;
+    do{
+        printf("1.Insert\n2.Delete\n3.Inorder\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1: root = insert(root);
+            break;
+
+            case 2: delete(root);
+            break;
+
+            case 3: inorder(root);
+            break;
+        }
+    }while(ch<=3);
+}
+
