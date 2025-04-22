4234e36803ec7c2a2fa46e648d74dc77c9098352
08abhinav <abhinavnegi101@gmail.com>
2025-04-22 17:42:58 +0530

    implemented main method

--- C/LinkedList/singly_rev.c
+++ C/LinkedList/singly_rev.c
@@ -11,3 +11,24 @@ node* insert(node *);
 void display(node *);
 void reverse(node *);
 
+int main(){
+    node *r=NULL, *h=NULL;
+    int ch;
+    do{
+        printf("1.Insert\n2.Display\n3.Reverse\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1: r = insert(r);
+            if(h==NULL){
+                h = r;
+            }
+            break;
+
+            case 2: display(h);
+            break;
+
+            case 3: reverse(h);
+            break;
+        }
+    }while(ch>=1 && ch<=3);
+}
