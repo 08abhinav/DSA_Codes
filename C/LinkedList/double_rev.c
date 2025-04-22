81f96f4e4c52f17588a8e562faaffd6b85a0ebf2
08abhinav <abhinavnegi101@gmail.com>
2025-04-22 16:38:51 +0530

    declare insert, display and main function

--- C/LinkedList/double_rev.c
+++ C/LinkedList/double_rev.c
@@ -9,4 +9,23 @@ typedef struct list{
     struct list* next;
 }node;
 
+node* insert(node*);
+void display(node *);
 
+int main(){
+    node *h = NULL, *r=NULL;
+    do{
+        printf("1.Insert\n2.Display\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1: r = insert(r);
+            if(l==NULL){
+                l = r;
+            }
+            break;
+
+            case 2: display(r);
+            break;
+        }
+    }while(ch>=1 && ch<=2);
+}
