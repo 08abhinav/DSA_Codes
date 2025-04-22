517235327c976717ba4937141a56b40001d0de54
08abhinav <abhinavnegi101@gmail.com>
2025-04-22 18:36:21 +0530

    incomplete invertTree

--- C/Trees/invertTree.c
+++ C/Trees/invertTree.c
@@ -9,4 +9,32 @@ typedef struct list{
     struct list *right;
 }tree;
 
-tree* insert(tree* );
+tree* insert(tree*);
+tree* inorder(tree*);
+tree* invert(tree*);
+
+int main(){
+    tree *r = NULL;
+    int ch;
+    do{
+        printf("1.Insert\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1: r = insert(r);
+            break;
+
+            case 2: tree(r);
+            break;
+        }
+    }while(ch<=2);
+}
+
+tree* insert(tree *r){
+    node *ptr = NULL;
+    ptr = (node*)malloc(sizeof(node));
+    if(ptr){
+
+    }else{
+        printf("Memory not allocated\n");
+    }
+}
