05fa982d2cf2434494b7426b2872fd97b088cb50
08abhinav <abhinavnegi101@gmail.com>
2025-04-23 18:34:40 +0530

    implemented push method

--- C/Stack/stack5.c
+++ C/Stack/stack5.c
@@ -1,11 +1,34 @@
-/* Stack application 
--> valid parentheses
-*/
 #include<stdio.h>
 #define max 100
-typedef struct stk{
+
+typedef struct stk {
     char arr[max];
     int top;
-}stack;
+} stack;
+
+void push(stack *, char);
+char pop(stack *);
+int checkValidParentheses(char []);
+
+int main() {
+    char ch[max];
+    printf("Enter a string of brackets: ");
+    fgets(ch, max, stdin);
+    
+    if (checkValidParentheses(ch)) {
+        printf("Valid parentheses\n");
+    } else {
+        printf("Invalid parentheses\n");
+    }
+}
+
+void push(stack *s, char ch) {
+    if (s->top == max - 1) {
+        printf("Stack overflow\n");
+    } else {
+        s->arr[++(s->top)] = ch;
+    }
+}
+
 
 
