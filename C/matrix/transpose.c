92af72e1121c5a3f5ab671ea7383d2183b33a2b9
08abhinav <abhinavnegi101@gmail.com>
2025-04-29 17:06:09 +0530

    implemented transpose of a matrix

--- C/matrix/transpose.c
+++ C/matrix/transpose.c
@@ -1,20 +1,44 @@
 //Transpose of a matrix without using extra matrix
 
 #include<stdio.h>
+void trans(int [][100], int, int);
 
-void trans(int [][], int, int);
 int main(){
-    int r, c,i,j;
+    int r, c,i,j, mat[100][100];
     printf("Enter the number of rows: ");
     scanf("%d", &r);
     printf("Enter the number of columns: ");
     scanf("%d", &c);
-    int mat[r][c];
     printf("Enter the elements\n");
     for(i=0;i<r;i++){
         for(j=0;j<c;j++){
             scanf("%d", &mat[i][j]);
         }
     }
+    printf("Entered matrix\n");
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
     trans(mat, r, c);
+}
+
+void trans(int mat[][100], int r, int c){
+    for (int i = 0; i < r; i++) {
+        for (int j = i + 1; j < c; j++) {
+            int temp = mat[i][j];
+            mat[i][j] = mat[j][i];
+            mat[j][i] = temp;
+        }
+    }
+
+    printf("After transposing matrix\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
 }
