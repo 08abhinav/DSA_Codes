71447d399e0d179268044e324fb2d787d724a893
08abhinav <abhinavnegi101@gmail.com>
2025-04-30 11:59:54 +0530

    implementing quick sort

---
+++ C/Sorting/ExchangeSort/quickSort.c
@@ -0,0 +1,16 @@
+/*Implementing Quick sort 
+->Qucik sort is Divide and Conquer algo:
+--  1-> Choose a pivot element.
+    2-> Partition the array into two halfs:
+        2.1-> Elements smaller than the pivot will be in its left.
+        2.2-> Elements greater than the pivot will be in its right.
+    3-> using recursion repeat step 2.1 and 2.2
+
+-- Time Complexity
+Best/Average: O(n log(n))
+Worst case: O(n^2) when pivot is always smallest or largest element.
+*/
+#include<stdio.h>
+#define max 5
+void quickSort(int arr[], int, int);
+
