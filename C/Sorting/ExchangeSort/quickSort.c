fee188cae254c914f6a6cf531c4bb0c097c2a50a
08abhinav <abhinavnegi101@gmail.com>
2025-04-30 12:05:22 +0530

    main method declared

--- C/Sorting/ExchangeSort/quickSort.c
+++ C/Sorting/ExchangeSort/quickSort.c
@@ -14,3 +14,19 @@ Worst case: O(n^2) when pivot is always smallest or largest element.
 #define max 5
 void quickSort(int arr[], int, int);
 
+int main(){
+    int arr[max];
+    printf("Enter the elements of array\n");
+    for(int i=0;i<max;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Before applying quick sort\n");
+    for(int i=0;i<max;i++){
+        printf("%d\t", arr[i]);
+    }
+    quickSort(arr, 0, max-1);
+    printf("After applying quick sort\n");
+    for(int i=0;i<max;i++){
+        printf("%d\t", arr[i]);
+    }
+}
