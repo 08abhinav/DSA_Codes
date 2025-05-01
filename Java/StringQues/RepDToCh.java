fd72fffae4721287acae4cf10141d341fcfed47c
08abhinav <abhinavnegi101@gmail.com>
2025-05-01 12:25:09 +0530

    updated logic

--- Java/StringQues/RepDToCh.java
+++ Java/StringQues/RepDToCh.java
@@ -4,12 +4,9 @@
 class RepDToCh{
     public String replaceDigit(String s){
         char arr[] = s.toCharArray();
-        for(int i=1;i<s.length();i++){
-            if(i%2!=0){
-                int shift = arr[i] - '0';
-                arr[i] = (char)((arr[i-1] + shift - 'a')%26 + 'a');
-            }
+        for(int i=1;i<s.length();i+=2){
+            arr[i] = (char)(arr[i-1] + arr[i] - '0');
         }
-        return new String(arr);
+        return String.valueOf(arr);
     }
 }
