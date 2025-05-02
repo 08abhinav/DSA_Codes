38ef477a1d78691d9bb474fdeeba5b83ac2c249a
08abhinav <abhinavnegi101@gmail.com>
2025-05-02 12:14:06 +0530

    implementing Lexicographicall ordered stirng

---
+++ Java/StringQues/Lexico.java
@@ -0,0 +1,10 @@
+/*We have given n and k where n is the number of characters in a string and k is 
+ sum of their numeric value.
+ -> for example:
+    -> n=3 and k=27 so it will be "aay"
+    a=1, a=1, and y=25
+ */
+
+class Lexico{
+    
+}
