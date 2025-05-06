b445fe96c68208feeaf7becc1aed354ccc261476
08abhinav <abhinavnegi101@gmail.com>
2025-05-06 19:27:38 +0530

    minor updation

---
+++ Java/StringQues/sortSent.java
@@ -0,0 +1,13 @@
+class SortSent{
+    public String sortSentence(String s){
+        String[] str = s.split(" ");
+        String[] res = new String[str.length];
+
+        for(String word: str){
+            int i = word.length()-1;
+            res[word.charAt(i)-'1'] = word.substring(0, i);
+        }
+
+        return String.join(" ", res);
+    }
+}
