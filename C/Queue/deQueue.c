91cddddfa13c0caf41d87d09a34808de8c31db11
08abhinav <abhinavnegi101@gmail.com>
2025-05-07 22:46:38 +0530

    implementing dequeue

---
+++ C/Queue/deQueue.c
@@ -0,0 +1,18 @@
+/* DeQueue (Double ended Queue)
+What is a dequeue?
+-> A queue where insertion and deletion can be performed 
+at both the end (front and rear).
+-> Unlike simple queue where insertion is done from rear and deletion
+is done from front, dequeue enables insertion and deletion from both the end.
+
+There are two types of dequeue:
+-> Input restricted dequeue
+    - Insertion at rear
+    - Deleteion at rear
+    - Deletion at front
+-> Output restricted dequeue
+    - Insertion at rear
+    - Insertion at front
+    - Deletion at front
+*/
+
