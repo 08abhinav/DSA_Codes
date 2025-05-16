ca52b4b41dce783c9b1b7b9472d17cc2ff96212b
08abhinav <abhinavnegi101@gmail.com>
2025-05-16 16:28:50 +0530

    updated main function

--- C/LinkedList/termWork/ques2.c
+++ C/LinkedList/termWork/ques2.c
@@ -10,5 +10,20 @@ node* insert(node*);
 void display(node*);
 
 int main(){
-    
+    node* head = NULL, *right = NULL;
+    int ch;
+    do{
+        printf("1.Insert\n2.Display\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1: right = insert(right);
+            if(head == NULL){
+                head = right;
+            }
+            break;
+
+            case 2: display(head);
+            break;
+        }
+    }
 }
