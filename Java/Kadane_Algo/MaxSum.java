8c994ceb58e67f592b43578a5696ae110b4eb38d
08abhinav <abhinavnegi101@gmail.com>
2025-05-20 19:53:28 +0530

    implemented

--- Java/Kadane_Algo/MaxSum.java
+++ Java/Kadane_Algo/MaxSum.java
@@ -4,5 +4,15 @@
  */
 import java.util.*;
 class MaxSum{
-    
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int currSum = arr[0]; 
+        int maxSum = arr[0]; 
+        for(int i: arr){
+            currSum = Math.max(i, currSum+i);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        System.out.println("Given array was: "+Arrays.toString(arr));
+        System.out.println("Maximum sum of subarray is: \n and subarray is: "+maxSum);
+    }
 }
