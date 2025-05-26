cf68eedd6b571a6f4c64b4f075a4ae05210bbd58
08abhinav <abhinavnegi101@gmail.com>
2025-05-26 10:54:04 +0530

    implemented to check palindrom string

--- Java/StringQues/Palindrome.java
+++ Java/StringQues/Palindrome.java
@@ -1,3 +1,18 @@
 class Palindrome{
-    
+    static boolean checkPalind(String s){
+        int b = 0, e = s.length()-1;
+        
+        while(b<e){
+            if(s.charAt(b) != s.charAt(e)){
+                return false;
+            }
+            b++;
+            e--;
+        }
+        return true;
+    }
+    public static void main(String...ars){
+        String s = "racecar";
+        System.out.println(Palindrome.checkPalind(s));
+    }
 }
