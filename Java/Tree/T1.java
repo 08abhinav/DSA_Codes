ee3b0e2fe07c4794a1a3411199163d19c8b2bfbc
08abhinav <abhinavnegi101@gmail.com>
2025-05-30 16:42:03 +0530

    implementing bst tree in java

---
+++ Java/Tree/T1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class T1{
+    class Node{
+        int data;
+        Node left, right;
+
+        Node(int data){
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+   
+}
