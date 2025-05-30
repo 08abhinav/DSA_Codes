128ef128627ac1493dd7f3e65b8bb2a146e4e128
08abhinav <abhinavnegi101@gmail.com>
2025-05-30 16:45:16 +0530

    implemented insertNode for bst

--- Java/Tree/T1.java
+++ Java/Tree/T1.java
@@ -10,5 +10,33 @@ class T1{
         }
     }
 
-   
+    public Node insertBST(Node root, int data){
+        Node curr = root;
+        while(true){
+            if(curr == null){
+                curr.data = data;
+                curr.left = null;
+                curr.right = null;
+            }
+            else if(curr.data > data){
+                if(curr.left == null){
+                    curr.left = new Node(data);
+                    break;
+                }else{
+                    curr = curr.left;
+                }
+            }else if(curr.data < data){
+                if(curr.right == null){
+                    curr.right  =new Node(data);
+                    break;
+                }else{
+                    curr = curr.right;
+                }
+            }else{
+                System.out.println("Data already present");
+                break;
+            }
+        }
+        return root;
+    }
 }
