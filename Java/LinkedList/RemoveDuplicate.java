664ccb4877dea6fdf02380bc384c74e2afb2816f
08abhinav <abhinavnegi101@gmail.com>
2025-05-31 14:26:17 +0530

    implementing removeduplicate from sorted list

---
+++ Java/LinkedList/RemoveDuplicate.java
@@ -0,0 +1,15 @@
+/*LeetCode Problem number 82 Remove Duplicate from sorted list II
+ */
+class RemoveDuplicate{
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    Node head = null;
+    
+
+}
