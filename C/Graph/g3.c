138cb3b309f04c94757550e1f0026f9523d363b8
08abhinav <abhinavnegi101@gmail.com>
2025-06-03 14:25:45 +0530

    implemented createGraph method

--- C/Graph/g3.c
+++ C/Graph/g3.c
@@ -5,6 +5,7 @@
 struct edg{
     int src;
     int dest;
+    int weight;
     struct edg* next;
 }typedef edge;
 
@@ -19,4 +20,36 @@ int main(){
     for(int i=0;i<vertices;i++){
         graph[i] = NULL;
     }
+    do{
+        printf("1.Create Graph\n2.Display Graph\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1: createGraph(graph, vertices);
+            break;
+        }
+    }
+}
+
+void createGraph(edge** graph, int vertices){
+    int ch, src, dest, weight;
+    do{
+        printf("Enter source node: ");
+        scanf("%d", &src);
+        
+        printf("Enter destination node: ");
+        scanf("%d", &dest);
+        
+        printf("Enter weight of node: ");
+        scanf("%d", &weight);
+
+        edge* newEdge = (edge*)malloc(sizeof(edge));
+        newEdge->src = src;
+        newEdge->dest = dest;
+        newEdge->weight = weight;
+        newEdge->next = graph[src];
+        graph[src] = newEdge
+
+        printf("Do you want to insert more nodes: ");
+        scanf("%d", &ch);
+    }while(ch==1);
 }
