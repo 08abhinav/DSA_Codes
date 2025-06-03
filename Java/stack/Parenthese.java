46cdc91b651831139d44e7c6ad7ff4bdb9f7aa80
08abhinav <abhinavnegi101@gmail.com>
2025-06-03 10:46:06 +0530

    implemented validParenthese method

--- Java/stack/Parenthese.java
+++ Java/stack/Parenthese.java
@@ -1,7 +1,21 @@
 import java.util.Stack;
 class Parenthese{
     static boolean validParenthese(String str){
-        
+        Stack<Character> stk = new Stack<>();
+        if(str.length() % 2==1) return false;
+
+        for(char c : str.toCharArray()){
+            if(c == '{' || c == '[' || c == '('){
+                stk.push(c);
+            }else{
+                if(c == '}' && stk.pop() != '{' ||
+                c == ']' && stk.pop() != '[' ||
+                c == ')' && stk.pop() != '('){
+                    return false;
+                }
+            }
+        }
+        return stk.isEmpty();
     }
     public static void main(String... args){
         
