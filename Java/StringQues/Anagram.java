aa5a4d7fe76b10d1e06d8bf802ef7705b5c38467
08abhinav <abhinavnegi101@gmail.com>
2025-06-03 12:45:00 +0530

    implemented validAnagram method

--- Java/StringQues/Anagram.java
+++ Java/StringQues/Anagram.java
@@ -1,7 +1,14 @@
 import java.util.Arrays;
 
 public class Anagram {
-    
+    static boolean validAnagram(String s1, String s2){
+        char[] ch = s1.toCharArray();
+        char[] ch2 = s2.toCharArray();
+
+        Arrays.sort(ch);
+        Arrays.sort(ch2);
+        return Arrays.equals(ch, ch2);
+    }
 
     public static void main(String...args){
         
