907389dc49479150a37df8fb58004c4dd6b9630e
08abhinav <abhinavnegi101@gmail.com>
2025-06-03 14:19:15 +0530

    define the structure of the program

--- C/Graph/g3.c
+++ C/Graph/g3.c
@@ -1,1 +1,22 @@
 /*Implementing Graph using array of pointer */
+#include<stdio.h>
+#include<stdlib.h>
+
+struct edg{
+    int src;
+    int dest;
+    struct edg* next;
+}typedef edge;
+
+void createGraph(edge**, int);
+void displayGraph(edge**, int);
+
+int main(){
+    int vertices, ch;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &vertices);
+    edge* graph[vertices];
+    for(int i=0;i<vertices;i++){
+        graph[i] = NULL;
+    }
+}
