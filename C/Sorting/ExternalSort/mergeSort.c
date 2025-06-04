68815a1192387203be57158262f9c9516bd6e2ab
08abhinav <abhinavnegi101@gmail.com>
2025-06-04 22:58:17 +0530

    updated mergeSort method and merge method

--- C/Sorting/ExternalSort/mergeSort.c
+++ C/Sorting/ExternalSort/mergeSort.c
@@ -42,36 +42,42 @@ log(n) for dividing the array
  }
  
  void mergeSort(int arr[], int low, int high) {
-     int mid, temp[max];
-     if(low < high) {
+    int mid, temp[max];
+    if(low < high) {
         mid = (low + high) / 2;
         mergeSort(arr, low, mid);
         mergeSort(arr, mid + 1, high);
         merge(arr, temp, low, mid, high);
-     }
+    }
  }
  
  void merge(int arr[], int temp[], int low, int mid, int high) {
-     int i = low, j = mid + 1, k = 0;
- 
-     while(i <= mid && j <= high) {
+    int i = low, j = mid + 1, k = 0;
+
+    while(i <= mid && j <= high) {
         if(arr[i] < arr[j]) {
             temp[k++] = arr[i++];
         } else {
             temp[k++] = arr[j++];
         }
-     }
- 
-     while(i <= mid) {
-        temp[k++] = arr[i++];
-     }
- 
-     while(j <= high) {
-        temp[k++] = arr[j++];
-     }
- 
-     for(int i = 0; i < k; i++) {
-        arr[low + i] = temp[i];
-     }
+    }
+    for(;i<=mid;i++){
+        temp[k++] = arr[i];
+    }
+    
+    for(;j<=high;j++){
+        temp[k++] = arr[j];
+    }
+    // while(i <= mid) {
+    // temp[k++] = arr[i++];
+    // }
+
+    // while(j <= high) {
+    // temp[k++] = arr[j++];
+    // }
+
+    for(int i = 0; i < k; i++) {
+    arr[low + i] = temp[i];
+    }
  }
  
