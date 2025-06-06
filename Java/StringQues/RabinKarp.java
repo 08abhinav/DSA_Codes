d1c2b982f6c6514d6d07f7114e333286f3edbdec
08abhinav <abhinavnegi101@gmail.com>
2025-06-06 10:38:42 +0530

    implementing pattern searching algo

---
+++ Java/StringQues/RabinKarp.java
@@ -0,0 +1,18 @@
+/* Rabin-Karp Algorithm
+ * This string-searching algorithm is used to find a pattern in a text.
+ * It is done using hashing for eg-> we have to find a pattern(P) in text(T), so we will calculate the hashP and then compare it
+ with the hash value of substring in text(T) if matches return else continue.
+    Pros
+    -> Efficient for multiple pattern searching.
+    -> Rolling hash avoids recomputation.
+
+    Cons
+    -> Hash collisions can cause false positives → needs verification.
+    
+    Real World use case:
+    -> Plagiarism detection
+    -> Searching DNA sequences
+ */
+public class RabinKarp {
+    
+}
