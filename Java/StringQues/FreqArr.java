7a1dca7aaa63e0683b8a47f248d1c03171ecc7df
08abhinav <abhinavnegi101@gmail.com>
2025-06-06 10:21:16 +0530

    frequency count using array

---
+++ Java/StringQues/FreqArr.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class FreqArr{
+    public static void main(String...args){
+        String str = "abcdefghabcdijklmon";
+        char[] ch = str.toCharArray();
+        int[] freq = new int[26];
+        for(char c : ch){
+            freq[c - 'a']++;
+        }
+
+        System.out.println("Freq of each character is\n"+Arrays.toString(freq));
+    }
+}
