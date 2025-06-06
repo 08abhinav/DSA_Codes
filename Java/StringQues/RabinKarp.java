2b16a71a267b7e0c87db2393a47974ecad249bc3
08abhinav <abhinavnegi101@gmail.com>
2025-06-06 10:50:24 +0530

    implemented rabin karp algod

--- Java/StringQues/RabinKarp.java
+++ Java/StringQues/RabinKarp.java
@@ -8,11 +8,57 @@
 
     Cons
     -> Hash collisions can cause false positives → needs verification.
-    
+
     Real World use case:
     -> Plagiarism detection
     -> Searching DNA sequences
  */
 public class RabinKarp {
-    
+    static final int d = 256; 
+
+    static void rabinKarpSearch(String pattern, String text, int q){
+        int m = pattern.length();
+        int n = text.length();
+        int i, j, p = 0, t = 0, h = 1;
+
+        // Calculate h = pow(d, m-1) % q
+        for(i = 0; i < m - 1; i++) {
+            h = (h * d) % q;
+        }
+
+        for(i = 0; i < m; i++) {
+            p = (d * p + pattern.charAt(i)) % q;
+            t = (d * t + text.charAt(i)) % q;
+        }
+
+        
+        for(i = 0; i <= n - m; i++) {
+            if(p == t) {
+                for(j = 0; j < m; j++) {
+                    if(text.charAt(i + j) != pattern.charAt(j))
+                        break;
+                }
+
+                if(j == m) {
+                    System.out.println("Pattern found at index " + i);
+                }
+            }
+
+            if(i < n - m) {
+                t = (d * (t - text.charAt(i) * h) + text.charAt(i + m)) % q;
+
+                if(t < 0) {
+                    t = (t + q);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "ABCDDAEFGHI";
+        String pattern = "CDDA";
+        int q = 101; 
+
+        rabinKarpSearch(pattern, str, q);
+    }
 }
