6471b3828c4b76998e2d4c92d1efb7e25add4037
08abhinav <abhinavnegi101@gmail.com>
2025-06-07 00:21:50 +0530

    implemented java code to convert string to camelcase

---
+++ Java/StringQues/CamelCase.java
@@ -0,0 +1,20 @@
+public class CamelCase{
+    public static String convertToCamelCase(String s){
+        if(s == null) return "";
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(words[0]);
+        for(int i=1;i<words.length;i++){
+            if(!words[i].isEmpty()){
+                sb.append(Character.toUpperCase(words[i].charAt(0)))
+                .append(words[i].substring(1));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String...arg){
+        String str = "hello world this is java sample code";
+        System.out.println(CamelCase.convertToCamelCase(str));
+    }
+}
