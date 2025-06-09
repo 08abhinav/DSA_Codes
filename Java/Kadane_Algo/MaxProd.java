eff70c92b9b8335511e0859855d87aa47e2fd600
08abhinav <abhinavnegi101@gmail.com>
2025-06-09 22:26:49 +0530

    implemented calcProd method

--- Java/Kadane_Algo/MaxProd.java
+++ Java/Kadane_Algo/MaxProd.java
@@ -1,6 +1,22 @@
 class MaxProd{
-    public static calcProd(int[] nums){
-        
+    public static int calcProd(int[] nums){
+        if(nums.length == 0) return 0;
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int res = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            int currProd = nums[i];
+
+            int tempMax = Math.max(currProd, Math.max(currProd * maxProd, currProd * minProd));
+            int tempMin = Math.min(currProd, Math.min(currProd * maxProd, currProd * minProd));
+
+            maxProd = tempMax;
+            minProd = tempMin;
+
+            res = Math.max(res, maxProd);
+        }
+        return res;
     }
     public static void main(String[] args) {
         
