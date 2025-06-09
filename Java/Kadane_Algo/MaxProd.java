0923145c33597e3bb5f3f989ce6c4e018ef2feae
08abhinav <abhinavnegi101@gmail.com>
2025-06-09 22:28:54 +0530

    implementation done

--- Java/Kadane_Algo/MaxProd.java
+++ Java/Kadane_Algo/MaxProd.java
@@ -19,6 +19,7 @@ class MaxProd{
         return res;
     }
     public static void main(String[] args) {
-        
+        int[] nums = {5, 7, 8, 9, 11, 14};
+        System.out.println(MaxProd.calcProd(nums));
     }
 }
