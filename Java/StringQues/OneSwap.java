7f5baf35be4ae312f2c07e01ef6892c4795e81e6
08abhinav <abhinavnegi101@gmail.com>
2025-06-10 09:22:06 +0530

    countSwap method implemented

--- Java/StringQues/OneSwap.java
+++ Java/StringQues/OneSwap.java
@@ -2,7 +2,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class OneSwap {
+    public static int countSwap(String s){
+        Set<String> set = new HashSet<>();
+        char[] ch = s.toCharArray();
+        int n = ch.length;
 
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                char temp[] = s.toCharArray();
+
+                char c = temp[i];
+                temp[i] = temp[j];
+                temp[j] = c;
+
+                set.add(new String(temp));
+            }
+        }
+        return set.size();
+    }
     public static void main(String[] args) {
         
     }
