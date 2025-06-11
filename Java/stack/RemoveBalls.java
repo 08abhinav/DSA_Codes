69f29d1aad0f8b508fca39ed28a5d274df293763
08abhinav <abhinavnegi101@gmail.com>
2025-06-11 10:26:49 +0530

    added comment

--- Java/stack/RemoveBalls.java
+++ Java/stack/RemoveBalls.java
@@ -1,3 +1,13 @@
+/**You are given two arrays, color and radius, representing a sequence of balls:
+
+color[i] is the color of the i-th ball.
+
+radius[i] is the radius of the i-th ball.
+
+If two consecutive balls have the same color and radius, remove them both. Repeat this process until no more such pairs exist.
+
+
+Return the number of balls remaining after all possible removals. */
 import java.util.ArrayDeque;
 import java.util.Deque;
 
