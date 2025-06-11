6b742f8c7eb05e0350e2efb5ed427380ee6cfcfb
08abhinav <abhinavnegi101@gmail.com>
2025-06-11 09:42:38 +0530

    implementation done

--- Java/Hashmap/MaxFreqEle.java
+++ Java/Hashmap/MaxFreqEle.java
@@ -21,6 +21,7 @@ class MaxFreqEle{
         return count;
     }
     public static void main(String[] args) {
-        
+        int[] arr = {1, 2, 1, 3, 2, 4};
+        System.out.println(MaxFreqEle.maxFreq(arr));
     }
 }
