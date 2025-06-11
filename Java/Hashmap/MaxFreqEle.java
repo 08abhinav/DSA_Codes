b940c438ba1e3295369a8d3540f3195a6b05a7c4
08abhinav <abhinavnegi101@gmail.com>
2025-06-11 09:41:22 +0530

    implemented method maxFreq

--- Java/Hashmap/MaxFreqEle.java
+++ Java/Hashmap/MaxFreqEle.java
@@ -1,5 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 class MaxFreqEle{
-    
+    public static int maxFreq(int[] arr){
+        Map<Integer, Integer> freq = new HashMap<>();
+        int count = 0;
+        int max = 0;
+        for(int i: arr){
+            freq.put(i, freq.getOrDefault(i, 0)+1);
+        }
+        for(Entry<Integer, Integer> entry: freq.entrySet()){
+            if(entry.getValue() > max){
+                max = entry.getValue();
+                count = max;
+            }else if(entry.getValue() == max){
+                count += max;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         
     }
