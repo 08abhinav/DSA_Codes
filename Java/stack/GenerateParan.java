dddb7a544375b0c7ac7ae1ceb0bf8e4eb39a98c8
08abhinav <abhinavnegi101@gmail.com>
2025-06-11 18:31:07 +0530

    implemented generateParan method and dfs as a helper method

--- Java/stack/GenerateParan.java
+++ Java/stack/GenerateParan.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class GenerateParan{
+    public static List<String> generateParan(int n){
+        List<String> res = new ArrayList<>();
+        dfs(0, 0, "", n, res);
+        return res;
+    }
+    private static void dfs(int openP, int closeP, String s, int n, List<String> res){
+        if(openP == closeP && openP + closeP == n*2){
+            res.add(s);
+            return;
+        }
+
+        if(openP < n){
+            dfs(openP + 1, closeP, "(", n, res);
+        }
+
+        if(closeP < openP){
+            dfs(openP, closeP + 1, ")", n, res);
+        }
+    }
     public static void main(String[] args){
-        
     }
 }
