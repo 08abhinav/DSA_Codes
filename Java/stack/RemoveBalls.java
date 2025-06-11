9cfc972ebe39ddb9b5ead4cfbda03fabb2801c34
08abhinav <abhinavnegi101@gmail.com>
2025-06-11 10:24:16 +0530

    implemented findLen method

--- Java/stack/RemoveBalls.java
+++ Java/stack/RemoveBalls.java
@@ -1,4 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class RemoveBalls{
+    public static int findLen(int[] colors, int[] radius){
+        Deque<int[]> stack = new ArrayDeque<>();
+        
+        for(int i=0; i<colors.length; i++){
+            int[] curr = new int[]{colors[i], radius[i]};
+
+            if(!stack.isEmpty()){
+                int[] top = stack.peek();
+                if(top[0] == curr[0] && top[1] == curr[1]){
+                    stack.pop();
+                    continue;
+                }
+            }
+            stack.push(curr);
+        }
+        return stack.size();
+    }
     public static void main(String[] args) {
         
     }
