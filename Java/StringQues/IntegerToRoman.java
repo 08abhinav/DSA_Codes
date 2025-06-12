e1ac646893dababad555360aae31478992af6b5d
08abhinav <abhinavnegi101@gmail.com>
2025-06-12 09:54:51 +0530

    added comment

--- Java/StringQues/IntegerToRoman.java
+++ Java/StringQues/IntegerToRoman.java
@@ -1,3 +1,4 @@
+/**Integer to Roman conversion */
 public class IntegerToRoman {
     public static String integerToRoman(int num){
         final int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
