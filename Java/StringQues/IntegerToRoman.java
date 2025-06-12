3e08c4bd57ea5da45ae7be3d374bcd0568e018cb
08abhinav <abhinavnegi101@gmail.com>
2025-06-12 09:53:19 +0530

    implemented integerToRoman conversion

--- Java/StringQues/IntegerToRoman.java
+++ Java/StringQues/IntegerToRoman.java
@@ -1,5 +1,20 @@
 public class IntegerToRoman {
-    
+    public static String integerToRoman(int num){
+        final int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder s = new StringBuilder();
+        for(int i=0; i<nums.length; i++){
+            if(num == 0){
+                break;
+            }
+            while(num >= nums[i]){
+                s.append(symbols[i]);
+                num -= nums[i];
+            }
+        }
+        return s.toString();
+    }
     public static void main(String[] args) {
         
     }
