8a8a37681cb340c5b5968bb5bf457608e9a7d5e0
08abhinav <abhinavnegi101@gmail.com>
2025-06-12 09:11:21 +0530

    implemented maxDifference method

--- Java/Array/MaxDiff.java
+++ Java/Array/MaxDiff.java
@@ -1,4 +1,12 @@
 public class MaxDiff{
+    public static int maxDifference(int[] nums){
+        int maxDiff = 0;
+        for(int i=0; i<nums.length-1; i++){
+            maxDiff = Math.max(maxDiff, Math.abs(nums[i+1] - nums[i]));
+            maxDiff = Math.max(maxDiff, Math.abs(nums[nums.length-1] - nums[0]));
+        }
+        return maxDiff;
+    }
     public static void main(String[] args) {
         
     }
