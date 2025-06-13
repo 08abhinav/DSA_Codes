a25a70c32eb3954ffbba5f307f67f5cd9e666223
08abhinav <abhinavnegi101@gmail.com>
2025-06-13 22:34:45 +0530

    added comment

--- Java/StringQues/AnagramGroup.java
+++ Java/StringQues/AnagramGroup.java
@@ -1,3 +1,4 @@
+/**Given an array of strings strs, group the anagrams together. You can return the answer in any order. */
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
