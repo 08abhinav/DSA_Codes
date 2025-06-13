e187233fd4471fedf8f7c2b31471d95a7babdd99
08abhinav <abhinavnegi101@gmail.com>
2025-06-13 22:32:46 +0530

    implemented groudAna method

--- Java/StringQues/AnagramGroup.java
+++ Java/StringQues/AnagramGroup.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class AnagramGroup{
+    static List<List<String>> groupAna(String[] str){
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s: str){
+            char ch[] = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
     public static void main(String[] args) {
         
     }
