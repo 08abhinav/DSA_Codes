89bdc2d9ecd4370e2920c60085b98ce59c059794
08abhinav <abhinavnegi101@gmail.com>
2025-06-14 12:20:52 +0530

    implemented maxDiff method

--- Java/Greedy/RemapDigit.java
+++ Java/Greedy/RemapDigit.java
@@ -1,4 +1,34 @@
 public class RemapDigit{
+    public static int maxDiff(int num){
+        char[] maxChars = String.valueOf(num).toCharArray();
+        char[] minChars = String.valueOf(num).toCharArray();
+
+        char replaceMaxDigit = ' ';
+        for(char c: maxChars){
+            if(c != '9'){
+                replaceMaxDigit = c;
+                break;
+            }
+        }
+
+        for(int i=0; i<maxChars.length; i++){
+            if(maxChars[i] == replaceMaxDigit){
+                maxChars[i] = '9';
+            }
+        }
+
+        char replaceMinDigit = minChars[0];
+        for(int i=0; i<minChars.length; i++){
+            if(minChars[i] == replaceMinDigit){
+                minChars[i] = '0';
+            }
+        }
+
+        int maxVal = Integer.parseInt(new String(maxChars));
+        int minVal = Integer.parseInt(new String(minChars));
+
+        return maxVal - minVal;
+    }
     public static void main(String[] args) {
         
     }
