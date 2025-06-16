cce009c5fc4e9bb4cd140101029b4ff0a2c03f38
08abhinav <abhinavnegi101@gmail.com>
2025-06-16 19:03:51 +0530

    implemented alternate method too

--- Java/Array/MaxDiff2.java
+++ Java/Array/MaxDiff2.java
@@ -1,14 +1,27 @@
 public class MaxDiff2{
     public static int maxDiff(int[] arr){
         int maxValue = 0;
-        int minValue = arr[0];
-        for(int i=1; i<arr.length; i++){
-            if(arr[i] > minValue){
-                maxValue = Math.max(maxValue, arr[i] - minValue);
+        // method 1
+        // int minValue = arr[0];
+        // for(int i=1; i<arr.length; i++){
+        //     if(arr[i] > minValue){
+        //         maxValue = Math.max(maxValue, arr[i] - minValue);
+        //     }
+        //     minValue = Math.min(minValue, arr[i]);
+        // }
+        // return maxValue;
+
+        // method 2
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] < arr[j]){
+                    maxValue = Math.max(maxValue, arr[j] - arr[i]);
+                }else{
+                    continue;
+                }
             }
-            minValue = Math.min(minValue, arr[i]);
         }
-        return maxValue;
+        return maxValue == 0 ? -1 : maxValue;
     }
     public static void main(String[] args){
         int[] arr = {1, 2, 3, 4};
