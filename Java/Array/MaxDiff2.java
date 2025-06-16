54320679c37a4aa141424f7370c738a299943da9
08abhinav <abhinavnegi101@gmail.com>
2025-06-16 19:04:25 +0530

    added comment

--- Java/Array/MaxDiff2.java
+++ Java/Array/MaxDiff2.java
@@ -1,3 +1,6 @@
+/**Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+
+Return the maximum difference. If no such i and j exists, return -1. */
 public class MaxDiff2{
     public static int maxDiff(int[] arr){
         int maxValue = 0;
