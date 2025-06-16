127e30e624d5d052d0c030fefa1c4d1408af2ce7
08abhinav <abhinavnegi101@gmail.com>
2025-06-16 19:25:30 +0530

    implemented freqSort method

--- Java/Array/FreqSort.java
+++ Java/Array/FreqSort.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class FreqSort{
+    public int[] freqSort(int[] arr){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: arr){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+        return Arrays.stream(arr)
+            .boxed()
+            .sorted((a, b)-> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : (b-a))
+            .mapToInt(n -> n)
+            .toArray();
+    }
     public static void main(String... args){
-        
+
     }
 }
