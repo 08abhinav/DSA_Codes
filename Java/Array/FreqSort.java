54746ff035663e024e8c507f1a43e3cf016bbf19
08abhinav <abhinavnegi101@gmail.com>
2025-06-16 19:28:14 +0530

    added comment

--- Java/Array/FreqSort.java
+++ Java/Array/FreqSort.java
@@ -1,3 +1,6 @@
+/**Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+Return the sorted array. */
 import java.util.Arrays;
 import java.util.HashMap;
 
