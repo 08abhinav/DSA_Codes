4667ba6fa1b236962edc63a3e8da24484f82a42f
08abhinav <abhinavnegi101@gmail.com>
2025-06-18 21:09:21 +0530

    implemented 2nd solution too

--- Java/Array/DivideArray.java
+++ Java/Array/DivideArray.java
@@ -1,17 +1,40 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class DivideArray{
     public static int[][] minDiff(int[] nums, int k){
-        Arrays.sort(nums);
+        // Solution 1
+        // Arrays.sort(nums);
+        // List<int[]> res = new ArrayList<>();
+        // for(int i=0; i<nums.length; i+=3){
+        //     if(nums[i+2] - nums[i] > k){
+        //         return new int[0][];
+        //     }
+        //     res.add(new int[] {nums[i], nums[i+1], nums[i+2]});
+        // }
+        // return res.toArray(new int[res.size()][]);
+
+        // Solution 2
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num : nums){
+            pq.offer(num);
+        }
+
         List<int[]> res = new ArrayList<>();
-        for(int i=0; i<nums.length; i+=3){
-            if(nums[i+2] - nums[i] > k){
+        while(pq.size() >= 3){
+            int low = pq.poll();
+            int mid = pq.poll();
+            int high = pq.poll();
+
+            if(high - low <= k){
+                res.add(new int[]{low, mid, high});
+            }else{
                 return new int[0][];
             }
-            res.add(new int[] {nums[i], nums[i+1], nums[i+2]});
         }
+
         return res.toArray(new int[res.size()][]);
     }
     public static void main(String[] args) {
