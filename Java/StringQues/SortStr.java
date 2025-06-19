1bae81dabb08bfb0d6b7ab5e063a6d4d62947e28
08abhinav <abhinavnegi101@gmail.com>
2025-06-19 16:38:19 +0530

    implemented caseSort method

--- Java/StringQues/SortStr.java
+++ Java/StringQues/SortStr.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Collections;
+import java.util.List;
+
 public class SortStr{
+    public static String caseSort(String s){
+        List<Character> upper = new ArrayList();
+        List<Character> lower = new ArrayList();
+
+        for(char c : s.toCharArray()){
+            if(Character.isUpperCase(c)){
+                upper.add(c);
+            }else{
+                lower.add(c);
+            }
+        }
+
+        Collections.sort(upper);
+        Collections.sort(lower);
+
+        StringBuilder res = new StringBuilder();
+        int upperIdx = 0, lowerIdx = 0;
+        for(char ch : s.toCharArray()){
+            if(Character.isUpperCase(ch)){
+                res.append(upper.get(upperIdx++));
+            }else{
+                res.append(lower.get(lowerIdx++));
+            }
+        }
+        return res.toString();
+    }
     public static void main(String[] args){
-        
+
     }
 }
