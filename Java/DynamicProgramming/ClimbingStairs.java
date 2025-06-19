b5f683e0fcbaa0be5ace758258f0400a7ae54d5e
08abhinav <abhinavnegi101@gmail.com>
2025-06-19 23:21:21 +0530

    added comment

--- Java/DynamicProgramming/ClimbingStairs.java
+++ Java/DynamicProgramming/ClimbingStairs.java
@@ -1,3 +1,7 @@
+/**You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
 public class ClimbingStairs {
     public static int countSteps(int n){
         if(n <= 3) return n;
