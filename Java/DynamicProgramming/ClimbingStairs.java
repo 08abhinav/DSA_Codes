1d016878a06cc354f7a6cf11a0adf754b146f0f7
08abhinav <abhinavnegi101@gmail.com>
2025-06-19 23:14:14 +0530

    implemented countStep method

--- Java/DynamicProgramming/ClimbingStairs.java
+++ Java/DynamicProgramming/ClimbingStairs.java
@@ -1,4 +1,15 @@
 public class ClimbingStairs {
+    public static int countSteps(int n){
+        if(n <= 3) return n;
+
+        int prev1 = 3, prev2 = 2, curr = 0;
+        for(int i=0; i<n; i++){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
     public static void main(String[] args) {
         
     }
