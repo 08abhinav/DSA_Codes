1a430a34be0f0cdd211c6a9133c5fec684888dbd
08abhinav <abhinavnegi101@gmail.com>
2025-06-20 22:24:30 +0530

    implemented groupBall method

--- Java/Greedy/GroupBalls.java
+++ Java/Greedy/GroupBalls.java
@@ -1,5 +1,24 @@
 public class GroupBalls {
-    
+    public static boolean groupBalls(int[] arr, int k){
+        if(arr.length % k != 0) return false;
+
+        int max = 0;
+        for(int i : arr) max = Math.max(max, i);
+
+        int[] freq = new int[max + k + 1];
+        for(int i : arr) freq[i]++;
+
+        for(int i=0; i<=max; i++){
+            if(freq[i] > 0){
+                int count = freq[i];
+                for(int j=0; j<k; j++){
+                    if(freq[i + j] < count) return false;
+                    freq[i+j] -= count;
+                }
+            }
+        }
+        return true;
+    }
     public static void main(String[] args){
 
     }
