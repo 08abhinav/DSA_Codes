591bb2f3f7b31c78547be268d822c66db9157454
08abhinav <abhinavnegi101@gmail.com>
2025-06-21 14:28:24 +0530

    implemented catchThief method

--- Java/Greedy/PolThief.java
+++ Java/Greedy/PolThief.java
@@ -1,4 +1,27 @@
 public class PolThief{
+    public static int catchThief(int[] arr, int k){
+        int n = arr.length;
+        int i = 0, j = 0, count = 0;
+        while(i < n && j < n){
+            while( i < n && arr[i] != 'P') i++;
+            while( j < n && arr[j] != 'T') j++;
+
+            if( i < n && j < n && Math.abs(i - j) <= k){
+                count++;
+                i++;
+                j++;
+            }
+
+            else if(i < n && i < j){
+                i++;
+            }
+
+            else if(j < n && j < i){
+                j++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         
     }
