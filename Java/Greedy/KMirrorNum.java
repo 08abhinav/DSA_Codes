f801fcf40413210e41a4d508a1ba6019eefe012b
08abhinav <abhinavnegi101@gmail.com>
2025-06-23 11:20:02 +0530

    implemented helper method

--- Java/Greedy/KMirrorNum.java
+++ Java/Greedy/KMirrorNum.java
@@ -1,5 +1,18 @@
 public class KMirrorNum {
+    public static long kMirror(int k, int n){
+
+    }
+
+    long createPalindrome(long num, boolean odd){
+        long x = num;
+        if(odd) x /= 10;
+        while( x > 0){
+            num = num * 10 + x % 10;
+            x /= 10;
+        }
+        return num;
+    }
     public static void main(String...args){
-        
+
     }
 }
