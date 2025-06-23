7e6587d041691f774ab143d9be6719d547cbda62
08abhinav <abhinavnegi101@gmail.com>
2025-06-23 11:23:50 +0530

    implemented kMirror method

--- Java/Greedy/KMirrorNum.java
+++ Java/Greedy/KMirrorNum.java
@@ -1,9 +1,20 @@
 public class KMirrorNum {
     public static long kMirror(int k, int n){
-
+        long sum = 0;
+        for(long len=1; n>0; len*=10){
+            for(long i=len; n>0 && i<len*10; i++){
+                long p = createPalindrome(i, true);
+                if(isPalindrome(p, k)){ sum += p; n--;}
+            }
+            for(long i=len; n>0 && i<len*10; i++){
+                long p = createPalindrome(i, false);
+                if(isPalindrome(p, k)){sum += p; n--;}
+            }
+        }
+        return sum;
     }
 
-    long createPalindrome(long num, boolean odd){
+    private static long createPalindrome(long num, boolean odd){
         long x = num;
         if(odd) x /= 10;
         while( x > 0){
@@ -13,7 +24,7 @@ public class KMirrorNum {
         return num;
     }
 
-    boolean isPalindrome(long num, int base){
+    private static boolean isPalindrome(long num, int base){
         StringBuilder sb = new StringBuilder();
         while(num > 0){
             sb.append((char) (num % base + '0'));
@@ -24,7 +35,7 @@ public class KMirrorNum {
         while(i < j) if(s.charAt(i++) != s.charAt(j--))return false;
         return true;
     }
-    
+
     public static void main(String...args){
 
     }
