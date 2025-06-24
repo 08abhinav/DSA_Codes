ad07b91e7b2830e4f0ceba2ea6f32b90018bd01f
08abhinav <abhinavnegi101@gmail.com>
2025-06-24 17:48:59 +0530

    implementation done

--- Java/StringQues/MaxSubSeq.java
+++ Java/StringQues/MaxSubSeq.java
@@ -5,13 +5,16 @@ public class MaxSubSeq {
     public static String maxSubSeq(String s, int k){
         int n = s.length();
         Deque<Character> stk = new ArrayDeque<>();
-        for(int i=0; i<n; i++){
+
+        for(int i = 0; i < n; i++){
             char c = s.charAt(i);
-            while(!stk.isEmpty() && k > 0 && stk.pollLast() < c){
+            while(!stk.isEmpty() && k > 0 && stk.peekLast() < c){
                 stk.pollLast();
                 k--;
             }
+            stk.addLast(c); 
         }
+
         while(k-- > 0){
             stk.pollLast();
         }
@@ -23,6 +26,8 @@ public class MaxSubSeq {
         return sb.toString();
     }
     public static void main(String[] args) {
-        
+        String s = "ritz";
+        int k = 2;
+        System.out.println(MaxSubSeq.maxSubSeq(s, k));
     }
 }
