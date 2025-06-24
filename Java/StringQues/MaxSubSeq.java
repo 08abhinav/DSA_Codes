5f37456c3b14d7bcfb8651caa69e1b544cc6939a
08abhinav <abhinavnegi101@gmail.com>
2025-06-24 17:50:04 +0530

    added comment

--- Java/StringQues/MaxSubSeq.java
+++ Java/StringQues/MaxSubSeq.java
@@ -1,3 +1,7 @@
+/**Given a string s consisting of lowercase English letters and an integer k, 
+your task is to remove exactly k characters from the string. 
+ * The resulting string must be the largest possible in lexicographical  order, 
+while maintain the relative order of the remaining characters. */
 import java.util.ArrayDeque;
 import java.util.Deque;
 
