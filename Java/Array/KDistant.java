08bc0a3837855faa11f93c30b26c9f08fa602a10
08abhinav <abhinavnegi101@gmail.com>
2025-06-24 17:29:02 +0530

    implemented findDistant method

--- Java/Array/KDistant.java
+++ Java/Array/KDistant.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class KDistant{
+    public static List<Integer> findDistantIndices(int[] arr, int key, int k){
+        List<Integer> li = new ArrayList<>();
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(arr[j] == key && Math.abs(i - j) == k){
+                    li.add(i);
+                    break;
+                }
+            }
+        }
+        return li;
+    }
     public static void main(String[] args) {
         
     }
