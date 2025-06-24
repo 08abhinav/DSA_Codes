bd12569d7cb3468917cf45a8a88c972bfb4414a5
08abhinav <abhinavnegi101@gmail.com>
2025-06-24 17:30:36 +0530

    implementation done

--- Java/Array/KDistant.java
+++ Java/Array/KDistant.java
@@ -7,7 +7,7 @@ public class KDistant{
         int n = arr.length;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(arr[j] == key && Math.abs(i - j) == k){
+                if(arr[j] == key && Math.abs(i - j) <= k){
                     li.add(i);
                     break;
                 }
@@ -16,6 +16,8 @@ public class KDistant{
         return li;
     }
     public static void main(String[] args) {
-        
+        int[] arr = {3,4,9,1,3,9,5};
+        int key = 9, k = 1;
+        System.out.println(KDistant.findDistantIndices(arr, key, k));
     }
 }
