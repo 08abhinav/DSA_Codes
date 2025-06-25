0a4453040dbd75efe581dc539ac1b9b4e9723e2f
08abhinav <abhinavnegi101@gmail.com>
2025-06-25 14:20:58 +0530

    implemented smalledProduct method

--- Java/Array/SmallProduct.java
+++ Java/Array/SmallProduct.java
@@ -1,4 +1,18 @@
 public class SmallProduct{
+    public static long smallestProduct(int[] nums1, int[] nums2, int k){
+        long left = -10000000000L;
+        long right = 10000000000L;
+
+        while(left < right){
+            long mid = left + (right - left) / 2;
+            if(countProduct(nums1, nums2, mid) < k){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
     public static void main(String[] args) {
         
     }
