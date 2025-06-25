61df380b460bc056e5781e814cd3184375ad788d
08abhinav <abhinavnegi101@gmail.com>
2025-06-25 14:24:20 +0530

    implemented countProduct method

--- Java/Array/SmallProduct.java
+++ Java/Array/SmallProduct.java
@@ -13,6 +13,31 @@ public class SmallProduct{
         }
         return left;
     }
+
+    private static long countProduct(int[] n1, int[] n2, long target){
+        long count = 0;
+        for(int n : n1){
+            if(n == 0){
+                if(target >= 0) count += n2.length;
+                continue;
+            }
+
+            int low = 0, high = n2.length;
+            while(low < high){
+                int mid = low + (high - low) / 2;
+                long product = (long) n * n2[mid];
+                if(product <= target){
+                    if(n > 0) low = mid + 1;
+                    else high = mid;
+                }else{
+                    if(n > 0) high = mid;
+                    else low = mid + 1;
+                }
+            }
+            count += (n > 0) ? low : n2.length - low;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         
     }
