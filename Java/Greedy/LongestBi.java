1ee6d1e9560c2b974a855b9bff1062d1927fc0fb
08abhinav <abhinavnegi101@gmail.com>
2025-06-26 14:54:37 +0530

    implemented longestSubsequence method

--- Java/Greedy/LongestBi.java
+++ Java/Greedy/LongestBi.java
@@ -1,5 +1,27 @@
 public class LongestBi {
+    public static int longestSubsequence(String s, int k){
+        int n = s.length();
+        int cnt = 0;
+        int power = 0;
+        int val = 0;
+        for(int i=n-1; i>=0; i--){
+            char c = s.charAt(i);
+            if(c == '1'){
+                cnt++;
+            }else{
+                if(power < 32){
+                    long add = 1L << power;
+                    if(val + add <= k){
+                        val += add;
+                        cnt++;
+                    }
+                }
+            }
+            power++;
+        }
+        return cnt;
+    }
     public static void main(String... args){
-        
+
     }
 }
