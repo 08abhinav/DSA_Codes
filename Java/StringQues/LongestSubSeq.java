5b147d349f0aaeefb5ad2af1211e5fbf81ca27b1
08abhinav <abhinavnegi101@gmail.com>
2025-06-27 14:00:16 +0530

    implemented longestSubsequenceRepeatedK method

--- Java/StringQues/LongestSubSeq.java
+++ Java/StringQues/LongestSubSeq.java
@@ -1,5 +1,22 @@
-public class LongestSubSeq{
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class LongestSubSeq{
+    public String longestSubsequenceRepeatedK(String s, int k) {
+        String r="";
+        Queue<String> q=new LinkedList<>();
+        for(q.add("");!q.isEmpty();) {
+            String c=q.poll();
+            for(char ch='a';ch<='z';ch++) {
+                String n=c+ch;
+                if(isK(n,s,k)) {
+                    r=n;
+                    q.add(n);
+                }
+            }
+        }
+        return r;
+    }
     public static void main(String... args){
 
     }
