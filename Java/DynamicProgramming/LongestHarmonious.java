5b31524ed7333ce00d3dcd9bf21d4f66e6627fd5
08abhinav <abhinavnegi101@gmail.com>
2025-06-30 11:07:37 +0530

    implemented longestHarmoniousSubsequence method

--- Java/DynamicProgramming/LongestHarmonious.java
+++ Java/DynamicProgramming/LongestHarmonious.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class LongestHarmonious{
+    public static int longestHarmoniousSubsequence(int[] nums){
+        Arrays.sort(nums);
+        int j = 0, maxLength = 0;
+        for(int i=0; i<nums.length; i++){
+            while(nums[i] - nums[j] > 1){
+                j++;
+            }
+            if(nums[i] - nums[j] == 1){
+                maxLength = Math.max(maxLength, i - j + 1);
+            }
+        }
+        return maxLength;
+    }
     public static void main(String... args){
-        
+
     }
 }
