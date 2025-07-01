d3386e403241764afba8b57bde33a3c9c1910ea7
08abhinav <abhinavnegi101@gmail.com>
2025-07-01 10:19:48 +0530

    implementing original string typed question

--- Java/StringQues/OriginalString.java
+++ Java/StringQues/OriginalString.java
@@ -1,4 +1,11 @@
 public class OriginalString{
+    public static int possibleStringCount(String word){
+        int n = word.length(), count = 1;
+        for(int i=0; i<n-1; i++){
+            if(word[i] == word[i+1]) count++;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         
     }
