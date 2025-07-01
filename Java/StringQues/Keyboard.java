a9d637da5ce874afebbd6630dc12d44cd6bd1457
08abhinav <abhinavnegi101@gmail.com>
2025-07-01 10:30:45 +0530

    implemented findWords method

--- Java/StringQues/Keyboard.java
+++ Java/StringQues/Keyboard.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+
 public class Keyboard {
+    public static String[] findWords(String words[]) {
+        ArrayList<String> res = new ArrayList<>();
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+        for(String i : words){
+            if(isInRow(i, row1) || isInRow(i, row2) || isIntRow(i, row3)){
+                res.add(i);
+            }
+        }
+        return res.toArray(new String[0]);
+    }
     public static void main(String[] args) {
         
     }
