6c6832ef48549007294a872761887211595301a1
08abhinav <abhinavnegi101@gmail.com>
2025-07-01 10:22:50 +0530

    resolve the syntax error

--- Java/StringQues/OriginalString.java
+++ Java/StringQues/OriginalString.java
@@ -2,7 +2,7 @@ public class OriginalString{
     public static int possibleStringCount(String word){
         int n = word.length(), count = 1;
         for(int i=0; i<n-1; i++){
-            if(word[i] == word[i+1]) count++;
+            if(word.charAt(i) == word.charAt(i+1)) count++;
         }
         return count;
     }
