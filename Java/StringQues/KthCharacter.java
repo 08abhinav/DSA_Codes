ebac2a0fde59cb534cc62bf9ba35d661be41607d
08abhinav <abhinavnegi101@gmail.com>
2025-07-03 10:40:41 +0530

    implemented kthchar method

--- Java/StringQues/KthCharacter.java
+++ Java/StringQues/KthCharacter.java
@@ -1,4 +1,14 @@
 public class KthCharacter{
+    public static char kthchar(int k){
+        StringBuilder sb = new StringBuilder("a");
+        while(sb.length() < k){
+            int size = sb.length();
+            for(int i=0; i<size; i++){
+                sb.append((char) ('a' - ((sb.charAt(i) - 'a') + 1) % 26));
+            }
+        }
+        return (sb.charAt(k - 1));
+    }
     public static void main(String[] args) {
         
     }
