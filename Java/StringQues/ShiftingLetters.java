424c128351f202751c992f1b6314b9b43ec25105
08abhinav <abhinavnegi101@gmail.com>
2025-07-03 11:08:36 +0530

    implemented shiftLetters method

--- Java/StringQues/ShiftingLetters.java
+++ Java/StringQues/ShiftingLetters.java
@@ -1,4 +1,13 @@
 public class ShiftingLetters{
+    public static String shifitLetters(String s, int[] shifts){
+        int n = s.length(), totalShifts = 0;
+        char[] ch = s.toCharArray();
+        for(int i=n-1; i>=0; i--){
+            totalShifts = (totalShifts + shifts[i]) % 26;
+            ch[i] = (char)((ch[i] - 'a' + totalShifts) % 26 + 'a');
+        }
+        return new String(ch);
+    }
     public static void main(String[] args) {
         
     }
