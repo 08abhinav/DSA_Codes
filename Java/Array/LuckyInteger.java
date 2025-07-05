fcd60ef4108c78deb588b542c2db6854f9b8df35
08abhinav <abhinavnegi101@gmail.com>
2025-07-05 11:06:40 +0530

    implemented LuckyInteger method

--- Java/Array/LuckyInteger.java
+++ Java/Array/LuckyInteger.java
@@ -1,4 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LuckyInteger{
+    public static int findLucky(int[] arr){
+        Map<Integer, Integer> freq = new HashMap<>();
+        int luckyInt = -1;
+        for(int i : arr){
+            freq.put(i, freq.getOrDefault(freq, 0) + 1);
+        }
+
+        for(int key : freq.keySet()){
+            if(freq.get(key) == key){
+                luckyInt = key;
+            }
+        }
+        return luckyInt;
+    }
     public static void main(String[] args) {
         
     }
