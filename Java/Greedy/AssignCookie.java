3272a5a788e4ed3549374c703522ece960309a6a
08abhinav <abhinavnegi101@gmail.com>
2025-07-13 14:37:18 +0530

    implemented findContentChildren method

--- Java/Greedy/AssignCookie.java
+++ Java/Greedy/AssignCookie.java
@@ -1,4 +1,23 @@
+import java.util.Arrays;
+
 public class AssignCookie {
+    public static int findContentChildren(int[] g, int[] s){
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0, cookie = 0, happy = 0;
+
+        while(child < g.length && cookie < s.length){
+            if(s[cookie] >= g[child]){
+                happy++;
+                child++;
+                cookie++;
+            }else{
+                cookie++;
+            }
+        }
+        return happy;
+    }
     public static void main(String[] args) {
         
     }
