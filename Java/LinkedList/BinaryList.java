bbb9e3b9e08fe3f141b7a504e1d864faad50f0ad
08abhinav <abhinavnegi101@gmail.com>
2025-07-14 11:23:49 +0530

    implemented getDecimalValue method

--- Java/LinkedList/BinaryList.java
+++ Java/LinkedList/BinaryList.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class BinaryList {
-    public static void main(String... args){
-        
+    public static int getDecimalValue(L1.Node head){
+        L1.Node temp = head;
+        StringBuilder sb = new StringBuilder();
+        while(temp != null){
+            sb.append(temp.data);
+            temp = temp.next;
+        }
+        return Integer.parseInt(sb.toString(), 2);
+    }
+    public static void main(String... args) {
+       
     }
 }
