908c6c1843a7a83b9c06268aee7d9841455d752e
08abhinav <abhinavnegi101@gmail.com>
2025-07-14 11:24:18 +0530

    implementation done

--- Java/LinkedList/BinaryList.java
+++ Java/LinkedList/BinaryList.java
@@ -11,6 +11,36 @@ public class BinaryList {
         return Integer.parseInt(sb.toString(), 2);
     }
     public static void main(String... args) {
-       
+        Scanner sc = new Scanner(System.in);
+        L1 list = new L1(); 
+
+        int ch;
+        do {
+            System.out.print("1. Insert binary values\n2. Convert binary to decimal\n3. Exit\nEnter your choice: ");
+            ch = sc.nextInt();
+
+            switch (ch) {
+                case 1:
+                    System.out.print("Enter binary digit (0 or 1): ");
+                    int x = sc.nextInt();
+                    if (x == 0 || x == 1) {
+                        list.insert(x);
+                    } else {
+                        System.out.println("Only binary digits allowed!");
+                    }
+                    break;
+                case 2:
+                    int result = getDecimalValue(list.getHead());
+                    System.out.println("Decimal value: " + result);
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        } while (ch != 3);
+
+        sc.close();
     }
 }
