e8ed6a12dfacf35905f05da7528cb0de78ff3c22
08abhinav <abhinavnegi101@gmail.com>
2025-07-16 10:33:42 +0530

    implemented swap method

--- Java/Array/Sort0s1s2s.java
+++ Java/Array/Sort0s1s2s.java
@@ -51,6 +51,11 @@ public class Sort0s1s2s {
         }
     }
     
+    public static void swap(int[] arr, int i ,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
     
     public static void main(String... args){
         int[] arr = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
