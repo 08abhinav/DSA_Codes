931c40170a3edc2e117a2baeccfe8ae43fcb089f
08abhinav <abhinavnegi101@gmail.com>
2025-07-16 10:25:08 +0530

    implemeneted Sort0s1s2s method

--- Java/Array/Sort0s1s2s.java
+++ Java/Array/Sort0s1s2s.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class Sort0s1s2s {
+    public static void sort012(int[] arr){
+        int n = arr.length;
+        int c0 = 0, c1 = 0, c2 = 0, idx = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == 0) c0++;
+            else if(arr[i] == 1) c1++;
+            else c2++;
+        }
+
+        for(int i=0; i<c0; i++){
+            arr[idx++] = 0;
+        }
+
+        for(int i=0; i<c1; i++){
+            arr[idx++] = 1;
+        }
+
+        for(int i=0; i<c2; i++){
+            arr[idx++] = 2;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
     public static void main(String... args){
-        
+
     }
 }
