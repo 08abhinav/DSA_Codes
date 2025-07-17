78f35cf9a0b010eefc8fa5c0c04d93eb8ffa8fcb
08abhinav <abhinavnegi101@gmail.com>
2025-07-17 12:34:09 +0530

    implemented maxKPower and fact method

--- Java/Greedy/PowerOfK.java
+++ Java/Greedy/PowerOfK.java
@@ -1,5 +1,20 @@
 public class PowerOfK {
+    public static int fact(int num){
+        if(num == 0 || num == 1) return 1;
+
+        return num * fact(num - 1);
+    }
+    public static int maxKPower(int n, int k){
+        int factRes = fact(n);
+        int power = 0;
+
+        while(factRes % k == 0){
+            power++;
+            factRes /= k;
+        }
+        return power;
+    }
     public static void main(String...ar){
-        
+
     }
 }
