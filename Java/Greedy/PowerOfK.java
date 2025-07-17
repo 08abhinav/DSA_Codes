3b712183226870ea7db19f6721faca7e80bc546f
08abhinav <abhinavnegi101@gmail.com>
2025-07-17 12:44:13 +0530

    added comment

--- Java/Greedy/PowerOfK.java
+++ Java/Greedy/PowerOfK.java
@@ -1,4 +1,7 @@
+/**Given two positive integers n and k, determine the highest value of x such that 
+ * kx divides n! (n factorial) completely (i.e., n % (kx) == 0). */
 import java.util.ArrayList;
+
 
 public class PowerOfK {
     // Approach 1
