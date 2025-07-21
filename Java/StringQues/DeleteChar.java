d69546332c03d14cae7eba85b4c90898ac7ef7fe
08abhinav <abhinavnegi101@gmail.com>
2025-07-21 09:14:20 +0530

    implementation done

--- Java/StringQues/DeleteChar.java
+++ Java/StringQues/DeleteChar.java
@@ -4,19 +4,20 @@ public class DeleteChar{
         int count = 1;
         sb.append(s.charAt(0));
 
-        for(int i=1; i<s.length()-1; i++){
+        for(int i = 1; i < s.length(); i++) {
             if(s.charAt(i) == s.charAt(i - 1)){
                 count++;
-            }else{
+            } else {
                 count = 1;
             }
-            if(s.charAt(i) <= 2){
+            if(count <= 2){
                 sb.append(s.charAt(i));
             }
         }
         return sb.toString();
     }
     public static void main(String[] args) {
-        
+        String s = "Leeetcode";
+        System.out.println(DeleteChar.makeFancyString(s));
     }
 }
