def0eb08273d5cb2679c3d24a0ad1909ea7e91c3
08abhinav <abhinavnegi101@gmail.com>
2025-07-21 09:14:50 +0530

    added comment

--- Java/StringQues/DeleteChar.java
+++ Java/StringQues/DeleteChar.java
@@ -1,3 +1,8 @@
+/**A fancy string is a string where no three consecutive characters are equal.
+
+Given a string s, delete the minimum possible number of characters from s to make it fancy.
+
+Return the final string after the deletion. It can be shown that the answer will always be unique. */
 public class DeleteChar{
     public static String makeFancyString(String s){
         StringBuilder sb = new StringBuilder();
