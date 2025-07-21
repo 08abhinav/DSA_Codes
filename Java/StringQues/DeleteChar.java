4cb1cf09e795a7dcdc15da53f8e64ed7b27ec878
08abhinav <abhinavnegi101@gmail.com>
2025-07-21 09:12:29 +0530

    implemented makeFancyString method

--- Java/StringQues/DeleteChar.java
+++ Java/StringQues/DeleteChar.java
@@ -1,4 +1,21 @@
 public class DeleteChar{
+    public static String makeFancyString(String s){
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        sb.append(s.charAt(0));
+
+        for(int i=1; i<s.length()-1; i++){
+            if(s.charAt(i) == s.charAt(i - 1)){
+                count++;
+            }else{
+                count = 1;
+            }
+            if(s.charAt(i) <= 2){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
         
     }
