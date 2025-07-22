62dd2b7ca6097a821d3f6ee3241436a8895be0c9
08abhinav <abhinavnegi101@gmail.com>
2025-07-22 09:48:12 +0530

    implemented ErasureValue method

--- Java/Array/ErasureValue.java
+++ Java/Array/ErasureValue.java
@@ -1,5 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ErasureValue {
+    public static int maxUniqueSubarray(int[]nums){
+        Set<Integer> set = new HashSet<>();
+        int right = 0, left = 0;
+        int maxSum = 0, currSum = 0;
+
+        while(right < nums.length){
+            if(!set.contains(nums[right])){
+                set.add(nums[right]);
+                currSum += nums[right];
+                maxSum = Math.max(maxSum, currSum);
+                right++;
+            }else{
+                set.remove(nums[left]);
+                currSum -= nums[left];
+                left++;
+            }
+        }
+        return maxSum;
+    }
     public static void main(String...args){
-        
+
     }
 }
