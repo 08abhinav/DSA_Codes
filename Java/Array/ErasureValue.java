8139a48268f7cb021de0dc51643b87589b9ceb23
08abhinav <abhinavnegi101@gmail.com>
2025-07-22 09:54:41 +0530

    added comment

--- Java/Array/ErasureValue.java
+++ Java/Array/ErasureValue.java
@@ -1,3 +1,10 @@
+/**You are given an array of positive integers nums and want to erase a subarray containing unique elements. 
+ * The score you get by erasing the subarray is equal to the sum of its elements.
+
+Return the maximum score you can get by erasing exactly one subarray.
+
+An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, 
+if it is equal to a[l],a[l+1],...,a[r] for some (l,r). */
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,7 +14,6 @@ public class ErasureValue {
     //     Set<Integer> set = new HashSet<>();
     //     int right = 0, left = 0;
     //     int maxSum = 0, currSum = 0;
-
     //     while(right < nums.length){
     //         if(!set.contains(nums[right])){
     //             set.add(nums[right]);
