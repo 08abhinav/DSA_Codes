9d8a2061dd1cfcf3f68b1f943ad4d6212d72b753
08abhinav <abhinavnegi101@gmail.com>
2025-07-22 21:18:47 +0530

    impelmentation done

--- Golang/main/stk2.go
+++ Golang/main/stk2.go
@@ -8,11 +8,42 @@ type stack struct{
 	arr [MAX]int
 }
 
+func (s *stack) push(){
+	if s.top == MAX-1{
+		fmt.Println("Stack overflow")
+		return
+	}
+	var x int
+	fmt.Println("Enter the value to push: ")
+	fmt.Scan(&x)
 
+	s.top++
+	s.arr[s.top] = x
+}
+
+func (s *stack) pop(){
+	if s.top == -1{
+		fmt.Println("Stack underflow")
+		return
+	}
+	fmt.Println("Popped element: ", s.arr[s.top])
+	s.top--
+}
+
+func (s *stack) display(){
+	if s.top == -1{
+		fmt.Println("Stack is empty")
+		return
+	}
+	fmt.Println("Elements in stack")
+	for i:=s.top; i>=0; i--{
+		fmt.Println(s.arr[i])
+	}
+}
 
 func main(){
 	var s stack
-	s.top = 1
+	s.top = -1
 
 	for{
 		var ch int
