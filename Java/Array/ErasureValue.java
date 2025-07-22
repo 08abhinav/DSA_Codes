5f3a091f8d50e5ad93a119193f20de2b775a27b4
08abhinav <abhinavnegi101@gmail.com>
2025-07-22 09:53:34 +0530

    implemented second approach using frequency count

--- Java/Array/ErasureValue.java
+++ Java/Array/ErasureValue.java
@@ -2,22 +2,45 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ErasureValue {
-    public static int maxUniqueSubarray(int[]nums){
-        Set<Integer> set = new HashSet<>();
+    // Approach 1 using Set
+    // public static int maxUniqueSubarray(int[]nums){
+    //     Set<Integer> set = new HashSet<>();
+    //     int right = 0, left = 0;
+    //     int maxSum = 0, currSum = 0;
+
+    //     while(right < nums.length){
+    //         if(!set.contains(nums[right])){
+    //             set.add(nums[right]);
+    //             currSum += nums[right];
+    //             maxSum = Math.max(maxSum, currSum);
+    //             right++;
+    //         }else{
+    //             set.remove(nums[left]);
+    //             currSum -= nums[left];
+    //             left++;
+    //         }
+    //     }
+    //     return maxSum;
+    // }
+
+    // Approach 2 using Frequency count
+    public static int maxUniqueSubarray(int[] nums){
+        int freq[] = new int[10001];
         int right = 0, left = 0;
         int maxSum = 0, currSum = 0;
 
-        while(right < nums.length){
-            if(!set.contains(nums[right])){
-                set.add(nums[right]);
-                currSum += nums[right];
-                maxSum = Math.max(maxSum, currSum);
-                right++;
-            }else{
-                set.remove(nums[left]);
+        for(right=0; right<nums.length; right++){
+            int temp = nums[right];
+
+            while(freq[temp] > 0){
+                freq[nums[left]]--;
                 currSum -= nums[left];
                 left++;
             }
+
+            freq[temp]++;
+            currSum += temp;
+            maxSum = Math.max(maxSum, currSum);
         }
         return maxSum;
     }
