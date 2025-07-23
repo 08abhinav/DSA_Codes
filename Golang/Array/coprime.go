31560bfd959291f16f7d6e80a0cd9e4fc2399684
08abhinav <abhinavnegi101@gmail.com>
2025-07-24 00:24:08 +0530

    implemented to count co primes in a given array

---
+++ Golang/Array/coprime.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func gcd(a, b int)int{
+	if b == 0{
+		return a
+	}else{
+		return gcd(b, a % b)
+	}
+}
+
+func countCoprime(nums []int)int{
+	n := len(nums)
+	var count int
+	for i:=0; i<n-1; i++{
+		for j:=i+1; j<n; j++{
+			if gcd(nums[i], nums[j]) == 1{
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func main(){
+	nums := [...]int{1, 2, 3}
+	fmt.Println(countCoprime(nums[:]))
+}
