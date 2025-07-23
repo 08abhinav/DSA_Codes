9fc7f43485cb57e9e7cd2cab0c3aa0c5a051dec8
08abhinav <abhinavnegi101@gmail.com>
2025-07-24 00:12:47 +0530

    implemented gcd helper method

--- Java/Array/Coprimes.java
+++ Java/Array/Coprimes.java
@@ -8,6 +8,11 @@ public class Coprimes {
         }
         return count;
     }
+
+    private static int gcd(int a, int b){
+        if(b == 0) return a;
+        else return gcd(b, a % b);
+    }
     public static void main(String... args){
 
     }
