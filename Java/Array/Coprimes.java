665760e28651f5b8418862fb880c7e85581a27e1
08abhinav <abhinavnegi101@gmail.com>
2025-07-24 00:11:34 +0530

    implemented countCoPrimes method

--- Java/Array/Coprimes.java
+++ Java/Array/Coprimes.java
@@ -1,5 +1,14 @@
 public class Coprimes {
+    public static int countCoPrimes(int[] arr){
+        int n = arr.length, count = 0;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(gcd(arr[i], arr[j]) == 1) count++;
+            }
+        }
+        return count;
+    }
     public static void main(String... args){
-        
+
     }
 }
