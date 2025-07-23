74234e103963ddb7e59354ea1bacd157cc6b5ef8
08abhinav <abhinavnegi101@gmail.com>
2025-07-23 11:04:03 +0530

    implemented subArraySum method

--- Java/Array/SumSubArray.java
+++ Java/Array/SumSubArray.java
@@ -1,4 +1,18 @@
 public class SumSubArray {
-    public static void main(String... args)
-    
+    // Approach 1
+    public static int subArraySum(int[] arr){
+        int n = arr.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                for(int k=i; k<=j; k++){
+                    sum += arr[k];
+                }
+            }
+        }
+        return sum;
+    }
+    public static void main(String... args) {
+        
+    }
 }
