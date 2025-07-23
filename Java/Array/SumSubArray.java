38c440b9e565a0b758751e4446a2ba0ec2b97212
08abhinav <abhinavnegi101@gmail.com>
2025-07-23 11:05:47 +0530

    implemented second approach also

--- Java/Array/SumSubArray.java
+++ Java/Array/SumSubArray.java
@@ -1,14 +1,24 @@
 public class SumSubArray {
     // Approach 1
+    // public static int subArraySum(int[] arr){
+    //     int n = arr.length;
+    //     int sum = 0;
+    //     for(int i=0; i<n; i++){
+    //         for(int j=i; j<n; j++){
+    //             for(int k=i; k<=j; k++){
+    //                 sum += arr[k];
+    //             }
+    //         }
+    //     }
+    //     return sum;
+    // }
+
+    // Approach 2
     public static int subArraySum(int[] arr){
         int n = arr.length;
         int sum = 0;
         for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                for(int k=i; k<=j; k++){
-                    sum += arr[k];
-                }
-            }
+            sum += (arr[i] * (i + 1) * (n - i));
         }
         return sum;
     }
