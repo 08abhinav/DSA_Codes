81ea0d350b55ad57af89a6ad950790b84811a75c
08abhinav <abhinavnegi101@gmail.com>
2025-07-23 10:38:30 +0530

    implemented ValidNumber method

--- Java/StringQues/ValidNumber.java
+++ Java/StringQues/ValidNumber.java
@@ -1,4 +1,34 @@
 public class ValidNumber {
+    public static boolean isNumber(String s){
+        s = s.trim();
+
+        boolean pointSeen = false;
+        boolean eSeen = false;
+        boolean numberSeen = false;
+        boolean numberAfterE = true;
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+
+            if(Character.isDigit(c)){
+                numberSeen = true;
+                numberAfterE = true;
+            }else if(c == '.'){
+                if(pointSeen || eSeen) return false;
+                pointSeen = true;
+            }else if(c == 'e' || c == 'E'){
+                if(eSeen || !numberSeen) return false;
+                eSeen = true;
+                numberAfterE = false;
+            }else if(c == '+' || c == '-'){
+                if(i != 0 && s.charAt(i) != 'e' && s.charAt(i) != 'E')return false;
+            }else{
+                return false;
+            }
+        }
+
+        return numberSeen && numberAfterE;
+    }
     public static void main(String[] args) {
         
     }
