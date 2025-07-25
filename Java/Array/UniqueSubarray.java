c44e8d2d44a0911bcb534e2d2f8e5546706ddac9
08abhinav <abhinavnegi101@gmail.com>
2025-07-25 11:00:56 +0530

    implemented maxSum method

--- Java/Array/UniqueSubarray.java
+++ Java/Array/UniqueSubarray.java
@@ -1,5 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class UniqueSubarray{
-    public static void main(String... args){
+    public static int maxSum(int[] arr){
+        Set<Integer> set = new HashSet<>();
+        boolean allNeg = true;
+        int maxValue = Integer.MIN_VALUE, currSum = 0;
         
+        for(int i : arr){
+            if(i >= 0){
+                allNeg = false;
+            }
+            if(i > maxValue){
+                maxValue = i;
+            }
+        }
+        if(allNeg) return maxValue;
+
+        for(int i : arr){
+            set.add(i);
+        }
+        for(int val : set){
+            if(val > 0) currSum += val;
+        }
+        return currSum;
+    }
+    public static void main(String... args){
+
     }
 }
