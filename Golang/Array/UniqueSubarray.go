424ba917e430decd87d54224fbee52ff4babe7b5
08abhinav <abhinavnegi101@gmail.com>
2025-07-25 11:06:43 +0530

    implemented maxSum method

--- Golang/Array/UniqueSubarray.go
+++ Golang/Array/UniqueSubarray.go
@@ -2,5 +2,18 @@ package main
 import "sort"
 
 func maxSum(arr []int)int{
+	sort.Ints(arr)
+	prev := arr[len(arr) - 1]
+	sum := prev
 
+	for i := len(arr) - 2; i >= 0; i--{
+		n := arr[i]
+		if n < 0{
+			return sum
+		}else if n != prev{
+			sum += n
+		}
+		prev = n
+	}
+	return sum;
 }
