ae490064a67f4821eb89facd5f2ded93d800251c
08abhinav <abhinavnegi101@gmail.com>
2025-07-26 12:22:00 +0530

    implemented LenghtOfLongestSubstring method

---
+++ Java/StringQues/SlidingWindow/LongestSubstr.java
@@ -0,0 +1,29 @@
+package Java.StringQues.SlidingWindow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstr {
+    public static int LengthOfLongestSubstring(String s){
+        Set<Character> set = new HashSet<>();
+        int left = 0, right = 0, maxLen = 0;
+        int n = s.length();
+
+        while(right < n){
+            char c = s.charAt(right);
+
+            if(!set.contains(c)){
+                set.add(c);
+                maxLen = Math.max(maxLen, right - left + 1);
+                right++;
+            }else{
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
