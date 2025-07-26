893cee3df511728dfd8e7678f7eb77ff253967e7
08abhinav <abhinavnegi101@gmail.com>
2025-07-26 12:05:15 +0530

    implementation done

--- Golang/Array/SlidingWindow/MinSizeSub.go
+++ Golang/Array/SlidingWindow/MinSizeSub.go
@@ -1,22 +1,29 @@
-package slidingwindow
+package main
 
-func minSubarray(nums[]int, target int)int{
+import "fmt"
+
+func minSubarray(nums []int, target int) int {
 	left, sum := 0, 0
 	minLen := len(nums) + 1
 
-	for right := 0; right<len(nums); right++{
+	for right := 0; right < len(nums); right++ {
 		sum += nums[right]
 
-		for sum >= target{
-			if right - left + 1 < minLen {
+		for sum >= target {
+			if right-left+1 < minLen {
 				minLen = right - left + 1
-            }			
+			}
 			sum -= nums[left]
 			left++
 		}
 	}
-	if minLen == len(nums) + 1{
+	if minLen == len(nums)+1 {
 		return 0
 	}
 	return minLen
+}
+
+func main() {
+	nums := []int{2, 3, 1, 2, 4, 3}
+	fmt.Println(minSubarray(nums, 7))
 }
