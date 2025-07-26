ec8e54ef455dedff0b93950efb6c5189aee689b8
08abhinav <abhinavnegi101@gmail.com>
2025-07-26 12:27:36 +0530

    implemented string question to find the length of longest substring without repeating characters

---
+++ Golang/String/SlidingWindow/LongestSubstr.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func LengthOfLongestSubString(s string) int {
+	charSet := make(map[byte]bool)
+	left, maxLen := 0, 0
+
+	for right := 0; right < len(s); right++ {
+		for charSet[s[right]] {
+			delete(charSet, s[left])
+			left++
+		}
+
+		charSet[s[right]] = true
+		if right-left+1 > maxLen {
+			maxLen = right - left + 1
+		}
+	}
+	return maxLen
+}
+
+func main() {
+	s := "abcabcbb"
+	fmt.Println(LengthOfLongestSubString(s));
+}
