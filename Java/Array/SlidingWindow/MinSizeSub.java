2c2e3ee6c2d6f654afdc4edef5cd2b30de27b9ec
08abhinav <abhinavnegi101@gmail.com>
2025-07-26 11:53:44 +0530

    added comment

--- Java/Array/SlidingWindow/MinSizeSub.java
+++ Java/Array/SlidingWindow/MinSizeSub.java
@@ -1,3 +1,6 @@
+/**Given an array of positive integers nums and a positive integer target, 
+ * return the minimal length of a subarray whose sum is greater than or equal to target. 
+ * If there is no such subarray, return 0 instead. */
 public class MinSizeSub{
     public static int minSubarray(int[] nums, int target){
         int left = 0, sum = 0;
