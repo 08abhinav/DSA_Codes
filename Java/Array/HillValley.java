e6c795186ad58852874e9b443573aa34b216645d
08abhinav <abhinavnegi101@gmail.com>
2025-07-27 19:25:12 +0530

    implementation done

--- Java/Array/HillValley.java
+++ Java/Array/HillValley.java
@@ -2,16 +2,18 @@ public class HillValley{
     public static int countHillValley(int[] nums){
         int res = 0, left = 0;
         for(int i = 1; i < nums.length - 1; i++){
-            if(nums[i] != nums[i + 1]){
-                if(nums[i] > nums[left] && nums[i] > nums[i + 1]) || (nums[i] < nums[left] && nums[i] < nums[i + 1]){
-                    count++;
+             if (nums[i] != nums[i + 1]) {
+                if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) || 
+                    (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                    res++;
                 }
                 left = i;
             }
         }
-        return count;
+        return res;
     }
     public static void main(String[] args) {
-        
+        int[] nums = {2, 4, 1, 1, 6, 5};
+        System.out.println(HillValley.countHillValley(nums));
     }
 }
