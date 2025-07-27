7138c28100e1d323217d4b3acb115f933e9f9ac8
08abhinav <abhinavnegi101@gmail.com>
2025-07-27 19:49:08 +0530

    impelementied lognestOnes method

--- Java/Array/SlidingWindow/ConsecutiveOnes.java
+++ Java/Array/SlidingWindow/ConsecutiveOnes.java
@@ -1,4 +1,19 @@
 public class ConsecutiveOnes {
+    public static int longesOnes(int[] nums){
+        int left = 0, maxLen = 0, zeroCount = 0;
+
+        for(int right = 0; right < nums.length; right++){
+            if(nums[right] == 0) zeroCount++;
+
+            while(zeroCount > k){
+                if(nums[left] == 0) zeroCount--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
     public static void main(String[] args) {
         
     }
