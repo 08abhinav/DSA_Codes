c7b7ca69cc511e4323b68d805322b89f6e580248
08abhinav <abhinavnegi101@gmail.com>
2025-07-28 10:50:14 +0530

    implemented maxCircularSum method

--- Java/Kadane_Algo/MaxCircular.java
+++ Java/Kadane_Algo/MaxCircular.java
@@ -1,4 +1,26 @@
 public class MaxCircular{
+    public static int maxCircularSum(int[] arr){
+        int totalSum = 0;
+        int maxSum = arr[0], minSum = 0;
+        int currMax = 0, currMin = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            currMax = Math.max(currMax + arr[i], arr[i]);
+            maxSum = Math.max(maxSum, currMax);
+
+            currMin = Math.min(currMin + arr[i], arr[i]);
+            minSum = Math.min(minSum, currMin);
+
+            totalSum += arr[i];
+        }
+
+        int sum = maxSum;
+        int circularSum = totalSum - minSum;
+
+        if(minSum == totalSum) return sum;
+
+        return Math.max(sum, circularSum);
+    }
     public static void main(String[] args) {
         
     }
