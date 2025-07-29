a41619b905cfd5157437b37adfc6b87e927f9364
08abhinav <abhinavnegi101@gmail.com>
2025-07-29 09:02:39 +0530

    implemented smallestSubarray method

--- Java/Array/SmallSubBit.java
+++ Java/Array/SmallSubBit.java
@@ -1,5 +1,19 @@
 public class SmallSubBit{
+    public static int[] smallestSubarray(int[]nums){
+        int n = nums.length;
+        int[] res = new int[n];
+
+        for(int i = 0; i < n; i++){
+            res[i] = 1;
+            for(int j = i - 1; j >= 0; j--){
+                if((nums[j] | nums[i]) == nums[j])break;
+                nums[j] |= nums[i];
+                res[j] = i - j + 1;
+            }   
+        }
+        return res;
+    }
     public static void main(String...args){
-        
+
     }
 }
