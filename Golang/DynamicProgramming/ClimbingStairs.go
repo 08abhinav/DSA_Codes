39f810cab9e8ef6fd809788d9dea386b42929bd8
08abhinav <abhinavnegi101@gmail.com>
2025-07-29 09:24:38 +0530

    implementation done

--- Golang/DynamicProgramming/ClimbingStairs.go
+++ Golang/DynamicProgramming/ClimbingStairs.go
@@ -1,17 +1,24 @@
 package main
 
-func climbStairs(n int)int {
-	if n == 3{
+import "fmt"
+
+func climbStairs(n int) int {
+	if n == 3 {
 		return n
 	}
 
 	prev1 := 3
 	prev2 := 2
 	curr := 0
-	for i := 3; i < n; i++{
+	for i := 3; i < n; i++ {
 		curr = prev1 + prev2
 		prev2 = prev1
 		prev1 = curr
 	}
 	return curr
+}
+
+func main() {
+	n := 8
+	fmt.Println(climbStairs(n))
 }
