84641d69ff8a31ccb9f6a55e4edfa4d41ed0e479
08abhinav <abhinavnegi101@gmail.com>
2025-07-29 09:10:23 +0530

    implementation done

--- Golang/Array/SmallSubBit.go
+++ Golang/Array/SmallSubBit.go
@@ -1,18 +1,25 @@
 package main
 
-func smallestSubarray(nums []int)[]int{
+import "fmt"
+
+func smallestSubarray(nums []int) []int {
 	n := len(nums)
 	res := make([]int, n)
 
-	for i := 0; i < n; i++{
+	for i := 0; i < n; i++ {
 		res[i] = 1
-		for j := i - 1; j >= 0; j--{
-			if (nums[j] | nums[i]) == nums[j]{
-				break;
+		for j := i - 1; j >= 0; j-- {
+			if (nums[j] | nums[i]) == nums[j] {
+				break
 			}
 			nums[j] |= nums[i]
 			res[j] = i - j + 1
 		}
 	}
 	return res
+}
+
+func main() {
+	nums := []int{1, 0, 2, 1, 3}
+	fmt.Println(smallestSubarray(nums))
 }
