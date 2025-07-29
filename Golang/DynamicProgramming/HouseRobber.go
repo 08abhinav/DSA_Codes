5ef64b2b59d370c9f2b7e95c88cf6a3d706d3fcb
08abhinav <abhinavnegi101@gmail.com>
2025-07-29 09:39:36 +0530

    implementation done

--- Golang/DynamicProgramming/HouseRobber.go
+++ Golang/DynamicProgramming/HouseRobber.go
@@ -1,23 +1,30 @@
 package main
 
-func max(a, b int)int{
-	if a > b{
+import "fmt"
+
+func max(a, b int) int {
+	if a > b {
 		return a
 	}
 	return b
 }
 
-func rob(nums[]int) int{
-	if len(nums) == 0{
+func rob(nums []int) int {
+	if len(nums) == 0 {
 		return 0
 	}
 
 	prev, prev2 := 0, 0
 
-	for i := 0; i < len(nums); i++{
+	for i := 0; i < len(nums); i++ {
 		temp := prev
-		prev = max(prev2 + nums[i], prev)
+		prev = max(prev2+nums[i], prev)
 		prev2 = temp
 	}
 	return prev
+}
+
+func main() {
+	nums := []int{1, 2}
+	fmt.Println(rob(nums))
 }
