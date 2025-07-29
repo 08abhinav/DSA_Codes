dbf11fd5206e1b281975c62d1cf61cec9e2a80d9
08abhinav <abhinavnegi101@gmail.com>
2025-07-29 09:38:50 +0530

    impelemented rob method

---
+++ Golang/DynamicProgramming/HouseRobber.go
@@ -0,0 +1,23 @@
+package main
+
+func max(a, b int)int{
+	if a > b{
+		return a
+	}
+	return b
+}
+
+func rob(nums[]int) int{
+	if len(nums) == 0{
+		return 0
+	}
+
+	prev, prev2 := 0, 0
+
+	for i := 0; i < len(nums); i++{
+		temp := prev
+		prev = max(prev2 + nums[i], prev)
+		prev2 = temp
+	}
+	return prev
+}
