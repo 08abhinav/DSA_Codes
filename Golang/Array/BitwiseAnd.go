ecbf7c1c333fe2433ff33527f82174db48b4322a
08abhinav <abhinavnegi101@gmail.com>
2025-07-30 20:15:10 +0530

    impelemented longeSubarray method

---
+++ Golang/Array/BitwiseAnd.go
@@ -0,0 +1,17 @@
+package main
+
+func longestSubarray(nums []int)int{
+	maxLen, currLen, maxVal := 0, 0, 0
+
+	for i := 0; i < len(nums); i++{
+		maxVal = Max(maxVal, nums[i])
+	}
+
+	for i := 0; i < len(nums); i++{
+		if nums[i] == maxVal{
+			currLen++
+			maxLen = Max(maxLen, currLen)
+		}
+	}
+	return maxLen
+}
