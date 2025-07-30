ff3396cd70bf7632a3f1ea8c4d66a67e0e93ea0a
08abhinav <abhinavnegi101@gmail.com>
2025-07-30 20:07:47 +0530

    implemented longestSubarray method

--- Java/Array/BitwiseAnd.java
+++ Java/Array/BitwiseAnd.java
@@ -1,4 +1,20 @@
 public class BitwiseAnd{
+    public static int longestSubarray(int[] nums){
+        int maxLen = 0, currLen = 0, maxVal = 0;
+        for(int i : nums){
+            maxVal = Math.max(maxVal, i);
+        }
+
+        for(int i : nums){
+            if(i == maxVal){
+                currLen++;
+                maxLen = Math.max(maxLen, currLen);
+            }else{
+                currLen = 0;
+            }
+        }
+        return maxLen;
+    }
     public static void main(String[] args) {
         
     }
