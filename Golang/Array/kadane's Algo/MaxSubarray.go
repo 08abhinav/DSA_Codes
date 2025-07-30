e69d5bc90f793bc7af054ff7c481b0177d019b71
08abhinav <abhinavnegi101@gmail.com>
2025-07-30 20:36:29 +0530

    implementation done

---
+++ Golang/Array/kadane's Algo/MaxSubarray.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func Max(a, b int)int{
+	if a > b{
+		return a
+	}
+	return b
+}
+
+func maxSubArray(nums []int)int{
+	maxSum, currSum := 0, 0
+	for i := 0; i < len(nums); i++ {
+		currSum += nums[i]
+		maxSum = Max(currSum, maxSum)
+		if currSum < 0{
+			currSum = 0
+		}
+	}
+	return maxSum
+}
+
+func main(){
+	nums := []int{-2,1,-3,4,-1,2,1,-5,4}
+	fmt.Println(maxSubArray(nums))
+	
+}
