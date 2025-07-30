5a63eab2d0d6b524046975f17c8194e22bcc4888
08abhinav <abhinavnegi101@gmail.com>
2025-07-30 20:30:02 +0530

    implemented maxSubArray method

--- Java/Kadane_Algo/MaxSubarray.java
+++ Java/Kadane_Algo/MaxSubarray.java
@@ -1,4 +1,16 @@
 public class MaxSubarray {
+    public static int maxSubArray(int[] nums){
+        int currSum = 0, maxSum = Integer.MIN_VALUE;
+
+        for(int i : nums){
+            currSum += i;
+            maxSum = Math.max(maxSum, currSum);
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
     public static void main(String[] args) {
         
     }
