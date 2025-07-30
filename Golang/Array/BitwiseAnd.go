d58c7c828057e6b2f7d67f714af7013aa2e94435
08abhinav <abhinavnegi101@gmail.com>
2025-07-30 20:16:55 +0530

    impelementation done

--- Golang/Array/BitwiseAnd.go
+++ Golang/Array/BitwiseAnd.go
@@ -1,24 +1,31 @@
 package main
 
-func Max(a, b int)int{
-	if a > b{
+import "fmt"
+
+func Max(a, b int) int {
+	if a > b {
 		return a
 	}
 	return b
 }
 
-func longestSubarray(nums []int)int{
+func longestSubarray(nums []int) int {
 	maxLen, currLen, maxVal := 0, 0, 0
 
-	for i := 0; i < len(nums); i++{
+	for i := 0; i < len(nums); i++ {
 		maxVal = Max(maxVal, nums[i])
 	}
 
-	for i := 0; i < len(nums); i++{
-		if nums[i] == maxVal{
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == maxVal {
 			currLen++
 			maxLen = Max(maxLen, currLen)
 		}
 	}
 	return maxLen
+}
+
+func main() {
+	nums := []int{1, 2, 3, 3, 2, 2}
+	fmt.Println(longestSubarray(nums))
 }
