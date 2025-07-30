ba88435a2e32e039c9e3dbb138b30185afaeaab7
08abhinav <abhinavnegi101@gmail.com>
2025-07-30 20:30:54 +0530

    implementation done

--- Java/Kadane_Algo/MaxSubarray.java
+++ Java/Kadane_Algo/MaxSubarray.java
@@ -12,6 +12,7 @@ public class MaxSubarray {
         return maxSum;
     }
     public static void main(String[] args) {
-        
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(MaxSubarray.maxSubArray(nums));
     }
 }
