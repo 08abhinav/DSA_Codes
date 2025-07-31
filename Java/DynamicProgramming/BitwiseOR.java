bc346c869d995f80c87cab32dd8de0c35dbc832c
08abhinav <abhinavnegi101@gmail.com>
2025-07-31 16:26:09 +0530

    implemented subarrayBitwise method using approach 1

--- Java/DynamicProgramming/BitwiseOR.java
+++ Java/DynamicProgramming/BitwiseOR.java
@@ -1,4 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class BitwiseOR{
+    public static int subarrayBitwise(int[] arr){
+        Set<Integer> res = new HashSet<>();
+        for(int i = 0; i < arr.length; i++){
+            res.add(arr[i]);
+            for(int j = i-1; j >= 0; j--){
+                if(arr[j] == (arr[j] | arr[i]))break;
+                arr[j] |= arr[i];
+                res.add(arr[j]);
+            }
+        }
+        return res.size();
+    }
     public static void main(String[] args) {
         
     }
