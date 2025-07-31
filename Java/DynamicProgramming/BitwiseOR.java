8632d16eb94ac2be61bea2f7a75bd1aae87ae335
08abhinav <abhinavnegi101@gmail.com>
2025-07-31 16:29:03 +0530

    implemented subarrayBitwise method using approach 2

--- Java/DynamicProgramming/BitwiseOR.java
+++ Java/DynamicProgramming/BitwiseOR.java
@@ -2,17 +2,35 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class BitwiseOR{
+    // Approach 1
+    // public static int subarrayBitwise(int[] arr){
+    //     Set<Integer> res = new HashSet<>();
+    //     for(int i = 0; i < arr.length; i++){
+    //         res.add(arr[i]);
+    //         for(int j = i-1; j >= 0; j--){
+    //             if(arr[j] == (arr[j] | arr[i]))break;
+    //             arr[j] |= arr[i];
+    //             res.add(arr[j]);
+    //         }
+    //     }
+    //     return res.size();
+    // }
+
+    // Approach 2
     public static int subarrayBitwise(int[] arr){
-        Set<Integer> res = new HashSet<>();
-        for(int i = 0; i < arr.length; i++){
-            res.add(arr[i]);
-            for(int j = i-1; j >= 0; j--){
-                if(arr[j] == (arr[j] | arr[i]))break;
-                arr[j] |= arr[i];
-                res.add(arr[j]);
+        Set<Integer> resOr = new HashSet<>();
+        Set<Integer> currOr = new HashSet<>();
+
+        for(int i : arr){
+            Set<Integer> nextOr = new HashSet<>();
+            nextOr.add(i);
+            for(int j : currOr){
+                nextOr.add(j | i);
             }
+            resOr.addAll(nextOr);
+            currOr = nextOr;
         }
-        return res.size();
+        return resOr.size();
     }
     public static void main(String[] args) {
         
