7f89564e3d4cce2f09f60934e30e9f79b59a4fea
08abhinav <abhinavnegi101@gmail.com>
2025-08-01 12:13:36 +0530

    implemented lengthOfLIS method

--- Java/BinarySearch/LongestIncreasingSub.java
+++ Java/BinarySearch/LongestIncreasingSub.java
@@ -1,6 +1,20 @@
 package Java.BinarySearch;
 
+import java.util.List;
+
 public class LongestIncreasingSub {
+    public static int lengthOfLIS(int[] nums){
+        List<Integer> res = new ArrayList<>();
+        for(int n : nums){
+            if(res.isEmpty() || res.get(res.size() - 1) < n){
+                res.add(n);
+            }else{
+                int idx = binarySearch(res, n);
+                res.set(idx, n);
+            }
+        }
+        return res.size();
+    }
     public static void main(String[] args) {
         
     }
