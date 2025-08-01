f82186611b45f88139bc2c8ceb297ced51c08829
08abhinav <abhinavnegi101@gmail.com>
2025-08-01 11:57:10 +0530

    implemented lenghtofLIS method

---
+++ Golang/DynamicProgramming/Subsequence/LongestIncreasingSub.go
@@ -0,0 +1,23 @@
+package main
+
+func lengthOfLIS(nums []int)int{
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	dp := make([]int, n)
+	for i := 0; i < n; i++{
+		dp[i] = 1;
+	}
+
+	maxLen := 1
+	for i := 1; i < n; i++{
+		for j := 0; j < i; j++{
+			if nums[i] > nums[j]{
+				dp[i] = Max(dp[j] + 1, dp[i])
+			}
+		}
+		maxLen = Max(dp[i], maxLen)
+	}
+	return maxLen
+}
