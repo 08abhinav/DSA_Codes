7e149ce1dcfc77b7b3b64509f6ed751e8eb53329
08abhinav <abhinavnegi101@gmail.com>
2025-08-01 11:52:23 +0530

    implemented lengthOfLIS method

--- Java/DynamicProgramming/Subsequence/LongestIncreaseSub.java
+++ Java/DynamicProgramming/Subsequence/LongestIncreaseSub.java
@@ -1,6 +1,26 @@
 package Subsequence;
 
+import java.util.Arrays;
+
 public class LongestIncreaseSub {
+    public static int lengthOfLIS(int[] nums){
+        int n = nums.length;
+        if(n == 0) return 0;
+        
+        int dp[] = new int[n];
+        Arrays.fill(dp, 1);
+        int maxLen = 1;
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
     public static void main(String[] args) {
         
     }
