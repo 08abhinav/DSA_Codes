97916546c68fcf53497f4d1b191f0c305d804871
08abhinav <abhinavnegi101@gmail.com>
2025-08-01 11:58:48 +0530

    implemenation done

--- Golang/DynamicProgramming/Subsequence/LongestIncreasingSub.go
+++ Golang/DynamicProgramming/Subsequence/LongestIncreasingSub.go
@@ -1,30 +1,37 @@
 package main
 
-func Max(a, b int)int{
-	if a > b{
+import "fmt"
+
+func Max(a, b int) int {
+	if a > b {
 		return a
 	}
 	return b
 }
 
-func lengthOfLIS(nums []int)int{
+func lengthOfLIS(nums []int) int {
 	n := len(nums)
 	if n == 0 {
 		return 0
 	}
 	dp := make([]int, n)
-	for i := 0; i < n; i++{
-		dp[i] = 1;
+	for i := 0; i < n; i++ {
+		dp[i] = 1
 	}
 
 	maxLen := 1
-	for i := 1; i < n; i++{
-		for j := 0; j < i; j++{
-			if nums[i] > nums[j]{
-				dp[i] = Max(dp[j] + 1, dp[i])
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = Max(dp[j]+1, dp[i])
 			}
 		}
 		maxLen = Max(dp[i], maxLen)
 	}
 	return maxLen
+}
+
+func main() {
+	nums := []int{10, 9, 2, 5, 3, 7, 101, 18}
+	fmt.Println(lengthOfLIS(nums))
 }
