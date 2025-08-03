769bc94c75c5f8f72339ffa758507487bd132178
08abhinav <abhinavnegi101@gmail.com>
2025-08-03 21:58:46 +0530

    implemented maxToatlFruits method

--- Java/Array/SlidingWindow/MaxFruite.java
+++ Java/Array/SlidingWindow/MaxFruite.java
@@ -1,4 +1,20 @@
 public class MaxFruite {
+    public static int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int left = 0, sum = 0, max = 0;
+
+        for (int right = 0; right < fruits.length; right++) {
+            sum += fruits[right][1];
+
+            while (left <= right && minSteps(fruits[left][0], fruits[right][0], startPos) > k) {
+                sum -= fruits[left][1];
+                left++;
+            }
+
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
     public static void main(String[] args) {
         
     }
