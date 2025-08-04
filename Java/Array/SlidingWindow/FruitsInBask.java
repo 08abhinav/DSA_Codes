4381d87121d5452505c804a497f35a71f41c0474
08abhinav <abhinavnegi101@gmail.com>
2025-08-04 16:22:11 +0530

    implementation done

--- Java/Array/SlidingWindow/FruitsInBask.java
+++ Java/Array/SlidingWindow/FruitsInBask.java
@@ -2,7 +2,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FruitsInBask {
-    
+    public static int totalFruit(int[] fruits){
+        int n = fruits.length;
+        if(n == 0) return 0;
+        
+        Map<Integer, Integer> count = new HashMap<>();
+        int left = 0, maxLen = 0;
+        for(int right = 0; right < n; right++){
+           count.put(fruits[right], count.getOrDefault(fruits[right], 0)+ 1);
+            
+           while(count.size() > 2){
+                int leftFruit = fruits[left];
+                if(count.get(leftFruit) == 0){
+                    count.remove(leftFruit);
+                }
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
     public static void main(String...args){
 
     }
