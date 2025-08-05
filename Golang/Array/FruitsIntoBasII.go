80bc3f534d0ad7fe930317311d98e73ad9a4ce5b
08abhinav <abhinavnegi101@gmail.com>
2025-08-05 15:02:21 +0530

    implemented to find the unplaced fruits into the basket

---
+++ Golang/Array/FruitsIntoBasII.go
@@ -0,0 +1,15 @@
+package main
+
+func numOfUnplacedFruits(fruits[]int, baskets[]int)int{
+	c := 0
+	for i := 0; i < len(fruits); i++{
+		for j :=0; j < len(baskets); j++{
+			if fruits[i] <= baskets[j]{
+				c++
+				baskets[j] = 0
+				break
+			}
+		}
+	}
+	return len(fruits) - c
+}
