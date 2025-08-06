bb46ee7b03a642a4b06b90fa6dcfcd15fa990529
08abhinav <abhinavnegi101@gmail.com>
2025-08-06 11:15:46 +0530

    implementation done

--- Java/Array/SlidingWindow/MaxSubarrayAvg.java
+++ Java/Array/SlidingWindow/MaxSubarrayAvg.java
@@ -13,6 +13,8 @@ public class MaxSubarrayAvg {
         return maxSum / k;
     }
     public static void main(String[] args) {
-        
+        int[] nums = {1,12,-5,-6,50,3};
+        int k = 4;
+        System.out.println(MaxSubarrayAvg.findMaxAvg(nums, k));
     }
 }
