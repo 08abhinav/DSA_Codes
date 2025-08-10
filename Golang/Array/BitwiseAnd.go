0a9079041f41b9a6110e216c9e95d8b78971073d
08abhinav <abhinavnegi101@gmail.com>
2025-08-10 21:58:34 +0530

    implementation done

--- Golang/Array/BitwiseAnd.go
+++ Golang/Array/BitwiseAnd.go
@@ -1,15 +1,4 @@
-/*2419. Longest Subarray With Maximum Bitwise AND
-You are given an integer array nums of size n.
 
-Consider a non-empty subarray from nums that has the maximum possible bitwise AND.
-
-In other words, let k be the maximum value of the bitwise AND of any subarray of nums. 
-Then, only subarrays with a bitwise AND equal to k should be considered. Return the 
-length of the longest such subarray.
-
-The bitwise AND of an array is the bitwise AND of all the numbers in it.
-
-A subarray is a contiguous sequence of elements within an array.*/
 package main
 
 import "fmt"
@@ -37,7 +26,3 @@ func longestSubarray(nums []int) int {
 	return maxLen
 }
 
-func main() {
-	nums := []int{1, 2, 3, 3, 2, 2}
-	fmt.Println(longestSubarray(nums))
-}
