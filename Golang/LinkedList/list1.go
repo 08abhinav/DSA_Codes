af769755621b361432e9ee4e8241398cd2512489
08abhinav <abhinavnegi101@gmail.com>
2025-08-11 20:27:50 +0530

    implemented delete method

--- Golang/LinkedList/list1.go
+++ Golang/LinkedList/list1.go
@@ -36,8 +36,32 @@ func display(head *List){
 		return
 	}
 	i := head
+	fmt.Println("Values in list")
 	for i != nil{
 		fmt.Println(i.data)
 		i = head.next
 	}
+}
+
+func delete(head *List) *List{
+	display(head)
+	var x int
+	fmt.Println("Enter the value to delete: ")
+	fmt.Scanf("%d", &x)
+
+	if head.data == x{
+		return head.next
+	}
+
+	temp := head
+	for temp.next != nil{
+		if temp.next.data == x{
+			temp.next = temp.next.next
+			break;
+		}
+		temp = temp.next
+	}
+	fmt.Println("After deletion")
+	display(head)
+	return head
 }
