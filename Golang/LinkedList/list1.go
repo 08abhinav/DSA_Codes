40105bd32cf59b1aa3b4c38b54d7a44e57658c4b
08abhinav <abhinavnegi101@gmail.com>
2025-08-11 20:15:24 +0530

    implemented insert function

--- Golang/LinkedList/list1.go
+++ Golang/LinkedList/list1.go
@@ -1,3 +1,31 @@
 /*
 Implemented Linked list in go where insert, delete and display functions are created
 */
+package main
+
+import "fmt"
+
+type List struct{
+	data int
+	next *List
+}
+
+func insert(head *List) *List{
+	var x int
+	fmt.Println("Enter the value to insert: ")
+	fmt.Scanf("%d", &x)
+
+	ptr := &List{data: x}
+
+	if head == nil{
+		return ptr
+	}
+
+	temp := head
+
+	for temp.next != nil{
+		temp = temp.next
+	}
+	temp.next = ptr;
+	return head;
+}
