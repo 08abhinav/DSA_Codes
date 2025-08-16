2ea84297ca2a47e5191828cd0c0511afe6952b9c
08abhinav <abhinavnegi101@gmail.com>
2025-08-16 16:56:08 +0530

    implemented maximum69Num method

--- Java/Greedy/Max69.java
+++ Java/Greedy/Max69.java
@@ -1,5 +1,14 @@
-public class Max69(){
-    public static void main(String..args){
-        
+public class Max69{
+    public static int maximun69Num(int num){
+        char[] ch = String.valueOf(num).toCharArray();
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] == '6'){
+                ch[i] = '9';
+            }
+        }
+        return Integer.parseInt(new String(ch));
+    }
+    public static void main(String...args){
+
     }
 }
