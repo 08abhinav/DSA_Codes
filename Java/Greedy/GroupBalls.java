282c5deb0e1d655ca6b8385f8ce52da9ebfc235f
08abhinav <abhinavnegi101@gmail.com>
2025-08-17 21:44:47 +0530

    added question

--- Java/Greedy/GroupBalls.java
+++ Java/Greedy/GroupBalls.java
@@ -1,7 +1,6 @@
-/**You are given an array arr[] of positive integers, where each element arr[i] represents the number written on the i-th ball, and a positive integer k.
+/**You are given an array arr[] of positive integers, where each element arr[i] represents 
+the number written on the i-th ball, and a positive integer k.
 Your task is to determine whether it is possible to rearrange all the balls into groups such that:
-
-
 Each group contains exactly k balls.
 The numbers in each group are consecutive integers */
 public class GroupBalls {
