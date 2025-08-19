a9afba7aca396857a7e5d308f1ef58ac2b75c59a
08abhinav <abhinavnegi101@gmail.com>
2025-08-19 08:59:15 +0530

    implementedn zeroFillSub method

--- Java/Array/ZeroFilledSub.java
+++ Java/Array/ZeroFilledSub.java
@@ -1,5 +1,17 @@
 public class ZeroFilledSub {
+    public static long zeroFillSub(int[] nums){
+        int count = 0, ans = 0;
+        for(int i : nums){
+            if(i == 0){
+                count++;
+                ans += count;
+            }else{
+                count = 0;
+            }
+        }
+        return ans;
+    }
     public static void main(String...args){
-        
+
     }
 }
