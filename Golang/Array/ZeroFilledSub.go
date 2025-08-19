d8ae0aa21ae251846c0a0d05853e386bbafba54d
08abhinav <abhinavnegi101@gmail.com>
2025-08-19 09:08:32 +0530

    implementation done

--- Golang/Array/ZeroFilledSub.go
+++ Golang/Array/ZeroFilledSub.go
@@ -1,15 +1,22 @@
 package main
 
-func zeroFilledSub(nums []int)int64{
+import "fmt"
+
+func zeroFilledSub(nums []int) int64 {
 	var count, ans int64
 	count, ans = 0, 0
-	for _, val := range nums{
-		if val == 0{
+	for _, val := range nums {
+		if val == 0 {
 			count++
 			ans += count
-		}else{
+		} else {
 			count = 0
 		}
 	}
-	return  ans
+	return ans
+}
+
+func main() {
+	num := []int{1, 3, 0, 0, 2, 0, 0, 4}
+	fmt.Println(zeroFilledSub(num))
 }
