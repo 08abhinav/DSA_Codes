e950a8386f03c8d9130d4c9df66d172ee1d439b7
08abhinav <abhinavnegi101@gmail.com>
2025-08-22 23:12:22 +0530

    implemented minLen method

--- Java/StringQues/MinLenAfterDelete.java
+++ Java/StringQues/MinLenAfterDelete.java
@@ -1,6 +1,20 @@
 package StringQues;
 
 public class MinLenAfterDelete {
+    public static int minLen(String s){
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right && s.charAt(left) == s.charAt(right)){
+            char ch = s.charAt(left);
+            while(left <= right && s.charAt(ch) == s.charAt(left)){
+                left++;
+            }
+            while(right >= left && s.charAt(ch) == s.charAt(right)){
+                right--;
+            }
+        }
+        return right - left + 1;
+    }
     public static void main(String[] args) {
         
     }
