f751a9b09b64239ab06fec7456bec6bf16894936
08abhinav <abhinavnegi101@gmail.com>
2025-08-27 22:49:26 +0530

    implemented searchK method

--- Java/BinarySearch/SearchInsertK.java
+++ Java/BinarySearch/SearchInsertK.java
@@ -1,4 +1,17 @@
 public class SearchInsertK{
+    public static int searchK(int[] arr, int k){
+        int low = 0, high = arr.length - 1, res = arr.length;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(arr[mid] >= k){
+                res = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+       }
+       return res;
+    }
     public static void main(String[] args) {
         
     }
