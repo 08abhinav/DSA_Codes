d05169d92c1caaefc6eb3b014e6ed34db8b34e8a
08abhinav <abhinavnegi101@gmail.com>
2025-08-27 22:54:34 +0530

    implementation done

--- Golang/BinarySearch/SearchInsertK.go
+++ Golang/BinarySearch/SearchInsertK.go
@@ -1,15 +1,24 @@
 package main
-func searchK(arr []int, k int)int{
+
+import "fmt"
+
+func searchK(arr []int, k int) int {
 	n := len(arr)
-	low, high, res := 0, n - 1, n
-	for low <= high{
+	low, high, res := 0, n-1, n
+	for low <= high {
 		mid := (low + high) / 2
-		if(arr[mid] >= k){
+		if arr[mid] >= k {
 			res = mid
 			high = mid - 1
-		}else{
+		} else {
 			low = mid + 1
 		}
 	}
 	return res
+}
+
+func main() {
+	arr := []int{1, 3, 5, 7}
+	k := 4
+	fmt.Println(searchK(arr, k))
 }
