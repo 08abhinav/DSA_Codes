f005c4a029a9c60b72a3dc2322dcde9546d44856
08abhinav <abhinavnegi101@gmail.com>
2025-08-28 10:27:51 +0530

    implemented findOccur method

--- Java/BinarySearch/FirstLastOccur.java
+++ Java/BinarySearch/FirstLastOccur.java
@@ -1,6 +1,16 @@
 package Java.BinarySearch;
 
+import java.util.ArrayList;
+
 public class FirstLastOccur {
+    public static ArrayList<Integer> findOcuur(int[] arr, int x){
+        int first = firstOccur(arr, x);
+        int last = lastOccur(arr, x);
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(first);
+        res.add(last);
+        return res;
+    }
     public static void main(String[] args) {
         
     }
