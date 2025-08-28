aa64a78de5b71d78f25bd81bdf41cd6f36b0d6b4
08abhinav <abhinavnegi101@gmail.com>
2025-08-28 10:29:47 +0530

    implemented firstOccur helper method

--- Java/BinarySearch/FirstLastOccur.java
+++ Java/BinarySearch/FirstLastOccur.java
@@ -11,6 +11,25 @@ public class FirstLastOccur {
         res.add(last);
         return res;
     }
+
+    public static int firstOccur(int[] arr, int x){
+        int low = 0, high = arr.length - 1, res = -1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] == x){
+                res = mid;
+                high = mid - 1;
+            }
+            else if(arr[mid] > x){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
