0b67db3ab3b1559218b9b75d65e992353ba6465e
08abhinav <abhinavnegi101@gmail.com>
2025-08-31 16:57:35 +0530

    implemented helper method firstOccur

--- Golang/BinarySearch/FirstLastOccur.go
+++ Golang/BinarySearch/FirstLastOccur.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 
+func firstOccur(arr []int, x int) int {
+	low, high, res := 0, len(arr)-1, -1
+	for low <= high {
+		mid := (low + high) / 2
 
+		if arr[mid] == x {
+			res = mid
+			high = mid - 1
+		} else if arr[mid] > x {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return res
+}
 
 func findOccur(arr []int, x int) []int {
 	first := firstOccur(arr, x)
