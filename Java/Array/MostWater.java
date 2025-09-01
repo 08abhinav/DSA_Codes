9a6b62233a9e0531c205a72295f795e93dccf137
08abhinav <abhinavnegi101@gmail.com>
2025-09-01 08:52:50 +0530

    implemented maxWater method

--- Java/Array/MostWater.java
+++ Java/Array/MostWater.java
@@ -1,4 +1,19 @@
 public class MostWater{
+    public static int maxWater(int[]arr){
+        int left = 0, right = arr.length - 1;
+        int res = 0;
+        while(left < right){
+            int water = Math.min(arr[left], arr[right] * (right - left));
+            res = Math.max(water, res);
+
+            if(arr[left] < arr[right]){
+                left += 1;
+            }else{
+                right -= 1;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
