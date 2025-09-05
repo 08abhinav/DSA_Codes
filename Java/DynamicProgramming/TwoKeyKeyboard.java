12102365757dcd568f8ea4533c7530db80d25d76
08abhinav <abhinavnegi101@gmail.com>
2025-09-05 11:01:16 +0530

    implemented minStep method

--- Java/DynamicProgramming/TwoKeyKeyboard.java
+++ Java/DynamicProgramming/TwoKeyKeyboard.java
@@ -1,4 +1,16 @@
 public class TwoKeyKeyboard {
+    public static int minStep(int n){
+        if(n == 1) return 0;
+        int res = 0, factor = 0;
+        while(n > 1){
+            while(n % factor == 0){
+                res += factor;
+                n /= factor;
+            }
+            factor += 1;
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
