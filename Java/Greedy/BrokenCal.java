5514beea90a8d7bb67710754cdb493fb956ebe28
08abhinav <abhinavnegi101@gmail.com>
2025-09-05 10:33:24 +0530

    implemented brokenCalcu method

--- Java/Greedy/BrokenCal.java
+++ Java/Greedy/BrokenCal.java
@@ -1,4 +1,16 @@
 public class BrokenCal {
+    public static int brokenCalcu(int startValue, int target){
+        int res = 0;
+        while(target > startValue){
+            if(target % 2 == 0){
+                target /= 2;
+            }else{
+                target += 1;
+            }
+            res += 1;
+        }
+        return res + (startValue - target);
+    }
     public static void main(String[] args){
         
     }
