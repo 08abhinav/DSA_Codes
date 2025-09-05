1784c923b281868c9bb7f9634a4e259ad54cae98
08abhinav <abhinavnegi101@gmail.com>
2025-09-05 10:38:35 +0530

    implementation done

--- Golang/Greedy/BrokenCal.go
+++ Golang/Greedy/BrokenCal.go
@@ -1,12 +1,21 @@
-func brokenCalcu(startValue, target int)int{
+package main
+
+import "fmt"
+
+func brokenCalcu(startValue, target int) int {
 	res := 0
-	for target > startValue{
-		if target % 2 == 0{
+	for target > startValue {
+		if target%2 == 0 {
 			target /= 2
-		}else{
+		} else {
 			target += 1
 		}
 		res += 1
 	}
 	return res + (startValue - target)
+}
+
+func main() {
+	startValue, target := 3, 10
+	fmt.Print(brokenCalcu(startValue, target))
 }
