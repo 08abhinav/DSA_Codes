1694a91d2aa9e729163d06d3f037feac504d4646
08abhinav <abhinavnegi101@gmail.com>
2025-09-05 11:02:40 +0530

    updated the value of factor

--- Java/DynamicProgramming/TwoKeyKeyboard.java
+++ Java/DynamicProgramming/TwoKeyKeyboard.java
@@ -1,7 +1,7 @@
 public class TwoKeyKeyboard {
     public static int minStep(int n){
         if(n == 1) return 0;
-        int res = 0, factor = 0;
+        int res = 0, factor = 2;
         while(n > 1){
             while(n % factor == 0){
                 res += factor;
