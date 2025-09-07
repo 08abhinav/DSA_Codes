db336e38f1d1bf2caec73677d7d3a02a082a5444
08abhinav <abhinavnegi101@gmail.com>
2025-09-07 15:17:05 +0530

    added question

--- Java/Array/NUniqueIntegers.java
+++ Java/Array/NUniqueIntegers.java
@@ -1,3 +1,5 @@
+/**1304. Find N Unique Integers Sum up to Zero
+Given an integer n, return any array containing n unique integers such that they add up to 0. */
 import java.util.Arrays;
 
 public class NUniqueIntegers {
