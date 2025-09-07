e0a7f7255850104f1da14ab82e698d7fec398c63
08abhinav <abhinavnegi101@gmail.com>
2025-09-07 15:21:58 +0530

    implemenation done

--- Golang/Array/NUniqueIntegers.go
+++ Golang/Array/NUniqueIntegers.go
@@ -1,11 +1,20 @@
-func(n int)[]int{
-	k := 1
-	res := make([] int, n)
+package main
 
-	for i := 0; i < n / 2; i++{
+import "fmt"
+
+func sumZeros(n int) []int {
+	k := 1
+	res := make([]int, n)
+
+	for i := 0; i < n/2; i++ {
 		res[i] = k
-		res[n - 1 - i] = -k
+		res[n-1-i] = -k
 		k += 1
 	}
 	return res
+}
+
+func main() {
+	n := 5
+	fmt.Println(sumZeros(n))
 }
