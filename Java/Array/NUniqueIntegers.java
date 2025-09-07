f430097429b1b54aae6323e8747ee341be934a50
08abhinav <abhinavnegi101@gmail.com>
2025-09-07 15:14:21 +0530

    implemented sumZero method

--- Java/Array/NUniqueIntegers.java
+++ Java/Array/NUniqueIntegers.java
@@ -1,5 +1,15 @@
 public class NUniqueIntegers {
+    public static int[] sumZero(int n){
+        int[] res = new int[n];
+        int k = 1;
+        for(int i = 0; i < n / 2 ; i++){
+            res[i] = k;
+            res[n - 1 - i] = -k;
+            k += 1;
+        }
+        return res;
+    }
     public static void main(String...args){
-        
+
     }
 }
