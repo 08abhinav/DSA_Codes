7be55bf84ae1746ea18dd021add2f1431323480f
08abhinav <abhinavnegi101@gmail.com>
2025-09-08 16:55:04 +0530

    added question

--- Java/Array/SumOfTwoNonZero.java
+++ Java/Array/SumOfTwoNonZero.java
@@ -1,3 +1,10 @@
+/*1317. Convert Integer to the Sum of Two No-Zero Integers
+No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+Given an integer n, return a list of two integers [a, b] where:
+
+a and b are No-Zero integers.
+a + b = n
+The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them. */
 class SumOfTwoNonZero{
     public static int[] getNoZero(int n){
         int a = n - 1, b = 1;
