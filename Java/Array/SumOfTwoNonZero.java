554c7fd217e2423322c0ad75fb10ffd75a38fd37
08abhinav <abhinavnegi101@gmail.com>
2025-09-08 16:53:40 +0530

    implemented getNoZero method

--- Java/Array/SumOfTwoNonZero.java
+++ Java/Array/SumOfTwoNonZero.java
@@ -1,4 +1,12 @@
 class SumOfTwoNonZero{
+    public static int[] getNoZero(int n){
+        int a = n - 1, b = 1;
+        while(String.valueOf(a).contains("0") || String.valueOf(b).contains("0")){
+            a -= 1;
+            b += 1;
+        }
+        return new int[]{a, b};
+    }
     public static void main(String[] args) {
         
     }
