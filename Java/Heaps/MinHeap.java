f68e3e5743f5d8123cd2b609fea7cc8da635e388
08abhinav <abhinavnegi101@gmail.com>
2025-09-09 16:55:41 +0530

    Implemented minHeapify method

--- Java/Heaps/MinHeap.java
+++ Java/Heaps/MinHeap.java
@@ -4,6 +4,26 @@ public class MinHeap {
             minHeapify(arr, i, n);
         }
     }
+
+    public static void minHeapify(int arr[], int i, int n){
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int smallest = i;
+
+        if(left < n && arr[left] < arr[smallest]){
+            smallest = left;
+        }
+        if(right < n && arr[right] < arr[smallest]){
+            smallest = right;
+        }
+        if(smallest != i){
+            int temp = arr[i];
+            arr[i] = arr[smallest];
+            arr[smallest] = temp;
+
+            minHeapify(arr, smallest, n);
+        }
+    }
     public static void main(String[] args) {
         
     }
