4f851274d5ec22f567a55e8b7275804a62df20a7
08abhinav <abhinavnegi101@gmail.com>
2025-09-09 16:44:10 +0530

    implemented convertToMaxHeap method

--- Java/Heaps/MaxHeap.java
+++ Java/Heaps/MaxHeap.java
@@ -1,5 +1,10 @@
 public class MaxHeap {
+    public static void convertToMaxHeap(int arr[], int n){
+        for(int i = (n - 2) / 2; i >=0; i--){
+            maxHeapify(arr, i, n);
+        }
+    }
     public static void main(String... args){
-        
+
     }
 }
