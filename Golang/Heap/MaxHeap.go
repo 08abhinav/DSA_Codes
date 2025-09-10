5f1922ea71e34205958f52bd1a4f3f6b2c374df3
08abhinav <abhinavnegi101@gmail.com>
2025-09-10 10:31:25 +0530

    implmented helper method maxHeapify

--- Golang/Heap/MaxHeap.go
+++ Golang/Heap/MaxHeap.go
@@ -1,7 +1,29 @@
 package main
 
+func maxHeapify(arr []int, i, n int){
+		left := 2 * i + 1;
+		right := 2 * i + 2;
+		large := i
+
+		if left < n && arr[left] > arr[large]{
+			large = left
+		}
+
+		if right < n && arr[right] > arr[large]{
+			large = right
+		}
+
+		if large != i{
+			temp := arr[i]
+			arr[i] = arr[large]
+			arr[large] = temp
+			maxHeapify(arr, large, n)
+		}
+}
+
 func convertToMaxHeap(arr []int, n int){
 	for i := (n - 2) / 2; i >= 0; i--{
 		maxHeapify(arr, i, n)
 	}
 }
+
