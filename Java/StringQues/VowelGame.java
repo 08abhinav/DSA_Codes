4a7d35624aff7390f8e92958a55a2c0999e79d77
08abhinav <abhinavnegi101@gmail.com>
2025-09-12 23:20:06 +0530

    implemented doesAliceWin method

--- Java/StringQues/VowelGame.java
+++ Java/StringQues/VowelGame.java
@@ -1,3 +1,11 @@
 public class VowelGame{
+    public static boolean doesAlicWin(String s){
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) = 'i'){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String...args){}
 }
