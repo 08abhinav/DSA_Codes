1e98e519937ea37e2223210d4698c9580849b310
08abhinav <abhinavnegi101@gmail.com>
2025-09-12 23:24:46 +0530

    implementation done

--- Golang/String/VowelGame.go
+++ Golang/String/VowelGame.go
@@ -1,8 +1,17 @@
-func doesAliceWins(s string)bool{
-	for i := 0; i < len(s); i++{
-		if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'{
+package main
+
+import "fmt"
+
+func doesAliceWins(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' {
 			return true
 		}
 	}
 	return false
+}
+
+func main() {
+	s := "leetcoder"
+	fmt.Println(doesAliceWins(s))
 }
