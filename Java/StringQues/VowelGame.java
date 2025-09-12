7caf0154b9b1bef64a4c498f1570a6de2e5b2108
08abhinav <abhinavnegi101@gmail.com>
2025-09-12 23:20:31 +0530

    minor update

--- Java/StringQues/VowelGame.java
+++ Java/StringQues/VowelGame.java
@@ -1,7 +1,7 @@
 public class VowelGame{
     public static boolean doesAlicWin(String s){
         for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) = 'i'){
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'i'){
                 return true;
             }
         }
