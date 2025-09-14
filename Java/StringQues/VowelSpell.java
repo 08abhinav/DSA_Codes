6a1b832ab13575975e06a7327c22aa487c2a2018
08abhinav <abhinavnegi101@gmail.com>
2025-09-14 19:48:29 +0530

    implemented spellChecker method

--- Java/StringQues/VowelSpell.java
+++ Java/StringQues/VowelSpell.java
@@ -1,4 +1,31 @@
 class VowelSpell{
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> exact = new HashSet<>(Arrays.asList(wordlist));
+        Map<String, String> caseMap = new HashMap<>();
+        Map<String, String> vowelMap = new HashMap<>();
+
+        for (String w : wordlist) {
+            String lower = toLower(w);
+            String devowel = deVowel(lower);
+            caseMap.putIfAbsent(lower, w);
+            vowelMap.putIfAbsent(devowel, w);
+        }
+        String[] result = new String[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String q = queries[i];
+            if (exact.contains(q)) {
+                result[i] = q;
+            } else {
+                String lower = toLower(q);
+                String devowel = deVowel(lower);
+                if (caseMap.containsKey(lower)) result[i] = caseMap.get(lower);
+                else if (vowelMap.containsKey(devowel)) result[i] = vowelMap.get(devowel);
+                else result[i] = "";
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         
     }
