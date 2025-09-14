8734265d58fd1e61f672d50622cbd01851d65edf
08abhinav <abhinavnegi101@gmail.com>
2025-09-14 19:49:28 +0530

    implemented deVowelmethod

--- Java/StringQues/VowelSpell.java
+++ Java/StringQues/VowelSpell.java
@@ -30,7 +30,14 @@ class VowelSpell{
     private String toLower(String s) {
         return s.toLowerCase();
     }
-
+    private String deVowel(String s) {
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if (isVowel(ch[i])) ch[i] = '*';
+        }
+        return new String(ch);
+    }
+    
 
     public static void main(String[] args) {
         
