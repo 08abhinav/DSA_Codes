848645956ceb1ee152ef65b5e4dd0adeb1bee881
08abhinav <abhinavnegi101@gmail.com>
2025-09-19 12:28:28 +0530

    implementation done

--- Golang/Math/PerfectSquare.go
+++ Golang/Math/PerfectSquare.go
@@ -1,15 +1,23 @@
-func isPerfectSquare(n int)boolean{
+package main
+import "fmt"
+
+func isPerfectSquare(n int) bool{
 	start := 1
 	end := n
-	while(start <= end){
+	for start <= end{
 		mid := (start + end) / 2
-		if int64(mid) * int64(mid) == num{
+		if int64(mid) * int64(mid) == int64(n){
 			return true;
-		}else if int64(mid) * int64(mid) > num{
+		}else if int64(mid) * int64(mid) > int64(n){
 			end = mid - 1
 		}else{
 			start = mid + 1
 		}
 	}
 	return false;
+}
+
+func main(){
+	n := 16
+	fmt.Println(isPerfectSquare(n))
 }
