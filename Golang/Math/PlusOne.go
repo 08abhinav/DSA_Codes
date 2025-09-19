f49653315fb8f798b4563830a71ba6e3fb19dd45
08abhinav <abhinavnegi101@gmail.com>
2025-09-19 11:58:48 +0530

    implemented plusOne function

--- Golang/Math/PlusOne.go
+++ Golang/Math/PlusOne.go
@@ -1,13 +1,22 @@
-func plusOne(digits []int)[]int{
-	for i := len(digits) - 1; i >=0; i--{
-		if digits[i] + 1 != 10{
+package main
+
+import "fmt"
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i]+1 != 10 {
 			digits[i] += 1
 			return digits
 		}
-		digits[i] = 0;
+		digits[i] = 0
 	}
 
-	newDigits := make([]int, len(digits + 1))
-	newDigits[0] = 1;
+	newDigits := make([]int, len(digits) + 1)
+	newDigits[0] = 1
 	return newDigits
+}
+
+func main() {
+	digits := []int{1, 2, 3}
+	fmt.Println(plusOne(digits))
 }
