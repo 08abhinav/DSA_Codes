c4ee5c8ff72d3e20e675cacac70e4f080cee39a5
08abhinav <abhinavnegi101@gmail.com>
2025-09-22 12:26:59 +0530

    implemented toHex method

--- Java/Math/ToHexaDecimal.java
+++ Java/Math/ToHexaDecimal.java
@@ -1,3 +1,18 @@
 public class ToHexaDecimal {
-    public static void main(String...args){}
+    public static String toHex(int num){
+        if(num == 0) return "0";
+
+        char[] hex = "0123456789abcdef".toCharArray();
+        StringBuilder res = new StringBuilder();
+
+        while(num != 0){
+            int rem = num & 15;
+            res.append(hex[rem]);
+            num >>>= 4;
+        }
+        return res.reverse().toString();
+    }
+    public static void main(String...args){
+
+    }
 }
