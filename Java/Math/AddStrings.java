97ba1fb4ca338b5f4cf6669af8481cc4f2414591
08abhinav <abhinavnegi101@gmail.com>
2025-09-22 12:04:07 +0530

    implementation done

--- Java/Math/AddStrings.java
+++ Java/Math/AddStrings.java
@@ -6,7 +6,7 @@ public class AddStrings {
 
         while(i >=0 || j >= 0 || carry != 0){
             int digit1 = i >= 0 ? num1.charAt(i) - '0' : 0;
-            int digit2 = j >= 0 ? num1.charAt(j) - '0' : 0;
+            int digit2 = j >= 0 ? num2.charAt(j) - '0' : 0;
 
             int total = digit1 + digit2 + carry;
             carry = total / 10;
@@ -18,6 +18,8 @@ public class AddStrings {
         return res.reverse().toString();
     }
     public static void main(String[] args) {
-        
+        String num1 = "456"; 
+        String num2 = "77"; 
+        System.out.println(AddStrings.add(num1, num2));
     }
 }
