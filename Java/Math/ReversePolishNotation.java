b8b1aa8e1f44353c2e2e68ce113832d788738c54
08abhinav <abhinavnegi101@gmail.com>
2025-09-23 10:25:18 +0530

    implemented evalRPN method

--- Java/Math/ReversePolishNotation.java
+++ Java/Math/ReversePolishNotation.java
@@ -1,5 +1,29 @@
+import java.util.Stack;
+
 public class ReversePolishNotation {
+    public static int evalRPN(String[] tokens){
+        Stack<Integer> stk = new Stack<>();
+        for(String i : tokens){
+            if(i.equals("+") || i.equals("-") || i.equals("*") || i.equals("/")){
+                int b = stk.pop();
+                int a = stk.pop();
+                int res = 0;
+
+                switch (i) {
+                    case "+": res = a + b; break; 
+                    case "-": res = a - b; break; 
+                    case "*": res = a * b; break; 
+                    case "/": res = a / b; break; 
+                }
+
+                stk.push(res);
+            }else{
+                stk.push(Integer.parseInt(i));
+            }
+        }
+        return stk.pop();
+    }
     public static void main(String...args){
-        
+
     }
 }
