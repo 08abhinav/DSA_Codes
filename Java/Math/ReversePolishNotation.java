2298d626a1602275bb9493f0a2a15fb611bc0757
08abhinav <abhinavnegi101@gmail.com>
2025-09-23 10:32:23 +0530

    modified the code with if else ladder

--- Java/Math/ReversePolishNotation.java
+++ Java/Math/ReversePolishNotation.java
@@ -14,20 +14,37 @@ import java.util.Stack;
 public class ReversePolishNotation {
     public static int evalRPN(String[] tokens){
         Stack<Integer> stk = new Stack<>();
+        // for(String i : tokens){
+        //     if(i.equals("+") || i.equals("-") || i.equals("*") || i.equals("/")){
+        //         int b = stk.pop();
+        //         int a = stk.pop();
+        //         int res = 0;
+
+        //         switch (i) {
+        //             case "+": res = a + b; break; 
+        //             case "-": res = a - b; break; 
+        //             case "*": res = a * b; break; 
+        //             case "/": res = a / b; break; 
+        //         }
+
+        //         stk.push(res);
+        //     }else{
+        //         stk.push(Integer.parseInt(i));
+        //     }
+        // }
+        // return stk.pop();
+
         for(String i : tokens){
-            if(i.equals("+") || i.equals("-") || i.equals("*") || i.equals("/")){
-                int b = stk.pop();
-                int a = stk.pop();
-                int res = 0;
-
-                switch (i) {
-                    case "+": res = a + b; break; 
-                    case "-": res = a - b; break; 
-                    case "*": res = a * b; break; 
-                    case "/": res = a / b; break; 
-                }
-
-                stk.push(res);
+            if(i.equals("+")){
+                stk.push(stk.pop() + stk.pop());
+            }else if(i.equals("-")){
+                int b = stk.pop(), a = stk.pop();
+                stk.push(a - b);
+            }else if(i.equals("*")){
+                stk.push(stk.pop() * stk.pop());
+            }else if(i.equals("/")){
+                int b = stk.pop(), a = stk.pop();
+                stk.push(a / b);
             }else{
                 stk.push(Integer.parseInt(i));
             }
