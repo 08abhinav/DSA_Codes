f309551819fbc8d696bd70f2c0924bb3c9cb1d6e
08abhinav <abhinavnegi101@gmail.com>
2025-09-24 10:55:26 +0530

    implemented makeGood method

--- Java/StringQues/GreatString.java
+++ Java/StringQues/GreatString.java
@@ -1,4 +1,21 @@
-public class GreatSting(){
+import java.util.Stack;
+
+public class GreatString{
+    public static String makeGood(String s){
+        Stack<Character> stk = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(!stk.isEmpty() && Math.abs(stk.peek() - ch) == 32){
+                stk.pop();
+            }else{
+                stk.push(ch);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c : stk){
+            sb.append(c);
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
         
     }
