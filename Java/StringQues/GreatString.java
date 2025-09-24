05ace4a46f0b31189f8e731b4516e6a8c21bc27f
08abhinav <abhinavnegi101@gmail.com>
2025-09-24 11:01:14 +0530

    implemented using another approach

--- Java/StringQues/GreatString.java
+++ Java/StringQues/GreatString.java
@@ -2,19 +2,33 @@ import java.util.Stack;
 
 public class GreatString{
     public static String makeGood(String s){
-        Stack<Character> stk = new Stack<>();
-        for(char ch : s.toCharArray()){
-            if(!stk.isEmpty() && Math.abs(stk.peek() - ch) == 32){
-                stk.pop();
+        // Approach 1 using stack
+        // Stack<Character> stk = new Stack<>();
+        // for(char ch : s.toCharArray()){
+        //     if(!stk.isEmpty() && Math.abs(stk.peek() - ch) == 32){
+        //         stk.pop();
+        //     }else{
+        //         stk.push(ch);
+        //     }
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for(char c : stk){
+        //     sb.append(c);
+        // }
+        // return sb.toString();
+
+        // Approach 2 using StringBuilder
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            int len = res.length();
+            if(len > 0 && (res.charAt(len - 1) + 32 == c || res.charAt(len - 1) - 32 == c)){
+                res.deleteCharAt(len - 1);
             }else{
-                stk.push(ch);
+                res.append(c);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for(char c : stk){
-            sb.append(c);
-        }
-        return sb.toString();
+        return res.toString();
     }
     public static void main(String[] args) {
         String s = "leEeetcode";
