6d9eff88ee4d00fd1d990a876b4b23dff2e07dd6
08abhinav <abhinavnegi101@gmail.com>
2025-09-25 10:30:15 +0530

    implementation done

--- Golang/Math/TwoPointers/HappyNumber.go
+++ Golang/Math/TwoPointers/HappyNumber.go
@@ -1,24 +1,31 @@
 package twopointers
 
+import "fmt"
+
 func isHappyNumber(n int) bool {
 	slow, fast := n, n
-	for{
+	for {
 		slow = square(slow)
 		fast = square(square(fast))
 
-		if slow == fast{
-			break;
+		if slow == fast {
+			break
 		}
 	}
 	return slow == 1
 }
 
-func square(n int)int{
+func square(n int) int {
 	ans := 0
-	for n > 0{
+	for n > 0 {
 		rem := n % 10
 		ans += rem * rem
 		n /= 10
 	}
 	return n
+}
+
+func main() {
+	n := 19
+	fmt.Println(isHappyNumber(n))
 }
