07cd229f4ea5ef0d3b114adf20d17f546718bce6
08abhinav <abhinavnegi101@gmail.com>
2025-09-25 10:22:39 +0530

    implemented isHappyNumber method

--- Java/Math/TwoPointers/HappyNumber.java
+++ Java/Math/TwoPointers/HappyNumber.java
@@ -1,4 +1,15 @@
+package Java.Math.TwoPointers;
 class HappyNumber{
+    public static boolean isHappyNumber(int n){
+        int slow = n;
+        int fast = n;
+        do{
+            slow = square(slow);
+            fast = square(square(fast));
+        }while(slow != fast);
+
+        return slow == 1;
+    }
     public static void main(String[] args) {
         
     }
