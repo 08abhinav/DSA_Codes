924a4e49ec9f86e224448330227371a60039417e
08abhinav <abhinavnegi101@gmail.com>
2025-09-25 12:01:56 +0530

    implemented mergeArray method

--- Java/Array/MergeTwoSorted.java
+++ Java/Array/MergeTwoSorted.java
@@ -1,5 +1,29 @@
 package Java.Array;
 
 public class MergeTwoSorted {
-    
+    public static void mergeArrays(int[] nums1, int m, int nums2[], int n){
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while(i >=0 && j >=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i -= 1;
+            }else{
+                nums1[k] = nums2[j];
+                j -= 1;
+            }
+            k -= 1;
+        }
+
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            k -= 1;
+            j -= 1;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
 }
