524377d315508fd9c836da3c6fb8dd9b1c79c629
08abhinav <abhinavnegi101@gmail.com>
2025-09-25 10:24:16 +0530

    implemented helper method square

--- Java/Math/TwoPointers/HappyNumber.java
+++ Java/Math/TwoPointers/HappyNumber.java
@@ -10,6 +10,16 @@ class HappyNumber{
 
         return slow == 1;
     }
+
+    public static int square(int n ){
+        int ans = 0;
+        while(n > 0){
+            int rem = n % 10;
+            ans += rem * rem;
+            n /= 10;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         
     }
