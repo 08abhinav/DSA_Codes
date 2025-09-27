c6cfbd2adcd7e21c3f604dba74195bf4bc7f227c
08abhinav <abhinavnegi101@gmail.com>
2025-09-27 20:47:24 +0530

    implementation done

--- Java/Math/DivideTwoInt.java
+++ Java/Math/DivideTwoInt.java
@@ -29,7 +29,7 @@ class DivideTwoInt{
         return ans*sign;
     }
     public static void main(String[] args) {
-        
-        
+        int dividend = 10, divisor = 3;
+        System.out.println(DivideTwoInt.divide(dividend, divisor));
     }
 }
