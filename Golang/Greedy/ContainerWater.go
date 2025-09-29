9d2a76f18efb46afe14b3b0cc00ce47d21087ab2
08abhinav <abhinavnegi101@gmail.com>
2025-09-29 11:19:26 +0530

    implemented maxArea method

--- Golang/Greedy/ContainerWater.go
+++ Golang/Greedy/ContainerWater.go
@@ -1,3 +1,14 @@
 func maxArea(height []int)int{
-	
+	l, res, r := 0, 0, len(height)
+
+	for l < r{
+		area := min(height[l], height[r]) * (r - l)
+		res : max(area, res)
+
+		if height[l] > height[r]{
+			r -= 1
+		}else{
+			l += 1
+		}
+	}
 }
