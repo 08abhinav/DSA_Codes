4f726f15ccb40f4c085f68d92df605238753528e
08abhinav <abhinavnegi101@gmail.com>
2025-09-29 11:14:34 +0530

    minor update

--- Golang/Array/RotateArray.go
+++ Golang/Array/RotateArray.go
@@ -8,7 +8,7 @@ func rotate(nums []int, k int) {
 	n := len(nums)
 	temp := make([]int, n)
 	for i := 0; i < n; i++ {
-		temp[(i+k)%n] = nums[i]
+		temp[(i + k) % n] = nums[i]
 	}
 
 	for i := 0; i < n; i++ {
