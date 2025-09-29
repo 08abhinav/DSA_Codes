fc71fd5fd61816e2170f29c3129c70bb0aaadeb4
08abhinav <abhinavnegi101@gmail.com>
2025-09-29 11:13:19 +0530

    implementation done

--- Golang/Array/RotateArray.go
+++ Golang/Array/RotateArray.go
@@ -1,11 +1,22 @@
-func rotate(nums []int, k int){
+package main
+
+import "fmt"
+
+func rotate(nums []int, k int) {
 	n := len(nums)
 	temp := make([]int, n)
-	for i := 0; i < n; i++{
-		temp[(i + k) % n] = nums[i]
+	for i := 0; i < n; i++ {
+		temp[(i+k)%n] = nums[i]
 	}
 
-	for i := 0; i < n; i++{
+	for i := 0; i < n; i++ {
 		nums[i] = temp[i]
 	}
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	k := 4
+	rotate(nums, k)
+	fmt.Print(nums)
 }
