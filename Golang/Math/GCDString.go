913c7ea6cf39bea7fb7c3cd3759faa0b72fcd793
08abhinav <abhinavnegi101@gmail.com>
2025-10-01 12:10:42 +0530

    implementation done

--- Golang/Math/GCDString.go
+++ Golang/Math/GCDString.go
@@ -1,14 +1,24 @@
-func gcdOfStrings(str1, str2 string)string{
-	if str1 + str2 != str2 + str1{
+package main
+
+import "fmt"
+
+func gcdOfStrings(str1, str2 string) string {
+	if str1+str2 != str2+str1 {
 		return ""
 	}
 	g := gcd(len(str1), len(str2))
 	return str1[:g]
 }
 
-func gcd(a, b int)int{
-	if b == 0{
+func gcd(a, b int) int {
+	if b == 0 {
 		return a
 	}
-	return gcd(b, a % b)
+	return gcd(b, a%b)
+}
+
+func main() {
+	str1 := "ABABAB"
+	str2 := "AB"
+	fmt.Println(gcdOfStrings(str1, str2))
 }
