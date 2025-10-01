d280efaa2cc2db2163b3945ae160b1ccc9ebeddd
08abhinav <abhinavnegi101@gmail.com>
2025-10-01 11:46:22 +0530

    implemented converToTitle method

--- Java/Math/ExcelSheet.java
+++ Java/Math/ExcelSheet.java
@@ -1,5 +1,15 @@
 class ExcelSheet{
+    public static String convertToTitle(int columnNumber){
+        StringBuilder res = new StringBuilder();
+        while(columnNumber > 0){
+            columnNumber -= 1;
+            int rem = columnNumber % 26;
+            res.append((char)('A' + rem));
+            columnNumber /= 26;
+        }
+        return res.reverse().toString();
+    }
     public static void main(String... args){
-        
+
     }
 }
