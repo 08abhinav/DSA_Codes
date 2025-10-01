840d4e09f3c4eb08779cc4b729d2d30aad3a349d
08abhinav <abhinavnegi101@gmail.com>
2025-10-01 12:14:04 +0530

    implemented gcdOfStrings method

--- Java/Math/GcdStrings.java
+++ Java/Math/GcdStrings.java
@@ -1,4 +1,10 @@
 public class GcdStrings {
+    public static String gcdOfStrings(String str1, String str2){
+        if(!(str1 + str2).equals(str2 + str1)) return "";
+
+        int g = gcd(str1.length(), str2.length());
+        return str2.substring(0, g);
+    }
     public static void main(String[] args) {
         
     }
