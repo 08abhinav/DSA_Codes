72e03fb5884ba63830c1603d09bab020474f9a5f
08abhinav <abhinavnegi101@gmail.com>
2025-10-01 12:16:40 +0530

    added question

--- Java/Math/GcdStrings.java
+++ Java/Math/GcdStrings.java
@@ -1,3 +1,8 @@
+/*1071. Greatest Common Divisor of Strings
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t 
+(i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2. */
 public class GcdStrings {
     public static String gcdOfStrings(String str1, String str2){
         if(!(str1 + str2).equals(str2 + str1)) return "";
