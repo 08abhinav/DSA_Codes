759b8257bee9e5b10c641b29d2701c7c28caedec
08abhinav <abhinavnegi101@gmail.com>
2025-10-02 20:17:23 +0530

    implemented numWaterBottles method

--- Java/Math/WaterBottles.java
+++ Java/Math/WaterBottles.java
@@ -1,5 +1,17 @@
 class WaterBottles{
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int drinked = 0;
+        int empty = 0;
+        while(numBottles > 0){
+            drinked += numBottles;
+            empty += numBottles;
+
+            numBottles = empty / numExchange;
+            empty = empty % numExchange;
+        }
+        return drinked;
+    }
     public static void main(String... args){
-        
+
     }
 }
