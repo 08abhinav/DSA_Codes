b11e8fe2785c603a80ba0c3ff209c655b8c51d68
08abhinav <abhinavnegi101@gmail.com>
2025-10-03 17:47:10 +0530

    implemented maxBottleDrunk method

--- Java/Math/WaterBottleII.java
+++ Java/Math/WaterBottleII.java
@@ -1,4 +1,14 @@
 class WaterBottleII{
+    public static int maxBottleDrunk(int numBottles, int numExchange){
+        int empty = numBottles;
+        while(empty >= numExchange){
+            empty -= numExchange;
+            numBottles += 1;
+            numExchange += 1;
+            empty += 1;
+        }
+        return numBottles;
+    }
     public static void main(String... ags){
 
     }
