ecf031ae0bbf5086d46d9c67227927fb63c7861d
08abhinav <abhinavnegi101@gmail.com>
2025-10-03 17:53:20 +0530

    implementation done

--- Golang/Math/WaterBottleII.go
+++ Golang/Math/WaterBottleII.go
@@ -1,10 +1,19 @@
-func maxWaterBottles(numBottles, numExchange int)int  {
+package main
+
+import "fmt"
+
+func maxWaterBottles(numBottles, numExchange int) int {
 	empty := numBottles
-	for empty >= numExchange{
+	for empty >= numExchange {
 		empty -= numExchange
 		numBottles += 1
 		numExchange += 1
 		empty += 1
 	}
 	return numBottles
+}
+
+func main() {
+	numBottles, numExchange := 13, 6
+	fmt.Println(maxWaterBottles(numBottles, numExchange))
 }
