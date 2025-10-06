574d6c2b6621a047f2ade27b454cd1cf18049970
08abhinav <abhinavnegi101@gmail.com>
2025-10-06 21:20:41 +0530

    implemented minEatingSpeed method

--- Java/BinarySearch/KokoEating.java
+++ Java/BinarySearch/KokoEating.java
@@ -1,4 +1,26 @@
+import java.util.Arrays;
+
 class KokoEating{
+    public static int minEatingSpped(int[] piles, int h){
+        int low = 1;
+        int high = Arrays.stream(piles).max().getAsInt();
+
+        while(low <= high){
+            int k = low + (high - low) / 2;
+
+            long totalTime = 0;
+            for(int i : piles){
+                totalTime += Math.ceil((double) i / k);
+            }
+
+            if(totalTime <= h){
+                high = k - 1;
+            }else{
+                low = k + 1;
+            }
+        }
+        return low;
+    }
     public static void main(String[] args) {
         
     }
