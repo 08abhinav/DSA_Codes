2e27f2aeba294138dda24bae1e7454f50a13e9f5
08abhinav <abhinavnegi101@gmail.com>
2025-10-09 09:43:42 +0530

    implemented binarySearch method

--- Golang/BinarySearch/SuccessfulPairs.go
+++ Golang/BinarySearch/SuccessfulPairs.go
@@ -10,11 +10,28 @@ func successfulPairs(potions, spells []int, success int64) []int{
     res := make([]int, m)
 
     for i := 0; i < len(spells); i++{
-        idx := binarySearch(potions, spells[i], success)
+        strength := (int64)(spells[i])
+        idx := binarySearch(potions, strength, success)
         
         if idx != -1{
             res[i] = m - idx
         }
     }
     return res
+}
+
+func binarySearch(potions []int, strength, success int64) int{
+    idx, low, high := -1, 0, len(potions) - 1
+    
+    for low <= high{
+        mid := low + (high - low) / 2
+        
+        if(potions[mid] * int(strength) >= int(success)){
+            idx = mid
+            high = mid - 1
+        }else{
+            low = mid + 1
+        }
+    }
+    return idx
 }
