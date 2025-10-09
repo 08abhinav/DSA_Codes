d1cd890256b563f2b1a91eb97777f199d4e5bb2b
08abhinav <abhinavnegi101@gmail.com>
2025-10-09 09:51:00 +0530

    implemented binarySearch method

--- Java/BinarySearch/SuccessfulPairs.java
+++ Java/BinarySearch/SuccessfulPairs.java
@@ -17,6 +17,22 @@ public class SuccessfulPairs {
         }
         return res;
     }
+
+    public static int binarySearch(int[] potions, long strength, long success){
+        int idx = -1;
+        int low = 0, high = potions.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(potions[mid] * strength >= success){
+                idx = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return idx;
+    }
+
     public static void main(String... args){
 
     }
