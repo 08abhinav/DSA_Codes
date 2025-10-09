792dda73a73a550412729e65e1c15ee7b8779fb7
08abhinav <abhinavnegi101@gmail.com>
2025-10-09 09:39:52 +0530

    implemented successfulPairs function

--- Golang/BinarySearch/SuccessfulPairs.go
+++ Golang/BinarySearch/SuccessfulPairs.go
@@ -1,3 +1,20 @@
-func successfulPairs(potions, spells []int, success long)int[]{
-    
+package main
+import (
+    "fmt"
+    "sort"
+)
+
+func successfulPairs(potions, spells []int, success int64) []int{
+    sort.Ints(potions)
+    m := len(potions)
+    res := make([]int, m)
+
+    for i := 0; i < len(spells); i++{
+        idx := binarySearch(potions, spells[i], success)
+        
+        if idx != -1{
+            res[i] = m - idx
+        }
+    }
+    return res
 }
