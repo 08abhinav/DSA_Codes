643423b4a4b58e0bcb845c33e91d09141f8854eb
08abhinav <abhinavnegi101@gmail.com>
2025-10-09 09:49:12 +0530

    implemented successPairs method

--- Java/BinarySearch/SuccessfulPairs.java
+++ Java/BinarySearch/SuccessfulPairs.java
@@ -1,7 +1,23 @@
 package Java.BinarySearch;
 
+import java.util.Arrays;
+
 public class SuccessfulPairs {
+    public static int[] successPairs(int[] potions, int[] spells, long success){
+        Arrays.sort(potions);
+        int m = potions.length;
+        int n = spells.length;
+        int[] res = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int idx = binarySearch(potions, spells[i], success);
+            if(idx != -1){
+                res[i] = m - i;
+            }
+        }
+        return res;
+    }
     public static void main(String... args){
-        
+
     }
 }
