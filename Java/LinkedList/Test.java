addfbdb2ea8b018f56498453c892d5df294bf8a0
08abhinav <abhinavnegi101@gmail.com>
2025-10-10 10:28:21 +0530

    implemented getMiddle method

--- Java/LinkedList/Test.java
+++ Java/LinkedList/Test.java
@@ -1,16 +1,29 @@
 class Test{
     public static void main(String...str){
-        RemoveDuplicate rd = new RemoveDuplicate();
-        rd.insert(10);
-        rd.insert(10);
-        rd.insert(10);
-        rd.insert(40);
-        rd.insert(50);
-        System.out.println("Original List");
-        rd.display();
+        //Singly Linked list
+        L1 obj = new L1();
+        obj.insert(10);
+        obj.insert(20);
+        obj.insert(30);
+        obj.insert(40);
+        obj.insert(50);
 
-        rd.head = rd.removeDuplicate(rd.head);
-        System.out.println("After removing duplicates");
-        rd.display();
+        obj.display();
+
+        int x = obj.getMiddle(obj.getHead());
+        System.out.println("\nMiddle of linked list: " + x);
+
+
+        //Doubly Linked list
+        // DoublyLinkedList dl = new DoublyLinkedList();
+        // int[] arr = {1, 2, 3, 4, 5};
+
+        // DoublyLinkedList.Node head = dl.arrayToDoublyLL(arr);
+        // dl.display(head);
+
+        // System.out.println("After Reversing");
+        // DoublyLinkedList.Node newHead = dl.reverseDLL(head);
+        // dl.display(newHead);
+        
     }
 }
