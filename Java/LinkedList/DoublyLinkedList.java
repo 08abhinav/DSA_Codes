04fda098da51e57b227ec8333ef273497c534ea5
08abhinav <abhinavnegi101@gmail.com>
2025-10-10 10:01:45 +0530

    implemented doubly linkedl list

---
+++ Java/LinkedList/DoublyLinkedList.java
@@ -0,0 +1,42 @@
+class DoublyLinkedList{
+    class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int val){
+            data = val;
+            next = null;
+            prev = null;
+        }
+
+        public Node(int data1, Node next1, Node prev1){
+            data = data1;
+            next = next1;
+            prev = prev1;
+        }
+    }
+
+    public Node arrayToDoublyLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for(int i = 1; i < arr.length; i++){
+            Node temp = new Node(arr[i], null, prev);
+            prev.next = temp;
+            prev = temp;
+        }
+        return head;
+    }
+
+    public void display(Node head){
+        if(head == null){
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
