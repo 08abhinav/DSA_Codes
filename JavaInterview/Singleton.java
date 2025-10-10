2a3f4df297a2db1f808c03d9335c23f741951a96
08abhinav <abhinavnegi101@gmail.com>
2025-10-10 20:00:51 +0530

    implemented the solution

--- JavaInterview/Singleton.java
+++ JavaInterview/Singleton.java
@@ -1,3 +1,5 @@
+package JavaInterview;
+
 /*Most asked question in java interviews about singleton pattern in java.
  * The purpose of singleton pattern is to ensure that only one instance of a class is created and provide a global
 acess point ot that instance.
@@ -6,4 +8,18 @@ acess point ot that instance.
 instance is easily accessible from anywhere in our application. 
 
  * This is where the Singleton Desgin Pattern roles comes to the rescue.
- */
+*/
+
+class Singleton{
+    private static Singleton instance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
