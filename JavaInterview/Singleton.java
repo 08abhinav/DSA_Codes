10ba835a92a7a89fef53ca774aae5983d56dd0e2
08abhinav <abhinavnegi101@gmail.com>
2025-10-10 20:47:42 +0530

    updated

--- JavaInterview/Singleton.java
+++ JavaInterview/Singleton.java
@@ -7,7 +7,13 @@ acess point ot that instance.
  * In other words we can say that sometime we need to ensure that a class has only one instance and that
 instance is easily accessible from anywhere in our application. 
 
+ * So in singleton pattern design we focus on like our class should only have at most object if we try to create a new
+ object it should return that same object only
  * This is where the Singleton Desgin Pattern roles comes to the rescue.
+ * Use Case
+ -> Logging
+ -> Database connectivity
+  -> cache
 */
 
 class Singleton{
@@ -15,6 +21,7 @@ class Singleton{
 
     private Singleton(){}
 
+    // This Solution is not thread Safe
     public static Singleton getInstance(){
         if(instance == null){
             instance = new Singleton();
