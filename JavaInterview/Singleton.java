6df7e6a9fcee00ac1a037eeff5a5597bad4d1408
08abhinav <abhinavnegi101@gmail.com>
2025-10-10 19:58:04 +0530

    implementing most asked interview question Singleton Design Pattern Implementation in java

---
+++ JavaInterview/Singleton.java
@@ -0,0 +1,9 @@
+/*Most asked question in java interviews about singleton pattern in java.
+ * The purpose of singleton pattern is to ensure that only one instance of a class is created and provide a global
+acess point ot that instance.
+
+ * In other words we can say that sometime we need to ensure that a class has only one instance and that
+instance is easily accessible from anywhere in our application. 
+
+ * This is where the Singleton Desgin Pattern roles comes to the rescue.
+ */
