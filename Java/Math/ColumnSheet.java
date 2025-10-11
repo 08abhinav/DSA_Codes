139e5a0709801ac6ebfa5de8328b2cf058ada763
08abhinav <abhinavnegi101@gmail.com>
2025-10-11 22:25:15 +0530

    implemented titleNumber method

--- Java/Math/ColumnSheet.java
+++ Java/Math/ColumnSheet.java
@@ -1,4 +1,11 @@
 public class ColumnSheet {
+    public static int titleNumber(String columnTitle){
+        int res = 0;
+        for(int i = 0; i < columnTitle.length(); i++){
+            res = res * 26 + (columnTitle.charAt(i) - 'A' + 1);
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
