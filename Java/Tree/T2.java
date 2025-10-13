2796a99ffb3fdad000c288e6ab03d6fcc127fe6e
08abhinav <abhinavnegi101@gmail.com>
2025-10-13 10:39:25 +0530

    Implemented preOrder method without recursion

--- Java/Tree/T2.java
+++ Java/Tree/T2.java
@@ -1,11 +1,39 @@
-public class T2 {
-    private int data;
-    private T2 left;
-    private T2 right;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
     
-    T2(int x){
+    Node(int x){
         data = x;
         left = null;
         right = null;
+    }
+}
+
+public class T2{
+    // Preorder Traversal of a binary tree
+    public List<Integer> preOrder(Node root){
+        Stack<Node> stk = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+
+        if(root == null) return res;
+        stk.push(root);
+
+        while(!stk.isEmpty()){
+            root = stk.pop();
+            res.add(root.data);
+
+            if(root.left != null){
+                stk.push(root.left);
+            }
+            if(root.right != null){
+                stk.push(root.right);
+            }
+        }
+        return res;
     }
 }
