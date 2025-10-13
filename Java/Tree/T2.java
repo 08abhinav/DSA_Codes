5553494790ab6a59cf132eb2425ee9f67246f65c
08abhinav <abhinavnegi101@gmail.com>
2025-10-13 10:29:39 +0530

    implementing binary tree

---
+++ Java/Tree/T2.java
@@ -0,0 +1,11 @@
+public class T2 {
+    private int data;
+    private T2 left;
+    private T2 right;
+    
+    T2(int x){
+        data = x;
+        left = null;
+        right = null;
+    }
+}
