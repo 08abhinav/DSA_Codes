23cce1876d2a3aa1b808f4380d2459eae8577e9c
08abhinav <abhinavnegi101@gmail.com>
2025-10-15 20:24:52 +0530

    updated solution

--- Java/Array/LongSubarray.java
+++ Java/Array/LongSubarray.java
@@ -6,40 +6,40 @@ import java.util.Map;
 
 public class LongSubarray{
     // Approach 1
-    public static int longestSubarr(int[] arr, int k){
-        int res = 0;
-        for(int i=0; i<arr.length; i++){
-            int sum = 0;
-            for(int j=i; j<arr.length; j++){
-                sum += arr[j];
-
-                if(sum == k){
-                    int subLen = j - i + 1;
-                    res = Math.max(res, subLen);
-                }
-            }
-        }
-        return res;
-    }
-    // Approach 2
     // public static int longestSubarr(int[] arr, int k){
-    //     Map<Integer, Integer> map = new HashMap<>();
-    //     int res = 0, prefix = 0;
-
+    //     int res = 0;
     //     for(int i=0; i<arr.length; i++){
-    //         prefix += arr[i];
+    //         int sum = 0;
+    //         for(int j=i; j<arr.length; j++){
+    //             sum += arr[j];
 
-    //         if(prefix == k) res = i + 1;
-    //         else if(map.containsKey(prefix - k)){
-    //             res = Math.max(res, i - map.get(prefix - k));
-    //         }
-            
-    //         if(!map.containsKey(prefix)){
-    //                 map.put(prefix, i);
+    //             if(sum == k){
+    //                 int subLen = j - i + 1;
+    //                 res = Math.max(res, subLen);
+    //             }
     //         }
     //     }
     //     return res;
     // }
+    // Approach 2
+    public static int longestSubarr(int[] arr, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = 0, prefix = 0;
+
+        for(int i=0; i<arr.length; i++){
+            prefix += arr[i];
+
+            if(prefix == k) res = i + 1;
+            else if(map.containsKey(prefix - k)){
+                res = Math.max(res, i - map.get(prefix - k));
+            }
+            
+            if(!map.containsKey(prefix)){
+                    map.put(prefix, i);
+            }
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         int[] arr = {10, 5, 2, 7, 1, -10};
