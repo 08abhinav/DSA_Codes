d2accb85892419bd22989bfcfd57e8709418fece
08abhinav <abhinavnegi101@gmail.com>
2025-10-17 14:29:42 +0530

    implementing to check if given number is perfect square or not

---
+++ Java/BinarySearch/PerfectSquare.java
@@ -0,0 +1,15 @@
+class PerfectSquare{
+    // Approach 1: Brute Force
+    public static int floorSqrt(int n){
+        if(n < 2) return n;
+        int ans = 0;
+        for(int i = 2; i < n; i++){
+            if(i * i <= n){
+                ans = i;
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+}
