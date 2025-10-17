086e57e68046f32aed5132ed0e5e5bd065295297
08abhinav <abhinavnegi101@gmail.com>
2025-10-17 14:30:34 +0530

    implemented approach 1

--- Java/BinarySearch/PerfectSquare.java
+++ Java/BinarySearch/PerfectSquare.java
@@ -2,14 +2,14 @@ class PerfectSquare{
     // Approach 1: Brute Force
     public static int floorSqrt(int n){
         if(n < 2) return n;
-        int ans = 0;
+        int res = 0;
         for(int i = 2; i < n; i++){
             if(i * i <= n){
-                ans = i;
+                res = i;
             }else{
                 break;
             }
         }
-        return ans;
+        return res;
     }
 }
