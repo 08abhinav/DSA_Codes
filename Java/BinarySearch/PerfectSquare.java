aace881e4a11fbb6c96a74d75374a1cfd9af4b62
08abhinav <abhinavnegi101@gmail.com>
2025-10-17 14:35:09 +0530

    implemented the solution using binary search approach

--- Java/BinarySearch/PerfectSquare.java
+++ Java/BinarySearch/PerfectSquare.java
@@ -1,20 +1,36 @@
 class PerfectSquare{
     // Approach 1: Brute Force
+    // public static int floorSqrt(int n){
+    //     if(n < 2) return n;
+    //     int res = 0;
+    //     for(int i = 2; i < n; i++){
+    //         if(i * i <= n){
+    //             res = i;
+    //         }else{
+    //             break;
+    //         }
+    //     }
+    //     return res;
+    // }
+
+    // Approach 2: Optimal approach using Binary Search
     public static int floorSqrt(int n){
         if(n < 2) return n;
-        int res = 0;
-        for(int i = 2; i < n; i++){
-            if(i * i <= n){
-                res = i;
+        int left = 0, right = n / 2, ans = 0;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(mid * mid <= n){
+                ans = mid;
+                left = mid + 1;
             }else{
-                break;
+                right = mid - 1;
             }
         }
-        return res;
+        return ans;
     }
-
     public static void main(String... args){
         int n = 11;
-        System.out.println(floorSqrt(n));
+        System.out.println(PerfectSquare.floorSqrt(n));
     }
 }
