981d42a453bd4d0668d65fb427374d9cab11fec7
08abhinav <abhinavnegi101@gmail.com>
2025-10-19 19:22:56 +0530

    implemented findRelativeRank method

--- Java/Heaps/RelativeRanks.java
+++ Java/Heaps/RelativeRanks.java
@@ -1,4 +1,29 @@
+import java.util.PriorityQueue;
+
 public class RelativeRanks {
+    public String[] findRelativeRank(int[] score){
+        String[] res = new String[score.length];
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> score[b] - score[a]);
+        for(int i = 0; i < score.length; i++){
+            maxHeap.add(i);
+        }
+
+        int i = 1;
+        while(!maxHeap.isEmpty()){
+            if(i > 3){
+                res[i] = Integer.toString(i);
+            }else if(i == 0){
+                res[i] = "Gold Medal";
+            }else if(i == 1){
+                res[i] = "Silver Medal";
+            }else if(i == 2){
+                res[i] = "Bronze Medal";
+            }
+            i += 1;
+        }
+        return res;
+
+    }
     public static void main(String[] args) {
         
     }
