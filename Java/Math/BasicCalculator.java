9b17f7f5bbf622464891bf1effe168904209b23f
08abhinav <abhinavnegi101@gmail.com>
2025-10-21 19:53:42 +0530

    minor update

--- Java/Math/BasicCalculator.java
+++ Java/Math/BasicCalculator.java
@@ -3,6 +3,7 @@ Given a string s which represents an expression, evaluate this expression and re
 The integer division should truncate toward zero.
 You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
 Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval(). */
+
 import java.util.*;
 class BasicCalculator{
     public static int calculate(String s){
