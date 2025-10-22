b9287caa836bb277394111f2c6e5a6e1d76d1a85
08abhinav <abhinavnegi101@gmail.com>
2025-10-22 22:18:27 +0530

    implemented removeAnagram method

--- Java/StringQues/RemoveAnagrams.java
+++ Java/StringQues/RemoveAnagrams.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class RemoveAnagrams {
+    public static List<String> removeAnagrams(String[] words){
+        int n = words.length;
+        String[] ana = new String[n];
+
+        for(int i = 0; i < n; i++){
+            char[] ch = words[i].toCharArray();
+            Arrays.sort(ch);
+            ana[i] = new String(ch);
+        }
+        List<String> res = new ArrayList<>();
+        res.add(words[0]);
+
+        for(int i = 1; i < n; i++){
+            if(!ana[i - 1].equals(i)){
+                res.add(words[i]);
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
