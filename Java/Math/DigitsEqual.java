307b29d6200463543ae612c7041f60a147bb11ab
08abhinav <abhinavnegi101@gmail.com>
2025-10-23 17:42:30 +0530

    implemented hasSameDigit mehtod

--- Java/Math/DigitsEqual.java
+++ Java/Math/DigitsEqual.java
@@ -1,4 +1,16 @@
 public class DigitsEqual{
+    public static boolean hasSameDigits(String s){
+        int iterations = 0;
+        char[] arr = s.toCharArray();
+
+        while (arr.length - iteration != 2) {
+            for (int i = 0; i < arr.length - 1 - iteration; i++) {
+                arr[i] = (char)(((arr[i] - '0') + (arr[i + 1] - '0')) % 10 + '0');
+            }
+            iteration++;
+        }
+        return arr[0] == arr[1];
+    }
     public static void main(String[] args) {
         
     }
