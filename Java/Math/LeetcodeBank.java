ef3c4ef4e4d98ac61056c94ff29a48c882a143b6
08abhinav <abhinavnegi101@gmail.com>
2025-10-25 10:54:53 +0530

    implemented totalMoney method

--- Java/Math/LeetcodeBank.java
+++ Java/Math/LeetcodeBank.java
@@ -1,5 +1,10 @@
 public class LeetcodeBank {
+    public static int totalMoney(int n){
+        int w = n / 7;
+        int d = n % 7;
+        return (w * (49 + 7 * w) + d * (2 * w + d + 1)) / 2;
+    }
     public static void main(String... args){
-        
+
     }
 }
