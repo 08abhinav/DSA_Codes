70288298f4fd63be8fdc2971c97e2b97ac23c9d0
08abhinav <abhinavnegi101@gmail.com>
2025-10-25 11:00:42 +0530

    implementation done

--- Golang/Math/LeetcodeBank.go
+++ Golang/Math/LeetcodeBank.go
@@ -1,4 +1,13 @@
-func totalMoney(n int)int{
-	w, d := n / 7, n % 7
-	return (w * (49 + 7 * w) + d * (2 * w + d + 1)) / 2
+package main
+
+import "fmt"
+
+func totalMoney(n int) int {
+	w, d := n/7, n%7
+	return (w*(49+7*w) + d*(2*w+d+1)) / 2
+}
+
+func main() {
+	n := 4
+	fmt.Println(totalMoney(n))
 }
