56b37c0a3b71c80e63e3d6377380a4bd53e0c03e
08abhinav <abhinavnegi101@gmail.com>
2025-10-26 11:34:06 +0530

    implemented kDistinct method

--- Java/Array/SlidingWindow/SubarrayKDifferentInt.java
+++ Java/Array/SlidingWindow/SubarrayKDifferentInt.java
@@ -1,4 +1,7 @@
 public class SubarrayKDifferentInt{
+    public static int kDistinct(int[] nums, int k){
+        return atMostK(nums, k) - atMostK(nums, k - 1);
+    }
     public static void main(String[] args) {
         
     }
