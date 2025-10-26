cf0e6b7f0c11ad1ebfbdbc9dcbf2027da17cbfcf
08abhinav <abhinavnegi101@gmail.com>
2025-10-26 11:37:40 +0530

    implementing helper method atMostK

--- Java/Array/SlidingWindow/SubarrayKDifferentInt.java
+++ Java/Array/SlidingWindow/SubarrayKDifferentInt.java
@@ -1,6 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarrayKDifferentInt{
     public static int kDistinct(int[] nums, int k){
         return atMostK(nums, k) - atMostK(nums, k - 1);
+    }
+
+    public static int atMostK(int[] nums, int K){
+        int left = 0, right = 0, res = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+        
+        while(right < nums.length){
+            count.put(nums[right], count.getOrDefault(count, 0) + 1);
+
+            while(count.size() > K){
+                count.put(nums[left], count.get(nums[left]) - 1);
+                if(count.get(nums[left]) == 0){
+                    count.remove(nums[left]);
+                }
+                left += 1;
+            }
+            res += right - left  + 1;
+            right += 1;
+        }
+        return res;
     }
     public static void main(String[] args) {
         
