4dad891bf0f12e0761532972897cc867fc71394c
08abhinav <abhinavnegi101@gmail.com>
2025-10-27 11:56:09 +0530

    implemented absDiff method

--- Java/Hashmap/EvenOddChars.java
+++ Java/Hashmap/EvenOddChars.java
@@ -1,4 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class EvenOddChars{
+    public static int absDiff(String s){
+        Map<Character, Integer> freq = new HashMap<>();
+        
+        for(char c : s.toCharArray()){
+            freq.put(c, freq.getOrDefault(freq, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i : freq.values()){
+            if(i % 2 == 0){
+                min = Math.min(i, min);
+            }
+            if(i % 2 != 0){
+                max = Math.max(i, max);
+            }
+        }
+        return Math.abs(max - min);
+    }
     public static void main(String[] args) {
         
     }
