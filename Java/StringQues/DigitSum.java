fc6cdb100ecd998c2293691dfb1eea68584681a6
08abhinav <abhinavnegi101@gmail.com>
2025-10-28 21:45:12 +0530

    implemented digitSum method

--- Java/StringQues/DigitSum.java
+++ Java/StringQues/DigitSum.java
@@ -1,5 +1,19 @@
 public class DigitSum{
+    public static String digitSum(String s, int k){
+        while(s.length() > k){
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < s.length(); i += k){
+                int sum = 0;
+                for(int j = i; j < Math.min(i + k, s.length()); j++){
+                    sum += s.charAt(j) - '0';
+                }
+                sb.append(sum);
+            }
+            s = sb.toString();
+        }
+        return s;
+    }
     public static void main(String...args){
-        
+
     }
 }
