3c2add3fe56662568711666e8f3d115c104bb0b1
08abhinav <abhinavnegi101@gmail.com>
2025-10-29 10:31:29 +0530

    implemented smallestNum method using first approach

--- Java/BitManipulation/SetBits.java
+++ Java/BitManipulation/SetBits.java
@@ -1,5 +1,13 @@
 public class SetBits {
+    // Approach 1
+    public static int smallestNum(int n){
+        for(int i = 0; i <= n; i++){
+            int z = 1 << i;
+            if(z > n) return z - 1;
+        }
+        return 0;
+    }
     public static void main(String...args){
-        
+
     }
 }
