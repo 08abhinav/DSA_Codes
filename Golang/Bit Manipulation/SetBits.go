33b4d3cb300342ea9e1d89633476718c853d875a
08abhinav <abhinavnegi101@gmail.com>
2025-10-29 10:40:38 +0530

    implemented smallNum method using second approach

--- Golang/Bit Manipulation/SetBits.go
+++ Golang/Bit Manipulation/SetBits.go
@@ -1,13 +1,20 @@
 package main
 
 // Approach 1
-func smallNum(n int)int{
-	for i := 0; i <= n; i++{
-		z := 1 << i
-		if z > n{
-			return z - 1
-		}
-	}
-	return 0
-}
+// func smallNum(n int)int{
+// 	for i := 0; i <= n; i++{
+// 		z := 1 << i
+// 		if z > n{
+// 			return z - 1
+// 		}
+// 	}
+// 	return 0
+// }
 
+// Approach 2
+func smallNum(n int)int{
+	for (n & (n + 1)) != 0{
+		n |= n + 1
+	}
+	return n
+}
