72e49d6143832644ae5f298de23d1f9babbd2c4a
08abhinav <abhinavnegi101@gmail.com>
2025-10-30 21:02:19 +0530

    implemented approach 1

--- Java/stack/RemoveStars.java
+++ Java/stack/RemoveStars.java
@@ -1,4 +1,22 @@
+import java.util.Stack;
+
 public class RemoveStars{
+    // Approach 1
+    public static String removeStar(String s){
+        Stack<Character> stk = new Stack<>();
+        for(Character c : s.toCharArray()){
+            if(c == '*'){
+                stk.pop();
+            }else{
+                stk.push(c);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        while(!stk.isEmpty()){
+            res.append(stk.pop());
+        }
+        return res.reverse().toString();
+    }
     public static void main(String[] args) {
         
     }
