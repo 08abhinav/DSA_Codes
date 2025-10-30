2a08bc23478e4e1e984c563565831fc34b61da95
08abhinav <abhinavnegi101@gmail.com>
2025-10-30 21:04:23 +0530

    implemented approach 2

--- Java/stack/RemoveStars.java
+++ Java/stack/RemoveStars.java
@@ -1,21 +1,39 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class RemoveStars{
     // Approach 1
+    // public static String removeStar(String s){
+    //     Stack<Character> stk = new Stack<>();
+    //     for(Character c : s.toCharArray()){
+    //         if(c == '*'){
+    //             stk.pop();
+    //         }else{
+    //             stk.push(c);
+    //         }
+    //     }
+    //     StringBuilder res = new StringBuilder();
+    //     while(!stk.isEmpty()){
+    //         res.append(stk.pop());
+    //     }
+    //     return res.reverse().toString();
+    // }
+
+    // Approach 2
     public static String removeStar(String s){
-        Stack<Character> stk = new Stack<>();
+        Deque<Character> d = new ArrayDeque<>();
         for(Character c : s.toCharArray()){
-            if(c == '*'){
-                stk.pop();
-            }else{
-                stk.push(c);
+            if(c == '*') d.removeLast();
+            else{
+                d.add(c);
             }
         }
         StringBuilder res = new StringBuilder();
-        while(!stk.isEmpty()){
-            res.append(stk.pop());
+        for(char c : d){
+            res.append(c);
         }
-        return res.reverse().toString();
+        return res.toString();
     }
     public static void main(String[] args) {
         
