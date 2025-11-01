9741361a1b937dc22053eae02ebca45368e58b0a
08abhinav <abhinavnegi101@gmail.com>
2025-11-01 20:30:36 +0530

    implementation done

--- Java/Math/HammingDist.java
+++ Java/Math/HammingDist.java
@@ -4,5 +4,8 @@ public class HammingDist{
         return Integer.bitCount(res);
     }
     public static void main(String... args){
+        int x = 4;
+        int y = 1;
+        System.out.println(HammingDist.hammingDistance(x, y));
     }
 }
