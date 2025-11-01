6308c9632abbb0245a12d4d0aa6665618b3c1534
08abhinav <abhinavnegi101@gmail.com>
2025-11-01 20:30:20 +0530

    implemented hammingDistance method

--- Java/Math/HammingDist.java
+++ Java/Math/HammingDist.java
@@ -1,5 +1,8 @@
 public class HammingDist{
+    public static int hammingDistance(int x, int y){
+        int res = x ^ y;
+        return Integer.bitCount(res);
+    }
     public static void main(String... args){
-        
     }
 }
