66df77a201354380639a1da15915d1efe64c77cd
08abhinav <abhinavnegi101@gmail.com>
2025-11-02 10:24:35 +0530

    implemented nextGreater method

--- Java/stack/MonotonicStack/NextGreaterEle.java
+++ Java/stack/MonotonicStack/NextGreaterEle.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 public class NextGreaterEle{
+    public static int[] nextGreater(int[] nums){
+        int[] res = new int[nums.length];
+        Arrays.fill(res, -1);
+        Stack<Integer> stk = new Stack<>();
+
+        for(int i = 0; i < nums.length; i++){
+            while(!stk.isEmpty() && nums[i] > nums[stk.peek()]){
+                int idx = stk.pop();
+                res[idx] = nums[i];
+            }
+            stk.push(i);
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
