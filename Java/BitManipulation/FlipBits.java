e7219e060af3b15e9afd4fccdde1117b5c84d746
08abhinav <abhinavnegi101@gmail.com>
2025-11-03 21:40:29 +0530

    implemented minBitsFlip method

--- Java/BitManipulation/FlipBits.java
+++ Java/BitManipulation/FlipBits.java
@@ -1,4 +1,7 @@
 public class FlipBits{
+    public static int minBitsFlip(int x, int y){
+        return Integer.bitCount(x ^ y);
+    }
     public static void main(String[] args) {
         
     }
