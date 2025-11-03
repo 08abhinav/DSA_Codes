f8ec7f1bdd3ff3d1988fdf773fd5c61fffa051ca
08abhinav <abhinavnegi101@gmail.com>
2025-11-03 21:41:22 +0530

    implementation done

--- Java/BitManipulation/FlipBits.java
+++ Java/BitManipulation/FlipBits.java
@@ -3,6 +3,7 @@ public class FlipBits{
         return Integer.bitCount(x ^ y);
     }
     public static void main(String[] args) {
-        
+        int x = 3, y = 4;
+        System.out.println(minBitsFlip(x, y));
     }
 }
