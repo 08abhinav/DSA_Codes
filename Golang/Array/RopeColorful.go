0cec35697e348652f68df707cdb6cb9c3abac178
08abhinav <abhinavnegi101@gmail.com>
2025-11-04 19:38:40 +0530

    implementation done

--- Golang/Array/RopeColorful.go
+++ Golang/Array/RopeColorful.go
@@ -1,16 +1,26 @@
-func ropeColorful(colors string, neededTime int)int{
+package main
+
+import "fmt"
+
+func ropeColorful(colors string, neededTime []int) int {
 	left, res := 0, 0
-	for right := 0; right < len(colors); right++{
-		if colors[left] == colors[right]{
-			if neededTime[left] < neededTime[right]{
+	for right := 0; right < len(colors); right++ {
+		if colors[left] == colors[right] {
+			if neededTime[left] < neededTime[right] {
 				res += neededTime[left]
 				left = right
-			}else{
+			} else {
 				res += neededTime[right]
 			}
-		}else{
+		} else {
 			left = right
 		}
 	}
 	return res
+}
+
+func main() {
+	colors := "aabaa"
+	neededTime := []int{1, 2, 3, 4, 1}
+	fmt.Println(ropeColorful(colors, neededTime))
 }
