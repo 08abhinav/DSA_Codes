2beed492e5cbaaa160463c1b4aa99a9f71e116d7
08abhinav <abhinavnegi101@gmail.com>
2025-11-04 19:31:30 +0530

    implemented ropeColorful method

--- Java/Array/RopeColorful.java
+++ Java/Array/RopeColorful.java
@@ -1,4 +1,17 @@
 public class RopeColorful{
+    public static int ropeColorful(String colors, int[] neededTime){
+        int minTime = 0;
+        int maxTime = neededTime[0];
+        for(int i = 1; i < colors.length(); i++){
+            if(colors.charAt(i) == colors.charAt(i - 1)){
+                minTime += Math.min(neededTime[i], maxTime);
+                maxTime = Math.max(neededTime[i], maxTime);
+            }else{
+                maxTime = Math.max(neededTime[i], maxTime);
+            }
+        }
+        return minTime;
+    }
     public static void main(String[] args) {
         
     }
