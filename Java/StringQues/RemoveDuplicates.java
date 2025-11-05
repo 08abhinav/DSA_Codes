93bb4101384cb15f8946d893f64716e7458e9024
08abhinav <abhinavnegi101@gmail.com>
2025-11-05 12:39:57 +0530

    implemented removeDuplicateLetters method

--- Java/StringQues/RemoveDuplicates.java
+++ Java/StringQues/RemoveDuplicates.java
@@ -1,4 +1,34 @@
+import java.util.Stack;
 public class RemoveDuplicates{
+    public static String removeDuplicateLetters(String s){
+        int[] res = new int[26];
+        boolean[] visited = new boolean[26];
+        char[] ch = s.toCharArray();
+
+        for(char c : ch){
+            res[c - 'a']++;
+        }
+
+        Stack<Character> stk = new Stack<>();
+        int idx = 0;
+        for(char c : ch){
+            idx = c - 'a';
+            res[idx]--;
+            if(visited[idx])continue;
+
+            while(!stk.isEmpty() && c < stk.peek() && res[stk.peek() - 'a'] != 0){
+                visited[stk.pop() - 'a'] = false;
+            }
+            stk.push(c);
+            visited[idx] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stk.isEmpty()){
+            sb.insert(0, stk.pop());
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
         
     }
