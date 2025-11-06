4ef443a3bdf245b3368709f6384795275ac6c5a3
08abhinav <abhinavnegi101@gmail.com>
2025-11-06 20:36:12 +0530

    resolve a minor issue

--- Java/Greedy/GasStation.java
+++ Java/Greedy/GasStation.java
@@ -1,7 +1,7 @@
 public class GasStation{
     public static int canCompleteCircle(int[] gas, int[] cost){
         int start = 0;
-        int currGas = 0, totalGas = 0;
+        int totalGas = 0, currGas = 0;
         for(int i = 0; i < gas.length; i++){
             totalGas += gas[i] - cost[i];
             currGas += gas[i] - cost[i];
@@ -11,7 +11,7 @@ public class GasStation{
                 start = i + 1;
             }
         }
-        return (totalGas > 0) ? start : -1;
+        return (totalGas >= 0) ? start : -1;
     }
     public static void main(String... args){
         int[] gas = {1, 2, 3, 4, 5};
