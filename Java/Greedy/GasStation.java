5a596d6b89aafefffd6a6eab1e97189ec41d6121
08abhinav <abhinavnegi101@gmail.com>
2025-11-06 20:34:11 +0530

    implementation done

--- Java/Greedy/GasStation.java
+++ Java/Greedy/GasStation.java
@@ -14,6 +14,8 @@ public class GasStation{
         return (totalGas > 0) ? start : -1;
     }
     public static void main(String... args){
-
+        int[] gas = {1, 2, 3, 4, 5};
+        int[] cost = {3, 4, 5, 1, 2};
+        System.out.println(GasStation.canCompleteCircle(gas, cost));
     }
 }
