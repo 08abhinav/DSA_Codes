81eb1ab4a1758bd3b074302e87c824054a61b350
08abhinav <abhinavnegi101@gmail.com>
2025-11-06 20:33:18 +0530

    implemented canCompleteCircle method

--- Java/Greedy/GasStation.java
+++ Java/Greedy/GasStation.java
@@ -1,5 +1,19 @@
 public class GasStation{
+    public static int canCompleteCircle(int[] gas, int[] cost){
+        int start = 0;
+        int currGas = 0, totalGas = 0;
+        for(int i = 0; i < gas.length; i++){
+            totalGas += gas[i] - cost[i];
+            currGas += gas[i] - cost[i];
+
+            if(currGas < 0){
+                currGas = 0;
+                start = i + 1;
+            }
+        }
+        return (totalGas > 0) ? start : -1;
+    }
     public static void main(String... args){
-        
+
     }
 }
