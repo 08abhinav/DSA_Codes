ceaad5cf82ca211bcc81fb02ac8b646a37d88721
08abhinav <abhinavnegi101@gmail.com>
2025-11-07 11:08:37 +0530

    added question

--- Java/Greedy/IntegerReplacement.java
+++ Java/Greedy/IntegerReplacement.java
@@ -1,3 +1,9 @@
+/*397. Integer Replacement
+Given a positive integer n, you can apply one of the following operations:
+
+If n is even, replace n with n / 2.
+If n is odd, replace n with either n + 1 or n - 1.
+Return the minimum number of operations needed for n to become 1. */
 public class IntegerReplacement {
     public static int integerReplace(int n){
         if(n == Integer.MAX_VALUE) return 32;
