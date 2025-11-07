790bf5280a211b3a09525de01ea766abb28b1094
08abhinav <abhinavnegi101@gmail.com>
2025-11-07 11:07:10 +0530

    implemented integerReplace method

--- Java/Greedy/IntegerReplacement.java
+++ Java/Greedy/IntegerReplacement.java
@@ -1,4 +1,17 @@
 public class IntegerReplacement {
+    public static int integerReplace(int n){
+        if(n == Integer.MAX_VALUE) return 32;
+        int count = 0;
+        while(n > 1){
+            if(n % 2 == 0) n /= 2;
+            else{
+                if((n + 1) % 4 == 0 && (n - 1 != 2)) n += 1;
+                else n -= 1;
+            }
+            count += 1;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         
     }
