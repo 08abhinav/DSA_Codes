98de2bf552ca9776fe9bea53b79006ca9e8a3c4d
08abhinav <abhinavnegi101@gmail.com>
2025-11-08 20:23:10 +0530

    implemented minOneBitOps

--- Java/BitManipulation/OneBitOps.java
+++ Java/BitManipulation/OneBitOps.java
@@ -1,5 +1,13 @@
 public class OneBitOps {
+    public static int minOneBitOps(int n){
+        int res = 0;
+        while(n > 0){
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
     public static void main(String...args){
-        
+
     }
 }
