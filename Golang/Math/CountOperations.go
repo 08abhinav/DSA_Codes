b5d2856b8fa58b921c563b82ce4b56ee0c6c86b3
08abhinav <abhinavnegi101@gmail.com>
2025-11-09 11:11:17 +0530

    implementation done

--- Golang/Math/CountOperations.go
+++ Golang/Math/CountOperations.go
@@ -1,9 +1,18 @@
-func countOps(num1, num2 int)int{
+package main
+
+import "fmt"
+
+func countOps(num1, num2 int) int {
 	res := 0
-	for num1 > 0 && num2 > 0{
+	for num1 > 0 && num2 > 0 {
 		res += num1 / num2
 		num1 %= num2
 		num1, num2 = num2, num1
 	}
 	return res
+}
+
+func main() {
+	num1, num2 := 2, 3
+	fmt.Println(countOps(num1, num2))
 }
