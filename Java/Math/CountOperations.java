8edb0e2b3819fd2903b2e0e4392d8c92b7961d90
08abhinav <abhinavnegi101@gmail.com>
2025-11-09 11:06:07 +0530

    implemented countOps method

--- Java/Math/CountOperations.java
+++ Java/Math/CountOperations.java
@@ -1,4 +1,15 @@
-public class countOperations{
+public class CountOperations{
+    public static int countOps(int num1, int num2){
+        int res = 0;
+        while(num1 > 0 && num2 > 0){
+            res += num1 / num2;
+            num1 %= num2;
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
