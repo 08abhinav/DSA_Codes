dae5b18f2ca2fce984d13ff1228d150b03b792c3
08abhinav <abhinavnegi101@gmail.com>
2025-11-11 10:56:23 +0530

    implemented findMaxForm method

--- Java/DynamicProgramming/OnesAndZeros.java
+++ Java/DynamicProgramming/OnesAndZeros.java
@@ -1,4 +1,21 @@
 public class OnesAndZeros{
+    public static int findMaxForm(String[] strs, int m, int n){
+        int[][] dp = new int[m + 1][n + 1];
+        for(String s : strs){
+            int zeros = 0, ones = 0;
+            for(char ch : s.toCharArray()){
+                if(ch == '0') zeros += 1;
+                else ones += 1;
+            }
+
+            for(int i = m; i >= zeros; i--){
+                for(int j = n; j >= ones; j--){
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
     public static void main(String[] args) {
         
     }
