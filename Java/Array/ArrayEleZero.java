d6a84e0e3a92fe38a6a071d9fa86b4e5324bb798
08abhinav <abhinavnegi101@gmail.com>
2025-11-12 10:57:18 +0530

    implemented minOps method

--- Java/Array/ArrayEleZero.java
+++ Java/Array/ArrayEleZero.java
@@ -1,5 +1,29 @@
 public class ArrayEleZero{
-    
+    public static int minOps(int[] nums){
+        int n = nums.length;
+        int countOnes = 0;
+        
+        for(int x : nums){
+            if(x == 1) countOnes += 1;
+        }
+        
+        if(countOnes > 0) return n - countOnes;
+
+        int minLen = 99;
+        for(int i = 0; i < n; i++){
+            int g = nums[i];
+            for(int j = i; j < n; j++){
+                g = gcd(g, nums[j]);
+                if(g == 1){
+                    minLen = Math.min(minLen, j - i + 1);
+                    break;
+                }
+            }
+        }
+        if(minLen == 99) return -1;
+        return (minLen - 1) + (n - 1);
+
+    }
     public static void main(String[] args) {
         
     }
