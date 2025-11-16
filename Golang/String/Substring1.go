c51d8382d4d2969a9b58a1a677de9e165a05cd88
08abhinav <abhinavnegi101@gmail.com>
2025-11-16 11:47:12 +0530

    implementation done

--- Golang/String/Substring1.go
+++ Golang/String/Substring1.go
@@ -1,13 +1,22 @@
-func numSub(s string) int{
+package main
+
+import "fmt"
+
+func numSub(s string) int {
 	var total, count, mod int64 = 0, 0, 1000000007
 
-	for i := 0; i < len(s); i++{
-		if s[i] == '1'{
+	for i := 0; i < len(s); i++ {
+		if s[i] == '1' {
 			count += 1
-		}else{
+		} else {
 			count = 0
 		}
 		total = (total + count) % mod
 	}
 	return int(total)
+}
+
+func main() {
+	s := "0110111"
+	fmt.Print(numSub(s))
 }
