e997651b281a41dda197a9496edbf16daa889d9a
08abhinav <abhinavnegi101@gmail.com>
2025-11-16 11:45:50 +0530

    implementing leetcode problem number of substring with 1 only

---
+++ Golang/String/Substring1.go
@@ -0,0 +1,13 @@
+func numSub(s string) int{
+	var total, count, mod int64 = 0, 0, 1000000007
+
+	for i := 0; i < len(s); i++{
+		if s[i] == '1'{
+			count += 1
+		}else{
+			count = 0
+		}
+		total = (total + count) % mod
+	}
+	return int(total)
+}
