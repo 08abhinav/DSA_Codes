4496ca8a25c2ab8d8caa5ec92064812c33138645
08abhinav <abhinavnegi101@gmail.com>
2025-11-18 21:39:50 +0530

    implementation done

--- Golang/Array/OneTwoBits.go
+++ Golang/Array/OneTwoBits.go
@@ -1,7 +1,16 @@
-func isOneBitCharacter(bits []int)bool{
+package main
+
+import "fmt"
+
+func isOneBitCharacter(bits []int) bool {
 	n, i := len(bits), 0
-	for i < n - 1{
+	for i < n-1 {
 		i += bits[i] + 1
 	}
-	return i == n - 1
+	return i == n-1
+}
+
+func main() {
+	bits := []int{1, 1, 1, 0}
+	fmt.Println(isOneBitCharacter(bits))
 }
