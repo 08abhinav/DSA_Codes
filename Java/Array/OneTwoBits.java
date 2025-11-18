53b2aa3978e465948304e939841b2783de7f0ec0
08abhinav <abhinavnegi101@gmail.com>
2025-11-18 21:34:47 +0530

    implemented isOneBitCharacter method

--- Java/Array/OneTwoBits.java
+++ Java/Array/OneTwoBits.java
@@ -1,7 +1,14 @@
 package Java.Array;
 
 public class OneTwoBits {
+    public static boolean isOneBitCharacter(int[] nums){
+        int i = 0, n = nums.length;
+        while(i < n - 1){
+            i += nums[i] + 1;
+        }
+        return i == n - 1;
+    }
     public static void main(String... args){
-        
+
     }
 }
