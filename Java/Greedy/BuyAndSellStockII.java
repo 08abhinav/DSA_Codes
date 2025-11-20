799989ce451360126fd74fc83ea9b5e9ef102e5c
08abhinav <abhinavnegi101@gmail.com>
2025-11-20 11:02:13 +0530

    implementation doen

--- Java/Greedy/BuyAndSellStockII.java
+++ Java/Greedy/BuyAndSellStockII.java
@@ -1,7 +1,7 @@
 public class BuyAndSellStockII{
     public static int maxProfit(int[] prices){
         int profit = 0;
-        for(int i = 0; i < prices.length; i++){
+        for(int i = 1; i < prices.length; i++){
             if(prices[i] > prices[i - 1]){
                 profit += prices[i] - prices[i - 1];
             }
@@ -9,6 +9,7 @@ public class BuyAndSellStockII{
         return profit;
     }
     public static void main(String[] args) {
-        
+        int[] profit = {7,1,5,3,6,4};
+        System.out.println(maxProfit(profit));
     }
 }
