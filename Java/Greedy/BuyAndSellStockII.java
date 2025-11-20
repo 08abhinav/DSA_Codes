6a26ef6a87aab8db711e021ab1f65268698bdd32
08abhinav <abhinavnegi101@gmail.com>
2025-11-20 11:00:57 +0530

    implemented maxProfit metho

--- Java/Greedy/BuyAndSellStockII.java
+++ Java/Greedy/BuyAndSellStockII.java
@@ -1,4 +1,13 @@
 public class BuyAndSellStockII{
+    public static int maxProfit(int[] prices){
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] > prices[i - 1]){
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
     public static void main(String[] args) {
         
     }
