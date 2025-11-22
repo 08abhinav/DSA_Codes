4955b5fbf66d76d2da171c9b3451275263d3e60f
08abhinav <abhinavnegi101@gmail.com>
2025-11-22 11:04:35 +0530

    implemented numSub method

--- Java/StringQues/SubstringWith1.java
+++ Java/StringQues/SubstringWith1.java
@@ -1,4 +1,16 @@
 public class SubstringWith1{
+    public static int numSub(String s){
+        long total = 0, count = 0, mod = 1000000007;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '1'){
+                count += 1;
+            }else{
+                count = 0;
+            }
+            total = (total + count) % mod;
+        }
+        return (int)total;
+    }
     public static void main(String[] args) {
         
     }
