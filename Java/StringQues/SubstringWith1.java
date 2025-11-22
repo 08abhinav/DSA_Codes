1093a0a9bccbfee6b980304a6e98fd42538dcc1f
08abhinav <abhinavnegi101@gmail.com>
2025-11-22 11:06:03 +0530

    added question

--- Java/StringQues/SubstringWith1.java
+++ Java/StringQues/SubstringWith1.java
@@ -1,3 +1,6 @@
+/*1513. Number of Substrings With Only 1
+Given a binary string s, return the number of substrings with all characters 1's. 
+Since the answer may be too large, return it modulo 109 + 7. */
 public class SubstringWith1{
     public static int numSub(String s){
         long total = 0, count = 0, mod = 1000000007;
