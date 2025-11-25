efffeb60966f68d2c1f1661d69c59646c45f1935
08abhinav <abhinavnegi101@gmail.com>
2025-11-25 16:28:04 +0530

    added question

--- Golang/Array/MinMoves.go
+++ Golang/Array/MinMoves.go
@@ -1,3 +1,8 @@
+/*
+453. Minimum Moves to Equal Array Elements
+Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+
+In one move, you can increment n - 1 elements of the array by 1.*/
 package main
 import "fmt"
 
