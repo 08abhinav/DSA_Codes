805627b152c80ed2db8c5db92dfcf9d82ed8521e
08abhinav <abhinavnegi101@gmail.com>
2025-11-25 16:27:23 +0530

    implementation done

--- Golang/Array/MinMoves.go
+++ Golang/Array/MinMoves.go
@@ -1,17 +1,25 @@
-func minMoves(nums []int)int{
-	if(len(nums) == 0){
+package main
+import "fmt"
+
+func minMoves(nums []int) int {
+	if len(nums) == 0 {
 		return 0
 	}
 
 	min, res := nums[0], 0
-	for _, i := range nums{
-		if i < min{
+	for _, i := range nums {
+		if i < min {
 			min = i
 		}
 	}
 
-	for _, j := range nums{
+	for _, j := range nums {
 		res += j - min
 	}
 	return res
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(minMoves(nums))
 }
