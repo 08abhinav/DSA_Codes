011ec6b9712451b27567eaf07c258de32d9c808b
08abhinav <abhinavnegi101@gmail.com>
2025-11-27 14:41:12 +0530

    updated file

--- Java/Math/SamllestIntegerk.java
+++ Java/Math/SamllestIntegerk.java
@@ -1,9 +1,11 @@
-/*1015. Smallest Integer Divisible by K
+/*
+1015. Smallest Integer Divisible by K
 Given a positive integer k, you need to find the length of the smallest positive integer n such 
 that n is divisible by k, and n only contains the digit 1.
 
 Return the length of n. If there is no such n, return -1.
-Note: n may not fit in a 64-bit signed integer. */
+Note: n may not fit in a 64-bit signed integer. 
+*/
 public class SamllestIntegerk{
     public static int smallestRepunitDivByK(int k) {
         if(k == 1) return 1;
