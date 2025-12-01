35cb116ce32fa8550ca7c86be47800e279cc80d7
08abhinav <abhinavnegi101@gmail.com>
2025-12-01 16:25:34 +0530

    implementation done

--- Java/Math/SumDivisibleByP.java
+++ Java/Math/SumDivisibleByP.java
@@ -30,6 +30,8 @@ public class SumDivisibleByP {
     }
 
     public static void main(String... args){
-
+        int nums[] = {3, 1, 4, 2};
+        int p = 6;
+        System.out.println(minSubarray(nums, p));
     }
 }
