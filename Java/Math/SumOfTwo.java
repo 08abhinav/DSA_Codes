605395adaba087242425eda99052b3f50c4383d0
08abhinav <abhinavnegi101@gmail.com>
2025-12-03 19:22:57 +0530

    implementatio done

--- Java/Math/SumOfTwo.java
+++ Java/Math/SumOfTwo.java
@@ -9,6 +9,7 @@ public class SumOfTwo {
         return a;
     }
     public static void main(String... args){
-        
+        int a = 2, b = 3;
+        System.out.println(getSum(a, b));
     }
 }
