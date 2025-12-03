0e8e54fbfb0e2820710a447a1da9d2f34334e0b4
08abhinav <abhinavnegi101@gmail.com>
2025-12-03 19:22:28 +0530

    implemented getSum method

--- Java/Math/SumOfTwo.java
+++ Java/Math/SumOfTwo.java
@@ -1,5 +1,14 @@
 public class SumOfTwo {
-    public static void main(Strig... args){
+    public static int getSum(int a, int b){
+        int c = 0;
+        while(b != 0){
+            c = a & b;
+            a = a ^ b;
+            b = c << 1;
+        }
+        return a;
+    }
+    public static void main(String... args){
         
     }
 }
