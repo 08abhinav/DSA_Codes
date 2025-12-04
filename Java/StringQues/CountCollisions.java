7b26d845581eac7102c6b16306d66bbcd29d6fb4
08abhinav <abhinavnegi101@gmail.com>
2025-12-04 20:41:53 +0530

    implementation done

--- Java/StringQues/CountCollisions.java
+++ Java/StringQues/CountCollisions.java
@@ -3,14 +3,15 @@ public class CountCollisions{
         int n = directions.length();
         int r = n - 1, l = 0, ans = 0;
         while(l < n &&  directions.charAt(l) == 'L') l += 1;
-        while(r >= l &&  directions.charAt(l) == 'R') r -= 1;
+        while(r >= l &&  directions.charAt(r) == 'R') r -= 1;
 
         for(int i = l; i <= r; i++){
-            if(directions.charAt(i) == 'S') ans += 1;
+            if(directions.charAt(i) != 'S') ans += 1;
         }
         return ans;
     }
     public static void main(String[] args) {
-        
+        String directions = "RLRSLL";
+        System.out.println(collisions(directions));
     }
 }
