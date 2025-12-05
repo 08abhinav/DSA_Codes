a3a85ee09991784189f26a25e7ecf27fba5fcaeb
08abhinav <abhinavnegi101@gmail.com>
2025-12-05 12:07:36 +0530

    implemented partitions method

--- Java/Array/CountPartitions.java
+++ Java/Array/CountPartitions.java
@@ -1,4 +1,20 @@
 public class CountPartitions{
+    public static int partitions(int[] nums){
+        int totalSum = 0;
+        for(int i : nums){
+            totalSum += i;
+        }
+
+        int leftSum = 0, count = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            leftSum += nums[i];
+            int rightSum = totalSum - leftSum;
+            if((leftSum % 2) == (rightSum % 2)){
+                count += 1;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         
     }
