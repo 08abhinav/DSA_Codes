d725eeda9480041792213a5a92b6b02032eab334
08abhinav <abhinavnegi101@gmail.com>
2025-12-06 18:52:59 +0530

    updated file

--- Golang/Array/SlidingWindow/FruiteInBask.go
+++ Golang/Array/SlidingWindow/FruiteInBask.go
@@ -1,29 +1,36 @@
 package main
 
-func totalFruit(fruits[]int)int{
+import "fmt"
+
+func totalFruit(fruits []int) int {
 	n := len(fruits)
-	if n == 0{
-		return 0;
+	if n == 0 {
+		return 0
 	}
 	count := make(map[int]int)
 	left, maxLen := 0, 0
 
-	for right := 0; right < n; right++{
+	for right := 0; right < n; right++ {
 		count[fruits[right]]++
 
-		for len(count) > 2{
+		for len(count) > 2 {
 			leftFruit := fruits[left]
 			count[fruits[left]]--
-			if count[leftFruit] == 0{
+			if count[leftFruit] == 0 {
 				delete(count, leftFruit)
 			}
 			left++
 		}
 
 		windowSize := right - left + 1
-		if maxLen < windowSize{
+		if maxLen < windowSize {
 			maxLen = windowSize
 		}
 	}
 	return maxLen
 }
+
+// func main() {
+// 	fruits := []int{1, 2, 1, 2, 3, 2, 2}
+// 	fmt.Println(totalFruit(fruits))
+// }
