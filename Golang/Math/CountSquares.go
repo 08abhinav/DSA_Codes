d83e491e24bc02fa65552e731af5b7c218df2ef5
08abhinav <abhinavnegi101@gmail.com>
2025-12-08 15:57:02 +0530

    implemented countSquare method

--- Golang/Math/CountSquares.go
+++ Golang/Math/CountSquares.go
@@ -1,3 +1,14 @@
-func (n int) int{
-	
+func countSquare(n int) int{
+	count := 0
+	for i:= 1; i <= n; i++{
+		for j := 1; j <=n; j++{
+			k := i*i + j*j
+			sq := int(math.Sqrt(float64(k)))
+
+			if sq <= n && sq * sq == k{
+				count += 1
+			}
+		}
+	}
+	return count
 }
