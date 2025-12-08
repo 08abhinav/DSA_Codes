bcc0b511fb2ee181eb7f539ad9f0cac9c36c1e5f
08abhinav <abhinavnegi101@gmail.com>
2025-12-08 15:59:23 +0530

    added question

--- Golang/Math/CountSquares.go
+++ Golang/Math/CountSquares.go
@@ -1,3 +1,11 @@
+/*1925. Count Square Sum Triples
+A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+
+Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.*/
+package main
+import (
+	"fmt"
+	"math")
 func countSquare(n int) int{
 	count := 0
 	for i:= 1; i <= n; i++{
