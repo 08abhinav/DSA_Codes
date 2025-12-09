c03a1ca06c59e23d853f714d37c3d93fd4d5a693
08abhinav <abhinavnegi101@gmail.com>
2025-12-09 18:01:34 +0530

    implemented checkSubArray method

--- Java/Array/ContiguousSum.java
+++ Java/Array/ContiguousSum.java
@@ -1,4 +1,24 @@
+import java.util.HashMap;
+
 public class ContiguousSum{
+    public static boolean checkSubArray(int[]nums, int k){
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int prefixSum = 0;
+        for(int i = 0; i < n; i++){
+            prefixSum += nums[i];
+            int remain = prefixSum % k;
+            if(map.containsKey(remain)){
+                if(i - map.get(remain) >= 2){
+                    return true;
+                }
+            }else{
+                map.put(remain, i);
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         
     }
