7cf400e31db81f2096693de9bf11f544116cd56f
08abhinav <abhinavnegi101@gmail.com>
2025-12-15 21:32:27 +0530

    implemented getProduct method

--- Java/Array/SlidingWindow/ProductK.java
+++ Java/Array/SlidingWindow/ProductK.java
@@ -1,6 +1,20 @@
 package SlidingWindow;
 
 public class ProductK {
+    public static int getProduct(int[] nums, int k){
+        if(k <= 1) return 0;
+        int left = 0, res = 0, product = 1;
+
+        for(int i = 0; i < nums.length; i++){
+            product *= nums[i];
+            while(product >= k){
+                product /= nums[left];
+                left += 1;
+            }
+            res += (i - left + 1);
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
