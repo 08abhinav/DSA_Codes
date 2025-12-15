ab2713e72c3e214cc5e828daccf672f92c89d1aa
08abhinav <abhinavnegi101@gmail.com>
2025-12-15 21:14:40 +0530

    implemented getDescentPeriod method

--- Java/Array/SmoothDescent.java
+++ Java/Array/SmoothDescent.java
@@ -1,5 +1,17 @@
 public class SmoothDescent {
+    public static long getDescentPeriod(int[] prices){
+        long res = 1, count = 1;
+        for(int i = 0; i < prices.length - 1; i++){
+            if(prices[i] == (prices[i + 1] + 1)){
+                count += 1;
+            }else{
+                count = 1;
+            }
+            res += count;
+        }
+        return res;
+    }
     public static void main(String... args){
-        
+
     }
 }
