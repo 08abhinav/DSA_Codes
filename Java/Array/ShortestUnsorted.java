4febf5f0678baa0139c879257495ab19f6196c23
08abhinav <abhinavnegi101@gmail.com>
2025-12-16 18:12:37 +0530

    implemented findUnsortedArray method

--- Java/Array/ShortestUnsorted.java
+++ Java/Array/ShortestUnsorted.java
@@ -1,5 +1,15 @@
 public class ShortestUnsorted {
+    public static int findUnsortedArray(int[] nums){
+        int n = nums.length, end = -2, beg = -1, max = nums[0], min = nums[n - 1];
+        for(int i = 1; i < n; i++){
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n - 1 - i]);
+            if(nums[i] < max) end = i;
+            if(nums[n - 1 - i] > min) beg = n - 1 - i;
+        }
+        return end - beg + 1;
+    }
     public static void main(String... args){
-        
+
     }
 }
