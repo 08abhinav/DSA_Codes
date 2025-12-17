6809048dbf5525185d0ad8e8adcd7dbb6f499573
08abhinav <abhinavnegi101@gmail.com>
2025-12-17 11:46:49 +0530

    updated file

--- Java/Array/SmallProduct.java
+++ Java/Array/SmallProduct.java
@@ -1,6 +1,8 @@
-/**Given two sorted 0-indexed integer arrays nums1 and nums2 as well as an integer k, 
+/*
+Given two sorted 0-indexed integer arrays nums1 and nums2 as well as an integer k, 
  * return the kth (1-based) smallest product of nums1[i] * nums2[j] 
- * where 0 <= i < nums1.length and 0 <= j < nums2.length. */
+ * where 0 <= i < nums1.length and 0 <= j < nums2.length. 
+ * */
 public class SmallProduct{
     public static long smallestProduct(int[] nums1, int[] nums2, int k){
         long left = -10000000000L;
