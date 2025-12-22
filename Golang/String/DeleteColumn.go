68213fa24079606ce4e9ab391ddf2a92a8f4f78a
08abhinav <abhinavnegi101@gmail.com>
2025-12-22 20:16:47 +0530

    implementation done

--- Golang/String/DeleteColumn.go
+++ Golang/String/DeleteColumn.go
@@ -1,13 +1,22 @@
-func minDeletion(strs []string)int{
+package main
+
+import "fmt"
+
+func minDeletion(strs []string) int {
 	m, n, count := len(strs), len(str[0]), 0
 
-	for col := 0; col < n; col++{
-		for row := 0; row < m - 1; row++{
-			if strs[row][col] > strs[row + 1][col]{
+	for col := 0; col < n; col++ {
+		for row := 0; row < m-1; row++ {
+			if strs[row][col] > strs[row+1][col] {
 				count += 1
 				break
 			}
 		}
 	}
 	return count
+}
+
+func main() {
+	strs := []string{"cba","daf","ghi"}
+	fmt.Println(minDeletion(strs))
 }
