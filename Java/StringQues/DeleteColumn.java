3783c5dd16c97c73d070176d8593e01de99fc01e
08abhinav <abhinavnegi101@gmail.com>
2025-12-22 20:10:23 +0530

    implemented minDeletion method

--- Java/StringQues/DeleteColumn.java
+++ Java/StringQues/DeleteColumn.java
@@ -1,5 +1,20 @@
 public class DeleteColumn {
+    public static int minDeletion(String[] strs){
+        int count = 0;
+        int m = strs.length;
+        int n = strs[0].length();
+
+        for(int col = 0; col < n; col++){
+            for(int row = 0; row < m - 1; row++){
+                if(strs[row].charAt(col) > strs[row + 1].charAt(col)){
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
     public static void main(String...args){
-        
+
     }
 }
