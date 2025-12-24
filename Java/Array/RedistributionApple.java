5c14b2654e3ed0cfb92496681496190da6ba897b
08abhinav <abhinavnegi101@gmail.com>
2025-12-24 13:56:37 +0530

    implemented minBoxed method

--- Java/Array/RedistributionApple.java
+++ Java/Array/RedistributionApple.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class RedistributionApple {
+    public static int minBoxes(int[] apple, int[] capacity){
+        Arrays.sort(capacity);
+        int sum = 0;
+        for(int i : apple){
+            sum += i;
+        }
+
+        int res = 0, cap = 0;
+        for(int i = capacity.length - 1; i >= 0; i--){
+            cap += capacity[i];
+            if(cap >= sum){
+                res = capacity.length - i;
+                break;
+            }
+        }
+        return res;
+    }
     public static void main(String...args){
-        
+
     }
 }
