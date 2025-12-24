79b54da8ff9bbac5881e126a5512b211a3689166
08abhinav <abhinavnegi101@gmail.com>
2025-12-24 14:01:15 +0530

    implemented minBoxes method

--- Golang/Array/RedistributionApple.go
+++ Golang/Array/RedistributionApple.go
@@ -1,3 +1,17 @@
 func minBoxes(apple, capacity []int)int{
-	
+	sort.Ints(capacity)
+	sum := 0
+	for _, i := range apple{
+		sum += i
+	}
+
+	res, cap := 0, 0
+	for i := len(capacity) - 1; i >= 0; i--{
+		cap = capacity[i]
+		if cap >= sum{
+			res = len(capacity) - i
+			break;
+		}
+	}
+	return res
 }
