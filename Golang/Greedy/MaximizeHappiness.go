74084202abe0ba283dc8084d8e80b15d006258bb
08abhinav <abhinavnegi101@gmail.com>
2025-12-26 12:11:28 +0530

    implemented maximizeHappiness method

--- Golang/Greedy/MaximizeHappiness.go
+++ Golang/Greedy/MaximizeHappiness.go
@@ -1,3 +1,14 @@
 func maximizeHappiness(happiness []int, k int)int64{
-	
+	sort.Ints(happiness)
+	n := happiness.length - 1
+	var res int64
+
+	for i := 0; i < k; i++{
+		if happiness[n - i] - i > 0{
+			res += int64 (happiness[n - i] - i)
+		}else{
+			break
+		}
+	}
+	return res
 }
