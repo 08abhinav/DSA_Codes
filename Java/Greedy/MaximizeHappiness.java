a119b7e1f9a92c3a1a8ec66f29925a87e00c97bf
08abhinav <abhinavnegi101@gmail.com>
2025-12-26 12:06:14 +0530

    implemented maximizeHappiness method

--- Java/Greedy/MaximizeHappiness.java
+++ Java/Greedy/MaximizeHappiness.java
@@ -1,4 +1,19 @@
+import java.util.Arrays;
+
 public class MaximizeHappiness {
+    public static long maximizeHappiness(int[] happiness, int k){
+        Arrays.sort(happiness);
+        int n = happiness.length - 1;
+        long res = 0;
+        for(int i = 0; i < k; i++){
+            if(happiness[n - i] - i > 0){
+                res += happiness[n - i] - i;
+            }else{
+                break;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         
     }
