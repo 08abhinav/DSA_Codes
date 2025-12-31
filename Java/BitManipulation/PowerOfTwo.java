a0009d85831656b2ca7d614f8607d5943ef2ef2e
08abhinav <abhinavnegi101@gmail.com>
2025-12-31 14:41:02 +0530

    updated file

--- Java/BitManipulation/PowerOfTwo.java
+++ Java/BitManipulation/PowerOfTwo.java
@@ -1,8 +1,8 @@
-/*231. Power of Two
+/*
+231. Power of Two
 Given an integer n, return true if it is a power of two. Otherwise, return false.
-
 An integer n is a power of two, if there exists an integer x such that n == 2x.
- */
+*/
 public class PowerOfTwo{
     // Approach 1
     // public static boolean isPowerOfTwo(int n){
