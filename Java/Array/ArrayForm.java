ef3315d8582031dd945dc083e18e08d81cde095d
08abhinav <abhinavnegi101@gmail.com>
2026-01-01 12:37:35 +0530

    implemented addToArrayForm method

--- Java/Array/ArrayForm.java
+++ Java/Array/ArrayForm.java
@@ -1,5 +1,22 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class ArrayForm {
+    public static List<Integer> addToArrayForm(int[] num, int k){
+        List<Integer> res = new LinkedList<>();
+        int i = num.length - 1;
+
+        while(k > 0 || i >= 0){
+            if(i >= 0){
+                k += num[i];
+                i--;
+            }
+            res.addFirst(k % 10);
+            k /= 10;
+        }
+        return res;
+    }
     public static void main(String... args){
-        
+
     }
 }
