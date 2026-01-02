dc774824e7bfccba0d8938147a3ee301671afe6a
08abhinav <abhinavnegi101@gmail.com>
2026-01-02 15:22:24 +0530

    implemented repeatedNTimes method

--- Java/Array/NRepeated.java
+++ Java/Array/NRepeated.java
@@ -1,4 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class NRepeated{
+    public static int repeatedNTimes(int[] nums){
+        Set<Integer> s = new HashSet<>();
+        for(int i : nums){
+            if(!s.add(i)){
+                return i;
+            }
+        }
+        return nums[nums.length - 1];
+    }
     public static void main(String[] args) {
         
     }
