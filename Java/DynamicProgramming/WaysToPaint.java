76e3e24ac2ed4c8ae60217227220a513bfaa9b03
08abhinav <abhinavnegi101@gmail.com>
2026-01-03 17:56:37 +0530

    implemented numOfWays method

--- Java/DynamicProgramming/WaysToPaint.java
+++ Java/DynamicProgramming/WaysToPaint.java
@@ -1,5 +1,16 @@
 public class WaysToPaint {
+    static final int MOD = 1000000007;
+    public static int numOfWays(int n){
+        long a[] = new long[n];
+        long b[] = new long[n];
+        a[0] = b[0] = 6;
+        for(int i = 1; i < n; i++){
+            a[i] = (2 * a[i - 1] + 2 * b[i - 1]) % MOD;
+            b[i] = (2 * a[i - 1] + 3 * b[i - 1]) % MOD;
+        }
+        return (int)((a[n - 1] + b[n - 1]) % MOD);
+    }
     public static void main(String... args){
-        
+
     }
 }
