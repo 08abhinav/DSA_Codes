16f8b9d4124422fdf41248431aa065eaeb35fc66
08abhinav <abhinavnegi101@gmail.com>
2026-01-04 16:59:54 +0530

    implemented sumFourDivisor method

--- Java/Math/FourDivisors.java
+++ Java/Math/FourDivisors.java
@@ -1,4 +1,12 @@
 public class FourDivisors{
+    public static int sumFourDivisors(int[] nums){
+        int sum = 0;
+        for(int i : nums){
+            int s = sumIfFour(i);
+            sum += s;
+        }
+        return sum;
+    }
     public static void main(String...args){
 
     }
