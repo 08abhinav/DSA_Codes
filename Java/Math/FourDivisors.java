83fb9b6e2c6c6ccbf5ebca57f3a93ca0b175ebba
08abhinav <abhinavnegi101@gmail.com>
2026-01-04 17:02:29 +0530

    implementation done

--- Java/Math/FourDivisors.java
+++ Java/Math/FourDivisors.java
@@ -8,7 +8,7 @@ public class FourDivisors{
         return sum;
     }
 
-    public static int sumIfFour(int n){
+    private static int sumIfFour(int n){
         int d1 = 0, d2 = 0;
         for(int i = 2; i * i <= n; i++){
             if(n % i == 0){
@@ -26,6 +26,7 @@ public class FourDivisors{
         return 1 + n + d1 + d2;
     }
     public static void main(String...args){
-
+        int[] nums = {21, 4, 7};
+        System.out.println(sumFourDivisors(nums));
     }
 }
