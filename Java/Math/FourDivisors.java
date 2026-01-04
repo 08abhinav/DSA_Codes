365450a2df0567e3ff4105f5db9973f6432c932a
08abhinav <abhinavnegi101@gmail.com>
2026-01-04 17:01:41 +0530

    implemented helper method sumIfFour

--- Java/Math/FourDivisors.java
+++ Java/Math/FourDivisors.java
@@ -7,6 +7,24 @@ public class FourDivisors{
         }
         return sum;
     }
+
+    public static int sumIfFour(int n){
+        int d1 = 0, d2 = 0;
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0){
+                if(d1 == 0){
+                    d1 = i;
+                    d2 = n / i;
+                }else{
+                    return 0;
+                }
+            }
+        }
+        if(d1 == 0)return 0;
+        if(d1 == d2) return 0;
+
+        return 1 + n + d1 + d2;
+    }
     public static void main(String...args){
 
     }
