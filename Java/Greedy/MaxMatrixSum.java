eabd0688ca11b76e0e57f8697b0a7927f230ae90
08abhinav <abhinavnegi101@gmail.com>
2026-01-05 19:25:29 +0530

    implemented maxSum method

--- Java/Greedy/MaxMatrixSum.java
+++ Java/Greedy/MaxMatrixSum.java
@@ -1,4 +1,22 @@
 public class MaxMatrixSum {
+    public static long maxSum(int[][] matrix){
+        int n = matrix.length;
+        int negCount = 0, minAbs = Integer.MAX_VALUE;
+        long totalSum = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int value = matrix[i][j];
+                totalSum += value;
+                if(value < 0){
+                    negCount++;
+                }
+                minAbs = Math.min(minAbs, Math.abs(value));
+            }
+        }
+
+        return negCount % 2 == 0 ? totalSum : totalSum - 2 * minAbs;
+    }
     public static void main(String[] args) {
         
     }
