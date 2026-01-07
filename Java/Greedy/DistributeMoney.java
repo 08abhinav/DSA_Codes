8b55d780b9b8e908f59f327fa65a900b3205f6c7
08abhinav <abhinavnegi101@gmail.com>
2026-01-07 17:52:12 +0530

    implemented distMoney method

--- Java/Greedy/DistributeMoney.java
+++ Java/Greedy/DistributeMoney.java
@@ -1,5 +1,18 @@
 public class DistributeMoney {
+    public static int distMoney(int money, int children){
+        money -= children;
+
+        if(money / 7 == children && money % 7 == 0){
+            return children;
+        }
+
+        if(money / 7 == children - 1 && money % 7 == 3){
+            return children - 2;
+        }
+
+        return Math.min(children - 1, money / 7);
+    }
     public static void main(String...args){
-        
+
     }
 }
