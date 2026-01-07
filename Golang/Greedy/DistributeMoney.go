1bdefd64a6b8819331455d0befe08e799a876573
08abhinav <abhinavnegi101@gmail.com>
2026-01-07 17:59:00 +0530

    implementation done

--- Golang/Greedy/DistributeMoney.go
+++ Golang/Greedy/DistributeMoney.go
@@ -1,20 +1,28 @@
-func distMoney(money, children int)int{
-	money -= children
-	
-	if (money / 7 == children && money % 7 == 0){
-		return children
-	}
+package main
 
-	if (money / 7 == children - 1 && money % 7 == 3){
-		return children - 2
-	}
+import "fmt"
 
-	return min(children - 1, money / 7)
-}
-
-func min(a, b int)int{
-	if a > b{
+func min(a, b int) int {
+	if a > b {
 		return b
 	}
 	return a
+}
+
+func distMoney(money, children int) int {
+	money -= children
+
+	if money/7 == children && money%7 == 0 {
+		return children
+	}
+
+	if money/7 == children-1 && money%7 == 3 {
+		return children - 2
+	}
+
+	return min(children-1, money/7)
+}
+
+func main() {
+	fmt.Println(distMoney(20, 3))
 }
