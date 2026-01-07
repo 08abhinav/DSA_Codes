c582e658eed147df811eec8d068aa8ad3c13e9d4
08abhinav <abhinavnegi101@gmail.com>
2026-01-07 17:57:05 +0530

    implemented distMoney method

--- Golang/Greedy/DistributeMoney.go
+++ Golang/Greedy/DistributeMoney.go
@@ -1,3 +1,13 @@
 func distMoney(money, children int)int{
+	money -= children
 	
+	if (money / 7 == children && money % 7 == 0){
+		return children
+	}
+
+	if (money / 7 == children - 1 && money % 7 == 3){
+		return children - 2
+	}
+
+	return min(children - 1, money / 7)
 }
