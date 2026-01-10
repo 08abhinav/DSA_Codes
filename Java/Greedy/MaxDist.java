3baeff9d586ff0254ed7047e1437374e1bfcbd0f
08abhinav <abhinavnegi101@gmail.com>
2026-01-10 13:37:07 +0530

    updated file

--- Java/Greedy/MaxDist.java
+++ Java/Greedy/MaxDist.java
@@ -1,11 +1,11 @@
-/*624. Maximum Distance in Arrayses
+/*
+624. Maximum Distance in Arrayses
 You are given m arrays, where each array is sorted in ascending order.
-
 You can pick up two integers from two different arrays (each array picks one) 
 and calculate the distance. We define the distance between two integers a and 
 b to be their absolute difference |a - b|.
-
-Return the maximum distance. */
+Return the maximum distance. 
+*/
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
