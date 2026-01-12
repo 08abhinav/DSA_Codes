cc35ffdbf5c952d36750d70c6a5c22d03131e5c0
08abhinav <abhinavnegi101@gmail.com>
2026-01-12 22:04:26 +0530

    implemented minTimeToVisitAllPoints method

--- Java/Math/MinTime.java
+++ Java/Math/MinTime.java
@@ -1,5 +1,12 @@
 public class MinTime {
+    public static int minTimeToVisitAllPoints(int[][] points) {
+        int res = 0;
+        for(int i = 1; i < points.length; i++){
+            res += Math.max(Math.abs(points[i][0] - points[i - 1][0]), Math.abs(points[i][1] - points[i - 1][1]));
+        }
+        return res;
+    }
     public static void main(String...args){
-        `
+        
     }
 }
