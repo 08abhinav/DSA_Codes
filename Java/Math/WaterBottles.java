6e1756b80da858b1fbad4b53298007ccb2524175
08abhinav <abhinavnegi101@gmail.com>
2026-01-16 20:14:47 +0530

    updated file

--- Java/Math/WaterBottles.java
+++ Java/Math/WaterBottles.java
@@ -1,9 +1,11 @@
-/*1518. Water Bottles
+/*
+1518. Water Bottles
 There are numBottles water bottles that are initially full of water. You can exchange numExchange empty 
 water bottles from the market with one full water bottle.
 
 The operation of drinking a full water bottle turns it into an empty bottle.
-Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink. */
+Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink. 
+*/
 class WaterBottles{
     public static int numWaterBottles(int numBottles, int numExchange) {
         int drinked = 0;
