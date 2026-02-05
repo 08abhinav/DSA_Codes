006da4a15293b4f2904cb3c96215eb26e8d9f802
08abhinav <abhinavnegi101@gmail.com>
2026-02-05 16:36:33 +0530

    implemented constructTransformedArray

--- Java/Array/TransformArray.java
+++ Java/Array/TransformArray.java
@@ -1,4 +1,12 @@
 public class TransformArray{
+    public static int[] constructTransformedArray(int[] nums){
+        int n = nums.length;
+        int[] res = new int[n];
+        for(int i = 0; i < n; i++){
+            res[i] = nums[(((i + nums[i]) % n) + n) % n];
+        }
+        return res;
+    }
     public static void main(String... args){
         
     }
