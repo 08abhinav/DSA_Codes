6908384806dda6324c0b61d9e257d8bdd58593e2
08abhinav <abhinavnegi101@gmail.com>
2026-02-06 20:45:47 +0530

    implemented minRemoval method

--- Java/Array/SlidingWindow/BalanceArray.java
+++ Java/Array/SlidingWindow/BalanceArray.java
@@ -1,4 +1,15 @@
 public class BalanceArray{
+    public static int minRemoval(int[] nums, int k){
+        Arays.sort(nums);
+        int l = 0, maxLen = 0;
+        for(int r = 0; r < nums.length; r++){
+            while((long)nums[r] > nums[l] * k){
+                l += 1
+            }
+            maxLen = Math.max(maxLen, r - l + 1);
+        }
+        return nums.length - maxLen;
+    }
     public static void main(String... args){
         
     }
