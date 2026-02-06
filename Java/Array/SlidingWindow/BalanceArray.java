6d4199a011e5b88c8502da6ca1627aa76be72b70
08abhinav <abhinavnegi101@gmail.com>
2026-02-06 20:47:00 +0530

    added question

--- Java/Array/SlidingWindow/BalanceArray.java
+++ Java/Array/SlidingWindow/BalanceArray.java
@@ -1,3 +1,10 @@
+/*
+3634. Minimum Removals to Balance Array
+You are given an integer array nums and an integer k.
+An array is considered balanced if the value of its maximum element is at most k times the minimum element.
+You may remove any number of elements from nums​​​​​​​ without making it empty. 
+Return the minimum number of elements to remove so that the remaining array is balanced.
+Note: An array of size 1 is considered balanced as its maximum and minimum are equal, and the condition always holds true. */
 public class BalanceArray{
     public static int minRemoval(int[] nums, int k){
         Arays.sort(nums);
