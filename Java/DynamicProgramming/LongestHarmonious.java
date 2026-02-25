f859e7b373e6bbee19e5a34b2c99d3bb809abe4e
08abhinav <abhinavnegi101@gmail.com>
2026-02-25 20:09:04 +0530

    updated file

--- Java/DynamicProgramming/LongestHarmonious.java
+++ Java/DynamicProgramming/LongestHarmonious.java
@@ -1,6 +1,7 @@
-/**We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
-
-Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences. */
+/*
+We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences. 
+*/
 import java.util.Arrays;
 
 public class LongestHarmonious{
