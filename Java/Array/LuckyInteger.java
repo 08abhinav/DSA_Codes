345693678daf84234680c5289cc9f66ae3c26d3f
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-03-01 21:03:11 +0530

    updated file

--- Java/Array/LuckyInteger.java
+++ Java/Array/LuckyInteger.java
@@ -1,7 +1,8 @@
-/**Given an array of integers arr, a lucky integer is an integer that has a frequency 
- * in the array equal to its value.
+/*
+Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
 
-Return the largest lucky integer in the array. If there is no lucky integer return -1. */
+Return the largest lucky integer in the array. If there is no lucky integer return -1.
+*/
 import java.util.HashMap;
 import java.util.Map;
 
