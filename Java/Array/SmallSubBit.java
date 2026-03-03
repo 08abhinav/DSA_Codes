f52c4e5a99ebe131783710799b4e698ca9303d4c
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-03-03 11:16:19 +0530

    Update SmallSubBit.java

--- Java/Array/SmallSubBit.java
+++ Java/Array/SmallSubBit.java
@@ -1,4 +1,5 @@
-/**2411. Smallest Subarrays With Maximum Bitwise OR
+/*
+2411. Smallest Subarrays With Maximum Bitwise OR
 You are given a 0-indexed array nums of length n, consisting of non-negative integers. 
 For each index i from 0 to n - 1, you must determine the size of the minimum sized non-empty 
 subarray of nums starting at i (inclusive) that has the maximum possible bitwise OR.
@@ -10,7 +11,8 @@ where i <= k <= n - 1. The bitwise OR of an array is the bitwise OR of all the n
 Return an integer array answer of size n where answer[i] is the length of the minimum sized 
 subarray starting at i with maximum bitwise OR.
 
-A subarray is a contiguous non-empty sequence of elements within an array. */
+A subarray is a contiguous non-empty sequence of elements within an array. 
+*/
 import java.util.Arrays;
 
 public class SmallSubBit{
