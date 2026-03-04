f6ea660b9f79f44938da12e248a8001d67f8c3e0
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-03-04 09:49:11 +0530

    Update ValidNumber.java

--- Java/StringQues/ValidNumber.java
+++ Java/StringQues/ValidNumber.java
@@ -1,4 +1,5 @@
-/**Given a string s, return whether s is a valid number.
+/*
+Given a string s, return whether s is a valid number.
 
 For example, all the following are valid numbers: "2", "0089", "-0.1", "+3.14", "4.", 
 "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789", while the following 
@@ -17,7 +18,8 @@ Digits followed by a dot '.' followed by digits.
 A dot '.' followed by digits.
 An exponent is defined with an exponent notation 'e' or 'E' followed by an integer number.
 
-The digits are defined as one or more digits. */
+The digits are defined as one or more digits. 
+*/
 public class ValidNumber {
     public static boolean isNumber(String s){
         s = s.trim();
