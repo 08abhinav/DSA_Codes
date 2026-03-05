c1ec4e9f6e0a6a466c6ae531c4d1eb2dfa705aa1
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-03-05 21:26:41 +0530

    updated file

--- Java/StringQues/KGroupString.java
+++ Java/StringQues/KGroupString.java
@@ -1,5 +1,5 @@
-/**A string s can be partitioned into groups of size k using the following procedure:
-
+/*
+A string s can be partitioned into groups of size k using the following procedure:
 The first group consists of the first k characters of the string, the second group consists
 of the next k characters of the string, and so on. Each element can be a part of exactly one group.
 For the last group, if the string does not have k characters remaining, a character fill is
@@ -8,7 +8,8 @@ Note that the partition is done so that after removing the fill character from t
 (if it exists) and concatenating all the groups in order, the resultant string should be s.
 
 Given the string s, the size of each group k and the character fill, return a string array 
-denoting the composition of every group s has been divided into, using the above procedure. */
+denoting the composition of every group s has been divided into, using the above procedure. 
+*/
 import java.util.Arrays;
 
 public class KGroupString {
