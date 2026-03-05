0d308ed66169151280083fe1991dc3bd40a28f73
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-03-05 21:26:18 +0530

    updated file

--- Java/StringQues/GreatString.java
+++ Java/StringQues/GreatString.java
@@ -1,4 +1,5 @@
-/*1544. Make The String Great
+/*
+1544. Make The String Great
 Given a string s of lower and upper case English letters.
 A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
     0 <= i <= s.length - 2
@@ -7,7 +8,8 @@ To make the string good, you can choose two adjacent characters that make the st
 You can keep doing this until the string becomes good.
 Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
 
-Notice that an empty string is also good. */
+Notice that an empty string is also good. 
+*/
 import java.util.Stack;
 
 public class GreatString{
