ddaeb3f3faccdeda3dc3e5554344a2fa881fd634
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-03-22 20:45:55 +0530

    updated file

--- Golang/BinarySearch/PeakElement.go
+++ Golang/BinarySearch/PeakElement.go
@@ -1,4 +1,5 @@
-/*162. Find Peak Element
+/*
+162. Find Peak Element
 A peak element is an element that is strictly greater than its neighbors.
 
 Given a 0-indexed integer array nums, find a peak element, and return its index. 
@@ -6,8 +7,8 @@ If the array contains multiple peaks, return the index to any of the peaks.
 
 You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always 
 considered to be strictly greater than a neighbor that is outside the array.
-
-You must write an algorithm that runs in O(log n) time.*/
+You must write an algorithm that runs in O(log n) time.
+*/
 package main
 import "fmt"
 
