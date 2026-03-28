192fe9f7367001081873c62e397444e7bf9d39b7
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-03-28 20:37:02 +0530

    updated file

--- Java/Greedy/BuyAndSellStockII.java
+++ Java/Greedy/BuyAndSellStockII.java
@@ -5,7 +5,8 @@ You are given an integer array prices where prices[i] is the price of a given st
 On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the 
 stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring 
 you never hold more than one share of the stock.
-Find and return the maximum profit you can achieve. */
+Find and return the maximum profit you can achieve. 
+*/
 public class BuyAndSellStockII{
     public static int maxProfit(int[] prices){
         int profit = 0;
