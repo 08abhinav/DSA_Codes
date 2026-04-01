fc1453b3835ee09f9daac117af037f58dbb6c096
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-04-01 21:08:16 +0530

    updated file

--- Java/DynamicProgramming/BitwiseOR.java
+++ Java/DynamicProgramming/BitwiseOR.java
@@ -5,7 +5,8 @@ Given an integer array arr, return the number of distinct bitwise ORs of all the
 The bitwise OR of a subarray is the bitwise OR of each integer in the subarray. 
 The bitwise OR of a subarray of one integer is that integer.
 
-A subarray is a contiguous non-empty sequence of elements within an array. */
+A subarray is a contiguous non-empty sequence of elements within an array. 
+*/
 import java.util.HashSet;
 import java.util.Set;
 
