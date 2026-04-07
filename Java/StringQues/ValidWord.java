349f31437ee7e269c2f299a1607b1041b518e773
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-04-07 20:14:36 +0530

    updated file

--- Java/StringQues/ValidWord.java
+++ Java/StringQues/ValidWord.java
@@ -1,5 +1,5 @@
-/**A word is considered valid if:
-
+/*
+A word is considered valid if:
 It contains a minimum of 3 characters.
 It contains only digits (0-9), and English letters (uppercase and lowercase).
 It includes at least one vowel.
@@ -7,7 +7,6 @@ It includes at least one consonant.
 You are given a string word.
 
 Return true if word is valid, otherwise, return false.
-
  */
 public class ValidWord{
     public static boolean isValid(String word){
