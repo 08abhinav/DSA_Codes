7b7b8e6362c21c0aa0d7984bef4020204dc6e3d4
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-04-24 22:03:12 +0530

    updated file

--- Java/Array/ContiguousSum.java
+++ Java/Array/ContiguousSum.java
@@ -1,4 +1,5 @@
-/*523. Continuous Subarray Sum
+/*
+523. Continuous Subarray Sum
 Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
 
 A good subarray is a subarray where:
@@ -7,7 +8,8 @@ the sum of the elements of the subarray is a multiple of k.
 Note that:
 
 A subarray is a contiguous part of the array.
-An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k. */
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k. 
+*/
 import java.util.HashMap;
 
 public class ContiguousSum{
