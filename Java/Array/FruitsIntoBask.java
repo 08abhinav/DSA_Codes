5b9ae7bc52488ccf70e8dc462e23dab16b85e52d
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-04-25 16:42:58 +0530

    updated file

--- Java/Array/FruitsIntoBask.java
+++ Java/Array/FruitsIntoBask.java
@@ -1,4 +1,5 @@
-/**3477. Fruits Into Baskets II
+/*
+3477. Fruits Into Baskets II
 You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] 
 represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
 
@@ -8,7 +9,8 @@ Each fruit type must be placed in the leftmost available basket with a capacity 
 or equal to the quantity of that fruit type.
 Each basket can hold only one type of fruit.
 If a fruit type cannot be placed in any basket, it remains unplaced.
-Return the number of fruit types that remain unplaced after all possible allocations are made. */
+Return the number of fruit types that remain unplaced after all possible allocations are made. 
+*/
 public class FruitsIntoBask{
     public static int numOfUnplacedFruits(int[] fruits, int[] baskets){
         int c = 0;
