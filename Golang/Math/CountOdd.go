d910834d07819c6f8f1b4226706468dfdc9f9762
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-04-30 19:52:36 +0530

    updated file

--- Golang/Math/CountOdd.go
+++ Golang/Math/CountOdd.go
@@ -1,5 +1,7 @@
-/*1523. Count Odd Numbers in an Interval Range
-Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).*/
+/*
+1523. Count Odd Numbers in an Interval Range
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+*/
 package main
 
 import "fmt"
