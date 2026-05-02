8774ed057feb5789abbaf364fe2c3c0f38256cef
Abhinav Negi <125854461+08abhinav@users.noreply.github.com>
2026-05-02 21:28:00 +0530

    updated file

--- Golang/String/DeleteColumn.go
+++ Golang/String/DeleteColumn.go
@@ -1,4 +1,5 @@
-/*944. Delete Columns to Make Sorted
+/*
+944. Delete Columns to Make Sorted
 You are given an array of n strings strs, all of the same length.
 
 The strings can be arranged such that there is one on each line, making a grid.
@@ -9,8 +10,8 @@ cae
 You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), 
 columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted, while column 1 ('b', 'c', 'a') is not, 
 so you would delete column 1.
-
-Return the number of columns that you will delete.*/
+Return the number of columns that you will delete.
+*/
 package main
 
 import "fmt"
